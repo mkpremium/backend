6bdc9917cdf13fb10ba43c5cec9d9e4fe93d34d5
khoagb1 <kodeplusdev+gb1@gmail.com>
2017-12-11 23:07:51 +0700

    Sample app for login

---
+++ development/app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var config = require('./utils/config');
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+require('./routes/routes')(app);
+
+app.listen(config.application.httpPort);
+
+var http = require('http'),
+    faye = require('faye');
+
+var server = http.createServer(),
+    bayeux = new faye.NodeAdapter({mount: '/faye', timeout: 45});
+
+bayeux.attach(server);
+server.listen(8000);
+
+//// ▶▶ uncaught exception - DEBUG only ◀◀ ////
+
+/*
+ process.on('uncaughtException', function (err) {
+     console.log('Caught exception: ' + err);
+     console.log('Please confirm the couchbase instance: ',config.couchbase.endPoint,
+         'is running and accessible. ');
+     process.exit();
+ });
+*/
---
+++ development/routes/routes.js
@@ -0,0 +1,138 @@
+//// ▶▶ require objects ◀◀ ////
+
+var bodyParser = require('body-parser');
+var http = require('http');
+var db = require('../utils/db');
+var airport=require('../model/airport');
+var flightPath=require('../model/flightPath');
+var auth=require('../model/auth.js');
+var user=require('../model/user.js');
+var jwt = require('jsonwebtoken');
+var config = require('./../utils/config');
+var sec=config.application.hashToken;
+
+//// ▶▶ application/json parser ◀◀ ////
+var jsonParser = bodyParser.json();
+
+//// ▶▶ application/x-www-form-urlencoded parser ◀◀ ////
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+module.exports = function (app) {
+
+    //// ▶▶ enable cors ◀◀ ////
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
+
+    //// ▶▶ airports ◀◀ ////
+    app.get('/api/airport/findAll',function(req,res) {
+        if (req.query.search) {
+            airport.findAll(req.query.search, jwt.decode(req.query.token).user,function (err, done) {
+                if (err) {
+                    res.status = 400;
+                    res.send(err);
+                    return;
+                }
+                res.status = 202;
+                res.send(done);
+            });
+        }else{
+            res.status = 400;
+            res.send({"airport":"bad request"});
+            return;
+        }
+    });
+
+    //// ▶▶ flightPath ◀◀ ////
+    app.get('/api/flightPath/findAll',function(req,res){
+        if(req.query.from && req.query.to && req.query.leave){
+            flightPath.findAll(req.query.from, req.query.to,req.query.leave,jwt.decode(req.query.token).user, function (err, done) {
+                if (err) {
+                    res.status = 400;
+                    res.send(err);
+                    return;
+                }
+                res.status = 202;
+                res.send(done);
+            });
+        }else{
+            res.status = 400;
+            res.send({"flightPath":"bad request"});
+            return;
+        }
+    });
+
+    //// ▶▶ create login ◀◀ ////
+    app.post('/api/user/login',jsonParser,function(req,res){
+        auth.createLogin(req.body.user,req.body.password,function(err,done){
+            if(err){
+                res.status=400;
+                res.send(err);
+                return;
+            }
+            res.status=202;
+            res.send(done);
+            return;
+        });
+    });
+
+    //// ▶▶ login ◀◀ ////
+    app.get('/api/user/login',urlencodedParser,function(req,res){
+        auth.login(req.query.user,req.query.password,function(err,check){
+            if(err){
+                res.status=400;
+                res.send(err);
+                return;
+            }
+            if(check){
+                res.status=202;
+                res.send(check);
+                return;
+            }
+        });
+    });
+
+    //// ▶▶ book flights ◀◀ ////
+    app.post('/api/user/flights',jsonParser,function(req,res){
+        auth.book(req.body.token,req.body.flights,function(err,done){
+            if(err){
+                res.status=400;
+                res.send(err);
+                return;
+            }
+            res.status=202;
+            res.send({added:done});
+            return;
+        });
+    });
+
+    //// ▶▶ booked flights ◀◀ ////
+    app.get('/api/user/flights',urlencodedParser,function(req,res){
+        auth.booked(req.query.token,function(err,done){
+            if(err){
+                res.status=400;
+                res.send(err);
+                return;
+            }
+            res.status=202;
+            res.send(done);
+            return;
+        });
+    });
+
+    //// ▶▶ verify environment and publish URL to console ◀◀ ////
+    db.query('SELECT NAME FROM system:keyspaces',function(err,result){
+       if(err){
+           console.log('Please confirm the couchbase instance:\n  ',config.couchbase.endPoint,
+               'Is running and accessible. ');
+           process.exit();
+       }
+        else{
+           console.log("ENVIRONMENT: READY--LOGIN AT:","http://" + config.application.hostName +
+               ":" + config.application.httpPort);
+       }
+    });
+
+}
