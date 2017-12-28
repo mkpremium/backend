836894547cbc8785f8883750a7374889cc104c74
Jordi Sabaté <zarpa37@gmail.com>
2017-12-28 17:47:08 +0100

    authentication enabled for API

--- app/routes/v1.js
+++ app/routes/v1.js
@@ -31,103 +31,103 @@ router.get('/', function(req, res) {
 	res.json({ message: 'Welcome to the v1 API!' });
 });
 
-router.get('/worksheets/queueitem', function(req, res) {
+router.get('/worksheets/queueitem', verifyToken, function(req, res) {
     v1Manager.getQueueItem(res);
 });
 
-router.get('/worksheets/fifo/normal', function(req, res) {
+router.get('/worksheets/fifo/normal', verifyToken, function(req, res) {
     v1Manager.getQueueItemNormal(res);
 });
 
-router.get('/worksheets/fifo/recall', function(req, res) {
+router.get('/worksheets/fifo/recall', verifyToken, function(req, res) {
     v1Manager.getQueueItemRecall(res);
 });
 
-router.get('/worksheets/queueitem2', function(req, res) {
+router.get('/worksheets/queueitem2', verifyToken, function(req, res) {
     v1Manager.getQueueItemWithOwners(res);
 });
 
-router.get('/worksheets/allqueue', function(req, res) {
+router.get('/worksheets/allqueue', verifyToken, function(req, res) {
     v1Manager.getQueue(res);
 });
 
-router.get('/worksheets/history/:worksheetId', function(req, res) {        
+router.get('/worksheets/history/:worksheetId', verifyToken, function(req, res) {        
     v1Manager.getHistory(res, req.params.worksheetId);
 });
-router.post('/worksheets/history/', function(req, res) {        
+router.post('/worksheets/history/', verifyToken, function(req, res) {        
     v1Manager.getHistory(res, req.body.worksheetId);
 });
 
-router.get('/worksheets/owner/:name', function(req, res) {        
+router.get('/worksheets/owner/:name', verifyToken, function(req, res) {        
     v1Manager.getOwnerProperties(res, req.params.name);
 });
-router.post('/worksheets/owner', function(req, res) {        
+router.post('/worksheets/owner', verifyToken, function(req, res) {        
     v1Manager.getOwnerProperties(res, req.body.name);
 });
 
-router.post('/worksheets/addOwner', function(req, res) {        
+router.post('/worksheets/addOwner', verifyToken, function(req, res) {        
     v1Manager.addWorksheetOwner(res, req.body.worksheetId, req.body.owner);
 });
 
-router.post('/worksheets/setFifoState', function(req, res) {        
+router.post('/worksheets/setFifoState', verifyToken, function(req, res) {        
     v1Manager.setFIFOState(res, req.body.worksheetId, req.body.fifo, req.body.state);
 });
 
-router.get('/owner/get/:id', function(req, res) {        
+router.get('/owner/get/:id', verifyToken, function(req, res) {        
     v1Manager.getOwner(res, req.params.id);
 });
 
-router.get('/owner/get/:id', function(req, res) {        
+router.get('/owner/get/:id', verifyToken, function(req, res) {        
     v1Manager.getOwner(res, req.params.id);
 });
 
-router.get('/owners/getregistry/:name', function(req, res) {        
+router.get('/owners/getregistry/:name', verifyToken, function(req, res) {        
     v1Manager.getRegistryOwners(res, req.params.name);
 });
-router.post('/owners/getregistry/', function(req, res) {        
+router.post('/owners/getregistry/', verifyToken, function(req, res) {        
     v1Manager.getRegistryOwners(res, req.body.name);
 });
 
-router.post('/owners/update/', function(req, res) {        
+router.post('/owners/update/', verifyToken, function(req, res) {        
     v1Manager.updateOwner(res, req.body.owner);
 });
 
-router.get('/persons/getowners/:name', function(req, res) {        
+router.get('/persons/getowners/:name', verifyToken, function(req, res) {        
     v1Manager.getPersonsOwners(res, req.params.name);
 });
-router.post('/persons/getowners/', function(req, res) {        
+router.post('/persons/getowners/', verifyToken, function(req, res) {        
     v1Manager.getPersonsOwners(res, req.body.name);
 });
 
-router.post('/persons/family/', function(req, res) {        
+router.post('/persons/family/', verifyToken, function(req, res) {        
     v1Manager.getPersonFamily(res, req.body.address, req.body.state, req.body.postalCode);
 });
 
-router.post('/persons/brothers/', function(req, res) {        
+router.post('/persons/brothers/', verifyToken, function(req, res) {        
     v1Manager.getPersonBrothers(res, req.body.surname1, req.body.surname2, req.body.bornYear, req.body.id);
 });
 
-router.post('/persons/sons/', function(req, res) {        
+router.post('/persons/sons/', verifyToken, function(req, res) {        
     v1Manager.getPersonSons(res, req.body.surname1, req.body.surname1Year, req.body.bornYear);
 });
 
-router.post('/persons/house/', function(req, res) {        
+router.post('/persons/house/', verifyToken, function(req, res) {        
     v1Manager.getPersonHouse(res, req.body.address, req.body.postalCode);
 });
 
-router.post('/housestate/get/:catastroid', function(req, res) {        
+router.post('/housestate/get/:catastroid', verifyToken, function(req, res) {        
     v1Manager.getHouseState(res, req.body.catastroid);
 });
 
-router.post('/history/add/', function(req, res) {        
+router.post('/history/add/', verifyToken, function(req, res) {        
     v1Manager.addHistory(res, req.body.history);
 });
 
-router.post('/history/remove/', function(req, res) {        
+router.post('/history/remove/', verifyToken, function(req, res) {        
     v1Manager.removeHistory(res, req.body.history);
 });
 
-router.post('/history/search/', function(req, res) {        
+router.post('/history/search/', verifyToken, function(req, res) {        
     v1Manager.searchHistory(res, req.body.search);
 });
 
