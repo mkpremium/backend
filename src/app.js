4d8846215ffc5fa91400d6a769975f2efacf80ad
Julian reyes Escrigas <julian.reyes.escrigas@gmail.com>
2018-01-18 07:39:54 -0500

    add jwt token management
    instead writing our own code about there's a good library that do that and allow exclude easily paths of the verification of the token

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import fileUpload from 'express-fileupload';
 import morgan from 'morgan';
 import cors from 'cors';
 import couchbase from './db/couchbase';
+import jwt from './middleware/jwt';
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(bodyParser.json());
 app.use(fileUpload());
 app.use(morgan('dev'));
 app.use(cors());
+app.use(jwt());
 
 Object.assign(app.locals, couchbase);
 
---
+++ src/middleware/jwt.js
@@ -0,0 +1,8 @@
+import jwtMiddleware from 'express-jwt';
+import {jwt} from '../../config';
+
+export default () => {
+  return jwtMiddleware(jwt).unless({
+    path: ['/operator/login']
+  });
+};
