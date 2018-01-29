2636e09f8dc24d9b0471a330f068a7e835dc4801
Julian reyes Escrigas <julian.reyes.escrigas@gmail.com>
2018-01-29 06:07:02 -0500

    improve jwt
    - show debug log
    - remove security for now

--- src/middleware/jwt.js
+++ src/middleware/jwt.js
@@ -1,9 +1,14 @@
+import debug from 'debug';
 import jwtMiddleware from 'express-jwt';
 import {jwt} from '../../config';
 
+const jwtDebug = debug('app:jwt');
+
 export default () => {
+  jwtDebug('initialized JWT');
   return jwtMiddleware(jwt).unless({
     path: [
+      /\/migration/,
       '/operator/login',
       '/webhooks/numintec'
     ]
