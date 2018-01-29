10577cad358e8f5ad4f774e8edeb6e87592588d3
Julian reyes Escrigas <julian.reyes.escrigas@gmail.com>
2018-01-29 06:06:15 -0500

    improve connection to db
    wait and watch until the connection is made with 3 retries

--- src/db/couchbase.js
+++ src/db/couchbase.js
@@ -1,10 +1,16 @@
+import debug from 'debug';
 import Couchbase from 'couchbase';
 import {couchbase} from '../../config';
 import Promise from 'bluebird';
 
 import attachHelpers from './helpers';
 
+const debugCouchbase = debug('app:couchbase');
+
+let retries = 3;
+
 export default () => {
+  debugCouchbase(`initializing couchbase connection with "${couchbase.uri}"`);
   const cluster = new Couchbase.Cluster(couchbase.uri);
   cluster.authenticate(couchbase.user, couchbase.pass);
 
@@ -12,10 +18,27 @@ export default () => {
   // library
   const bucket = Promise.promisifyAll(cluster.openBucket(couchbase.bucket));
 
-  attachHelpers(bucket);
+  checkBucket(bucket);
 
   return (req, res, next) => {
     Object.assign(req.app.locals, {cluster, bucket});
     next();
   };
 };
+
+function checkBucket(bucket) {
+  if (retries <= 0) {
+    console.error(`It's possible a error trying to connect bucket ${bucket._name} check your setup`);
+    process.exit(1);
+  }
+
+  debugCouchbase(`checking bucket ${bucket._name} for connection (${retries})`);
+
+  if (bucket.connected) {
+    debugCouchbase(`bucket ${bucket._name} connected`);
+    attachHelpers(bucket);
+  } else {
+    retries--;
+    setTimeout(() => checkBucket(bucket), 1500);
+  }
+}
