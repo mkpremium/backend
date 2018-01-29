cc6a88a4d052ca13eb88cd724553d567466f46d1
Julian reyes Escrigas <julian.reyes.escrigas@gmail.com>
2018-01-29 06:02:31 -0500

    lint bin folder

--- bin/www.js
+++ bin/www.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 import http from 'http';
 import debug from 'debug';
 
@@ -13,33 +14,32 @@ server.on('error', errorHandler);
 server.on('listen', listenHandler);
 
 function listenHandler() {
-    var addr = server.address();
-    var bind = typeof addr === 'string'
-        ? 'pipe ' + addr
-        : 'port ' + addr.port;
-    serverDebug('Listening on ' + bind);
+  const addr = server.address();
+  const bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  serverDebug('Listening on ' + bind);
 }
 
 function errorHandler(error) {
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
 
-    var bind = typeof port === 'string'
-        ? 'Pipe ' + port
-        : 'Port ' + port;
+  const bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
 
-    // handle specific listen errors with friendly messages
-    switch (error.code) {
-        case 'EACCES':
-            console.error(bind + ' requires elevated privileges');
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.error(bind + ' is already in use');
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
+  // handle specific listen errors with friendly messages
+  // noinspection FallThroughInSwitchStatementJS
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+    default:
+      throw error;
+  }
 }
