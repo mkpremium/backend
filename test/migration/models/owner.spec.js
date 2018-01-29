4bca5bda3fd3e45008bf4a141af748f93d992608
Julian reyes Escrigas <julian.reyes.escrigas@gmail.com>
2018-01-29 08:31:07 -0500

    added testing over more completed CSV

--- test/migration/models/owner.spec.js
+++ test/migration/models/owner.spec.js
@@ -5,6 +5,7 @@ import migrateFromCsv, {combineDuplicatesDocumentNumber} from "../../../src/migr
 import {csvToJson} from "../../../src/migration/lib";
 
 const filename = join(__dirname, '../../fixtures/sample_owners.csv');
+const csv = join(__dirname, '../../../csv/PROPIETARIOS.csv');
 
 describe('migration.models', () => {
   describe('owner', () => {
@@ -47,5 +48,26 @@ describe('migration.models', () => {
         .filter( o => o._documentType === 'owner')
         .should.have.length(26);
     });
+
+    it('migrateFromCsv() PROPIETARIOS.csv @slow', async () => {
+      const migratedData = [];
+      const spy = sinon.spy();
+
+      const processFunc = data => {
+        spy();
+
+        const {person, owner} = migrateFromCsv(data);
+        migratedData.push(person);
+        migratedData.push(owner);
+      };
+
+      await csvToJson(csv, processFunc);
+
+      spy.should.have.been.callCount(6293);
+      migratedData.should.have.length(6293 * 2);
+
+      const combinedData = combineDuplicatesDocumentNumber(migratedData);
+      combinedData.should.have.length(12280);
+    }).timeout(0);
   });
 });
