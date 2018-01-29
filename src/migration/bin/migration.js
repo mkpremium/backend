1d7646d3d6b492e2b27f5536301dbb387bd18f0d
Julian reyes Escrigas <julian.reyes.escrigas@gmail.com>
2018-01-29 08:32:11 -0500

    added migration logic for owner records

--- src/migration/bin/migration.js
+++ src/migration/bin/migration.js
@@ -1,8 +1,13 @@
 import minimist from 'minimist';
+import debug from 'debug';
+import Promise from 'bluebird';
 
 import {csvToJson} from '../lib';
 import models from '../models';
+import {combineDuplicatesDocumentNumber} from '../models/owner';
+import couchbase from '../../db/couchbase';
 
+const debugBin = debug('app:bin:migration');
 const options = {
   default: {},
   string: [
@@ -15,17 +20,51 @@ const options = {
   }
 };
 const {model, file} = minimist(process.argv.slice(2), options);
+const {bucket} = couchbase({middleware: false});
+
+let migratedData = [];
+let processedData = [];
 
 async function importCsv(name, filename) {
   if (typeof models[name] !== 'function') {
     throw new Error(`Model ${name}.migrateFromCsv() not found nor correctly exported`);
   }
+  debugBin('importing', filename, 'into', model);
   await csvToJson(filename, processFunc);
+  debugBin('readed', migratedData.length, 'records');
+  postImport();
+  debugBin(processedData.length, 'after post-processing');
+  debugBin('importing to db', processedData.length, 'records');
+  await importToDb();
+}
+
+function postImport() {
+  switch (model) {
+    case 'owner':
+      processedData = combineDuplicatesDocumentNumber(migratedData);
+      break;
+    default:
+      processedData = migratedData;
+      break;
+  }
+}
+
+async function importToDb() {
+  return Promise
+    .mapSeries(processedData, migratedRecord => bucket.upsertToDb(migratedRecord.id, migratedRecord));
 }
 
 function processFunc(data, row) {
   try {
-    models[model](data);
+    switch (model) {
+      case 'owner':
+        const {person, owner} = models[model](data);
+        migratedData.push(person);
+        migratedData.push(owner);
+        break;
+      default:
+        migratedData.push(models[model](data));
+    }
   } catch (e) {
     console.error(e.message, 'at', row, data);
     throw e;
@@ -33,4 +72,8 @@ function processFunc(data, row) {
 }
 
 importCsv(model, file)
-  .catch(console.error.bind(console));
+  .then(() => process.exit(0))
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
