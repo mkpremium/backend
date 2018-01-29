6fdb0fd36f4d17268a27938fa84004724267498f
Julian reyes Escrigas <julian.reyes.escrigas@gmail.com>
2018-01-29 06:07:17 -0500

    added migration command

---
+++ src/migration/bin/migration.js
@@ -0,0 +1,37 @@
+import minimist from 'minimist';
+
+import {csvToJson} from '../lib';
+import models from '../models';
+
+const options = {
+  default: {},
+  string: [
+    'model',
+    'file'
+  ],
+  alias: {
+    f: 'file',
+    m: 'model'
+  }
+};
+const {model, file} = minimist(process.argv.slice(2), options);
+
+async function importCsv(name, filename) {
+  if (typeof models[name] !== 'function') {
+    throw new Error(`Model ${name}.migrateFromCsv() not found nor correctly exported`);
+  }
+  await csvToJson(filename, processFunc);
+}
+
+function processFunc(data, row) {
+  try {
+    models[model](data);
+  } catch (e) {
+    console.error(e.message, 'at', row, data);
+    throw e;
+  }
+}
+
+importCsv(model, file)
+  .then(console.log.bind(console))
+  .catch(console.error.bind(console));
