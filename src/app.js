46ac034a234d928a00ad25f739bea00d6988c844
Julian reyes Escrigas <julian.reyes.escrigas@gmail.com>
2018-01-30 13:29:03 -0500

    enable swagger to the whole project

--- src/app.js
+++ src/app.js
@@ -4,11 +4,12 @@ import fileUpload from 'express-fileupload';
 import morgan from 'morgan';
 import cors from 'cors';
 import couchbase from './db/couchbase';
-import jwt from './middleware/jwt';
-import numintec from './numintec';
+// import jwt from './middleware/jwt';
+// import numintec from './numintec';
 import operator from './operator';
+import swagger from './swagger';
 
-import migration from './migration';
+// import migration from './migration';
 
 const app = express();
 
@@ -17,10 +18,11 @@ app.use(bodyParser.json());
 app.use(fileUpload());
 app.use(morgan('dev'));
 app.use(cors());
-app.use(jwt());
+app.use(swagger());
+// app.use(jwt());
 app.use(couchbase());
-app.use(numintec());
-app.use(migration());
+// app.use(numintec());
+// app.use(migration());
 app.use(operator());
 
 export default app;
