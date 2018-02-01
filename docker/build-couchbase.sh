3f5db8bdef3f9acb9d9175435b9155d65df33150
Julian reyes Escrigas <julian.reyes.escrigas@gmail.com>
2018-02-01 13:31:34 -0500

    minor change on docker

--- docker/build-couchbase.sh
+++ docker/build-couchbase.sh
@@ -2,6 +2,6 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-docker build \
-  -t couchbase:bitdistrict \
-  ${DIR}/couchbase
+docker build -t couchbase:bitdistrict ${DIR}/couchbase
+docker tag couchbase:bitdistrict rkmax/couchbase:bitdistrict
+docker push rkmax/couchbase:bitdistrict
