6bac504d2b6baf9ccbd3e2566ea3791d1ac33681
Julian reyes Escrigas <julian.reyes.escrigas@gmail.com>
2018-02-01 16:14:01 -0500

    improvements on docker

--- docker/couchbase/init.sh
+++ docker/couchbase/init.sh
@@ -25,25 +25,25 @@ wait_for_start \
 
 echo "Configuring cluster"
 couchbase-cli \
-  cluster-init \
-  -c 127.0.0.1 -u ${COUCHBASE_USER} -p ${COUCHBASE_PASS} \
+  cluster-init -c 127.0.0.1 \
   --cluster-username=${COUCHBASE_USER} \
   --cluster-password=${COUCHBASE_PASS} \
   --cluster-port=${COUCHBASE_PORT} \
   --cluster-ramsize=${COUCHBASE_CLUSTER_RAM_SIZE} \
-  --services=data,index,query
+  --services=data,index,query,fts
 
 echo "Configuring bucket ${COUCHBASE_BUCKET}"
 couchbase-cli \
-  bucket-create \
+  bucket-create -c 127.0.0.1 \
+  --username=${COUCHBASE_USER} \
+  --password=${COUCHBASE_PASS} \
   --bucket=${COUCHBASE_BUCKET} \
-  -c 127.0.0.1 -u ${COUCHBASE_USER} -p ${COUCHBASE_PASS} \
   --bucket-type=${COUCHBASE_BUCKET_TYPE} \
   --bucket-ramsize=${COUCHBASE_BUCKET_RAM_SIZE} \
   --wait
 
 echo "Create bucket ${COUCHBASE_BUCKET} primary index"
-cbq \
+wait_for_start cbq \
   -c 127.0.0.1 -u ${COUCHBASE_USER} -p ${COUCHBASE_PASS} \
   --script "CREATE PRIMARY INDEX \`${COUCHBASE_BUCKET}_primary\` ON \`${COUCHBASE_BUCKET}\`"
 
--- docker/run-couchbase.sh
+++ docker/run-couchbase.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 start() {
   docker run -d \
     --name couchtest \
     -p 8090-8099:8090-8099 -p 9110:9110 -p 11210:11210 -p 18091:18091 -p 18092:18092 \
-    -t couchbase:latest
+    -t couchbase:bitdistrict
 }
 
 stop() {
