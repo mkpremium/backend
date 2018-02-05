0fec6b169dc5ca2d2fe98ea1fe6997285f9c8702
Julian reyes Escrigas <julian.reyes.escrigas@gmail.com>
2018-02-05 05:29:16 -0500

    print just 500 errors

--- src/lib/error-handler.js
+++ src/lib/error-handler.js
@@ -18,14 +18,12 @@ function appErrorHandler(err, req, res, next) {
     err.message = err.message.replace('[tcomb] ', '');
   }
 
-  const throwErrorOnConsole = process.env.NODE_ENV !== 'test' || err.code === 500;
-
   // error from couchbase are outside HTTP range
   if (err.code < 400 || err.code > 599) {
     err.code = 500;
   }
 
-  if (throwErrorOnConsole) {
+  if (err.code === 500) {
     console.error(err);
   }
 
