1b863cbf0dc953a41783831f22d1756bd030435f
Julian reyes Escrigas <julian.reyes.escrigas@gmail.com>
2018-02-22 05:35:33 -0500

    ensure worksheet only appear once on each queue

--- src/worksheet/models/queue.js
+++ src/worksheet/models/queue.js
@@ -62,10 +62,13 @@ export class WorksheetQueueRepository extends WorksheetQueue {
 
   async addWorksheet(queue, worksheet) {
     const itemRepo = new QueueItemRepository();
-    if (worksheet.queue) {
-      throw newHttpError(409, `Worksheet ${worksheet.id} se encuentra en otra cola (${worksheet.queue})`);
+    const worksheetRepo = new WorksheetRepository();
+
+    if (worksheet.queueId) {
+      throw newHttpError(409, `Worksheet ${worksheet.id} se encuentra en otra cola (${worksheet.queueId})`);
     }
 
+    await worksheetRepo.save(t.update(worksheet, {queueId: {$set: queue.id}}));
     return itemRepo.save({worksheetId: worksheet.id});
   }
 
