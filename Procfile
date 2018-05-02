b86bbf71e45140f0cb2af82ed50cd7b054df28e7
Julian reyes Escrigas <julian.reyes.escrigas@gmail.com>
2018-05-02 05:26:15 -0500

    remove no operational code

--- Procfile
+++ Procfile
@@ -1,8 +1,6 @@
-# Temporary start just for migration
-# later we can restore the api: entry from here to start script
-
 socket: nodemon bin/socket.js --exec babel-node
 api: nodemon bin/www.js --exec babel-node
-cross_worker: nodemon --exec babel-node src/migration/workers/cross-csv.js
-seed_worker: nodemon --exec babel-node src/migration/workers/seed.js
-gearman: docker run -p 4730:4730 --rm -i artefactual/gearmand
+cron: nodemon bin/cronjobs.js --exec babel-node
+# cross_worker: nodemon --exec babel-node src/migration/workers/cross-csv.js
+# seed_worker: nodemon --exec babel-node src/migration/workers/seed.js
+# gearman: docker run -p 4730:4730 --rm -i artefactual/gearmand
--- bin/cronjobs.js
+++ bin/cronjobs.js
@@ -1,60 +1,10 @@
-import http from 'http';
-import express from 'express';
-import morgan from 'morgan';
-import debug from 'debug';
-
-import {cronjobsPort} from '../config';
-import appErrorHandler from '../src/lib/error-handler';
 import couchbase from '../src/db/couchbase';
 
 import scheduledEventCronJob from '../src/scheduled-events/cron';
 import scheduledTasksCronJob from '../src/firebase/cron';
 
-const app = express();
-const server = http.createServer(app);
-const cronjobsDebug = debug('app:cron');
-
-server.listen(cronjobsPort);
-server.on('error', errorHandler);
-server.on('listen', listenHandler);
-
-function listenHandler() {
-  const addr = server.address();
-  const bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  cronjobsDebug('Listening on ' + bind);
-}
-
-function errorHandler(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  const bind = typeof port === 'string'
-    ? 'Pipe ' + cronjobsPort
-    : 'Port ' + cronjobsPort;
-
-  // handle specific listen errors with friendly messages
-  // noinspection FallThroughInSwitchStatementJS
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-    default:
-      throw error;
-  }
-}
-
-if (process.env.NODE_ENV !== 'test') {
-  app.use(morgan('dev'));
-}
+const app = {locals: {}};
 
 couchbase(app);
 scheduledEventCronJob.start();
 scheduledTasksCronJob.start();
-
-app.use(appErrorHandler);
