efe532fc709301cf90c2a0a96f9386740f59f4f9
Julian reyes Escrigas <julian.reyes.escrigas@gmail.com>
2018-06-05 06:38:35 -0500

    fix: set buy=false when bank city data doesn't exist

--- src/banks/lib/services.js
+++ src/banks/lib/services.js
@@ -97,7 +97,7 @@ export async function retrievePricesAndLocationInfo(cadastreReference) {
   const buildingInfo = await cadastreAddressService(cadastreReference);
   const bankCityData = await cityData(buildingInfo.address.city);
   const priceSell = calculatePriceSell(pricing, buildingInfo);
-  const buy = !!bankCityData;
+  const buy = typeof bankCityData.priceCity !== 'undefined';
   return Object.assign({}, pricing, buildingInfo, bankCityData, {
     priceSell,
     location,
