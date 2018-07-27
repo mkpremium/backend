9c6b171c0058c43169ab674e8b9f6491f8fe2202
Julian reyes Escrigas <julian.reyes.escrigas@gmail.com>
2018-07-27 13:57:29 -0500

    updated csv

--- migrations/query_csv.sh
+++ migrations/query_csv.sh
@@ -3,17 +3,17 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 llamadas_propietarios() {
-  local llamadas="/home/rkmax/Development/BIX/bitdistrict_backend_A0/csv/LLAMADAS.csv"
-  local propietarios="/home/rkmax/Development/BIX/bitdistrict_backend_A0/csv/PROPIETARIOS.csv"
-  local edificios="/home/rkmax/Development/BIX/bitdistrict_backend_A0/csv/EDIFICIOS.csv"
-  q -H -d ";" "$(cat <<SQL
+  local llamadas="//home/rkmax/Development/BIX/mkpremium-backend/csv/LLAMADAS.csv"
+  local propietarios="/home/rkmax/Development/BIX/mkpremium-backend/csv/PROPIETARIOS.csv"
+  local edificios="/home/rkmax/Development/BIX/mkpremium-backend/csv/EDIFICIOS.csv"
+  q -O -H -d ";" "$(cat <<SQL
   SELECT
-    llamadas.id_chiamatafornitore,
+    llamadas.id_chiamatafornitore as id_chiamatafornitore,
     llamadas.Id_Catastro,
     propietarios.Id_Fornitore,
     edificios.ID,
     propietarios.Verificato,
-    llamadas.proprietari,
+    llamadas.proprietari as proprietari,
     propietarios.RagioneSociale,
     propietarios.CodFis,
     propietarios.ParIva,
@@ -42,14 +42,9 @@ llamadas_propietarios() {
       ON (propietarios.Id_Catastro = llamadas.Id_Catastro)
     JOIN ${edificios} edificios
       ON (llamadas.proprietari = edificios.PROPRIETARI)
-  WHERE llamadas.visitare = 1
-  COLLATE NOCASE
 SQL
 )"
 }
 
-#file=${DIR}/../csv/cross_table.csv
-#echo 'id_chiamatafornitore;Id_Catastro;Id_Fornitore;ID;Verificato;proprietari;RagioneSociale;CodFis;ParIva' > ${file}
-#llamadas_propietarios | tee -a ${file}
-llamadas_propietarios
-
+file=${DIR}/../csv/cross_table.csv
+llamadas_propietarios > ${file}
