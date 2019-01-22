3c9fa87346e6b67e3c2ddd9acbdb9cfa4daf343c
Julian reyes Escrigas <julian.reyes.escrigas@gmail.com>
2019-01-22 09:15:30 -0500

    chore: disable web hook logger for testing

--- src/webhooks/index.js
+++ src/webhooks/index.js
@@ -1,10 +1,11 @@
 import {webhookCallsRouter} from '../calls/routes';
 import morganBody from 'morgan-body';
+import {isTest} from '../../config';
 
 export default (app) => {
   morganBody(app, {
     skip: (req, res) => {
-      return !req.originalUrl.includes('/webhook');
+      return isTest() || !req.originalUrl.includes('/webhook');
     }
   });
   app.use('/webhook/calls', webhookCallsRouter);
