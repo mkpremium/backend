390223839f3b282fe42bcf73033a5be035160bb2
Julian reyes Escrigas <julian.reyes.escrigas@gmail.com>
2019-01-30 15:56:04 -0500

    chore: some cli are omitted for build process

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,7 +12,7 @@ babel ./ \
   --out-dir ./build \
   -s -q \
   --copy-files \
-  --ignore node_modules,development,docker,iml,lock,data
+  --ignore node_modules,development,docker,iml,lock
 
 # Node setup
 cp ./.nvmrc ./build/
