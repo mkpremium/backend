49bd36a8ecd63a6fbc9aecfa7a6cdb342c5d3ae7
Julian reyes Escrigas <julian.reyes.escrigas@gmail.com>
2019-01-30 16:06:09 -0500

    chore: helper for migrations

---
+++ scripts/run-migration.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+_current=$(owd)
+
+cd /home/ubuntu/apps/mkpremium
+nvm use
+
+NODE_CMD="node -r dotenv/config"
+DATA_DIR=~/picked_data
+
+${NODE_CMD} cli/cli-migrate-worksheets.js --clean ${DATA_DIR}/CSV/
+${NODE_CMD} cli/cli-owners-verify.js ${DATA_DIR}/CSV/PROPIETARIOS.csv
+${NODE_CMD} cli/cli-building-states.js ${DATA_DIR}/CSV/BUILDING_STATES/
+${NODE_CMD} cli/cli-business-states.js ${DATA_DIR}/CSV/BUSINESS_STATES/
+${NODE_CMD} cli/cli-building-notes.js ${DATA_DIR}/CSV/BUILDING_NOTES.csv
+${NODE_CMD} cli/cli-building-metadata.js ${DATA_DIR}/
+
+cd ${_current}
