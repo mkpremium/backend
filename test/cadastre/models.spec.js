8ac7dc8c9baef23478ba7933bddf77d327774379
Julian reyes Escrigas <julian.reyes.escrigas@gmail.com>
2019-05-08 10:54:12 -0500

    basic testing for models

---
+++ test/cadastre/models.spec.js
@@ -0,0 +1,23 @@
+import {CadastreRepository} from '../../src/cadastre/models';
+import {deleteAll} from '../common';
+
+describe('cadastre/model', () => {
+  describe('getProvinces', () => {
+    before(async() => deleteAll());
+    it('fetch fresh data from cadastre', async() => {
+      const repo = new CadastreRepository();
+      const provinces = await repo.getProvinces();
+      provinces.should.be.an('array');
+      provinces.length.should.not.equal(0);
+    });
+
+    it('fetch cached data from cadastre', async function() {
+      // force to finalice before be able to do request
+      this.timeout(1000);
+      const repo = new CadastreRepository();
+      const provinces = await repo.getProvinces();
+      provinces.should.be.an('array');
+      provinces.length.should.not.equal(0);
+    });
+  });
+});
