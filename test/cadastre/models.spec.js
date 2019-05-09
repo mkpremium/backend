62e6b2ee46d0266f4516f79f86994e4a71e8c742
Julian reyes Escrigas <julian.reyes.escrigas@gmail.com>
2019-05-09 17:22:31 -0500

    add getCities dummy testing

--- test/cadastre/models.spec.js
+++ test/cadastre/models.spec.js
@@ -3,7 +3,7 @@ import {deleteAll} from '../common';
 
 describe('cadastre/model', () => {
   describe('getProvinces', () => {
-    before(async() => deleteAll());
+    // before(async() => deleteAll());
     it('fetch fresh data from cadastre', async() => {
       const repo = new CadastreRepository();
       const provinces = await repo.getProvinces();
@@ -18,6 +18,24 @@ describe('cadastre/model', () => {
       const provinces = await repo.getProvinces();
       provinces.should.be.an('array');
       provinces.length.should.not.equal(0);
+    });
+  });
+
+  describe('getCities', () => {
+    it('fetch fresh data from cadastre', async() => {
+      const repo = new CadastreRepository();
+      const results = await repo.getCitiesByProvince('BARCELONA');
+      results.should.be.an('array');
+      results.length.should.not.equal(0);
+    });
+
+    it('fetch cached data from cadastre', async function() {
+      // force to finalice before be able to do request
+      this.timeout(1000);
+      const repo = new CadastreRepository();
+      const results = await repo.getCitiesByProvince('BARCELONA');
+      results.should.be.an('array');
+      results.length.should.not.equal(0);
     });
   });
 });
