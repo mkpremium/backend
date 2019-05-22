c04ec8aabd3a608a854c7c5c6edaed469848ae57
Julian reyes Escrigas <julian.reyes.escrigas@gmail.com>
2019-05-22 08:47:52 -0500

    full address migration

---
+++ test/cli-building-format-fulladdress.spec.js
@@ -0,0 +1,46 @@
+import {deleteAll} from '../test/common';
+import {createBuildingWithWorksheet} from '../src/worksheet/building/model';
+import {formatAllBuildingAddresses} from '../cli/cli-building-format-fulladdress';
+import {BuildingRepository} from '../src/building/models';
+import {WorksheetRepository} from '../src/worksheet/models/worksheet';
+
+describe('formatByBuildingAddress', () => {
+  const building = {
+    location: {
+      lat: 40.412253,
+      lng: -3.705473
+    },
+    address: {
+      type: 'Transversal',
+      street: '44',
+      number: 10082,
+      fullAddress: 'Transversal 44 # 100 - 82',
+      postalCode: {
+        number: 80001,
+        verified: true
+      },
+      city: 'Barranquilla',
+      province: 'Atlantico',
+      zone: 'Norte',
+      neighborhood: 'Miramar'
+    },
+    cadastre: {
+      reference: 'aaaaabbbbcvccc',
+      address: 'TV 44 # 100 - 82'
+    }
+  };
+
+  before(async() => {
+    await deleteAll();
+    await createBuildingWithWorksheet(building);
+  });
+
+  it('able to set placeId on building without', async() => {
+    await formatAllBuildingAddresses();
+    const expected = 'Transversal 44 10082, Barranquilla';
+    const building = await BuildingRepository.findByCadastreReference('aaaaabbbbcvccc');
+    const worksheet = await WorksheetRepository.findByBuilding(building.id);
+    building.address.fullAddress.should.equal(expected);
+    worksheet.buildingAddress.fullAddress.should.equal(expected);
+  });
+});
