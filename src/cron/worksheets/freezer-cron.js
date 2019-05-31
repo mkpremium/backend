893ff8abd8be557670e4d9501c90270ba8138007
Julian reyes Escrigas <julian.reyes.escrigas@gmail.com>
2019-05-31 16:29:59 -0500

    uses the preference to control de cronjob

--- src/cron/worksheets/freezer-cron.js
+++ src/cron/worksheets/freezer-cron.js
@@ -2,14 +2,18 @@ import {CronJob} from 'cron';
 import debug from 'debug';
 import {utc} from '../../lib/date';
 import {moveWorksheetOutOfFreezer} from '../../business/worksheets/freezer';
+import {SystemPreferencesRepository} from '../../system-preferences/models';
 
 const cronDebug = debug('app:cron:worksheets:freezer');
 const timeZone = 'UTC';
 const cronTime = '*/5 * * * *'; // every 5 minutes
 
 async function onTick() {
-  cronDebug(`Executing freezer cron at ${utc().startOf('minute').toISOString()}`);
-  await moveWorksheetOutOfFreezer();
+  const pref = await SystemPreferencesRepository.getPreferences();
+  if (pref.freezer.enable) {
+    cronDebug(`Executing freezer cron at ${utc().startOf('minute').toISOString()}`);
+    await moveWorksheetOutOfFreezer();
+  }
 }
 
 export default new CronJob({
