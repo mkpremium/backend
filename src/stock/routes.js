4dd64e19239f07c5866e9e7e27403a4e5d2cdfd2
Alejandro <alexalejandroem@gmail.com>
2019-07-29 16:06:31 +0200

    Added swagger documentation

--- src/stock/routes.js
+++ src/stock/routes.js
@@ -1,5 +1,6 @@
 import {Router} from 'express';
 import {
+  cancelSellStockController,
   closeSellStockController,
   createPurchaseStockController,
   getProfitsRakningController,
@@ -68,6 +69,60 @@ const router = Router();
  *           $ref: "#/definitions/Error"
  */
 router.post('/purchase', createPurchaseStockController);
+
+/**
+ * @swagger
+ * /stock/sell:
+ *   post:
+ *     tags: [Stock]
+ *     summary: Actualiza un stock en estado purchased a sell
+ *     security:
+ *       - operator: []
+ *       - admin: []
+ *       - manager: []
+ *       - comercial: []
+ *     consumes:
+ *       - "application/json"
+ *     produces:
+ *       - "application/json"
+ *     parameters:
+ *       - name: buildingId
+ *         type: string
+ *         in: body
+ *         required: true
+ *       - name: reservationAmount
+ *         type: number
+ *         in: body
+ *         required: true
+ *       - name: reservationDate
+ *         type: string
+ *         in: body
+ *         format: YYYY-MM-DDTHH:mm:ss.sssZ
+ *         required: true
+ *       - name: transactionAmount
+ *         type: number
+ *         in: body
+ *         required: true
+ *       - name: transactionDate
+ *         type: string
+ *         in: body
+ *         format: YYYY-MM-DDTHH:mm:ss.sssZ
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: Operación exitosa
+ *         schema:
+ *           $ref: "#/definitions/Stock"
+ *       400:
+ *         description: Solicitud mal formada
+ *         schema:
+ *           $ref: "#/definitions/Error"
+ *       401:
+ *         description: Credenciales inválidos o cuenta deshabilitada
+ *         schema:
+ *           $ref: "#/definitions/Error"
+ */
+router.post('/sell', sellPurchasedStockController);
 
 /**
  * @swagger
