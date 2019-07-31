ad036ad3d7f3e73b404e3d73aba0f1a23f52d389
Alejandro <alexalejandroem@gmail.com>
2019-07-31 20:30:56 +0200

    Added validation for duplicated stock

--- src/stock/application.js
+++ src/stock/application.js
@@ -27,6 +27,12 @@ export async function createPurchaseStock(params = {}, operatorId) {
 
   const stockRepository = new StockRepository();
 
+  const stockExist = stockRepository.findByBuildingIdOrDefault(params.buildingId);
+
+  if (stockExist) {
+    throw new Error('There is an stock related to that building');
+  }
+
   const stock = {
     buildingId: createStockParams.buildingId,
     currentStatus: StockStatuses.PURCHASE,
