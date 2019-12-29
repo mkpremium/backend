ad43f6ff35ea6e74a32a4fb63944524eac92f70f
Jorge Velasco <zen.chen@gmail.com>
2019-12-29 15:20:38 +0100

    Fix Couchbase local initialization

--- docker/couchbase/init-couchbase/init-couchbase.sh
+++ docker/couchbase/init-couchbase/init-couchbase.sh
@@ -2,26 +2,33 @@
 
 set -e
 
+
 COUCHBASE_PORT=${COUCHBASE_PORT:-8091}
 COUCHBASE_USERNAME=${COUCHBASE_USERNAME:-couchbase}
 COUCHBASE_PASSWORD=${COUCHBASE_PASSWORD:-couchbase}
+COUCHBASE_CLUSTER_RAM_SIZE=${COUCHBASE_CLUSTER_RAM_SIZE:-512}
+COUCHBASE_INDEX_RAM_SIZE=${COUCHBASE_INDEX_RAM_SIZE:-256}
 
 COUCHBASE_BUCKET_NAME=${COUCHBASE_BUCKET_NAME:-mkpremium}
-COUCHBASE_BUCKET_RAM_SIZE=${COUCHBASE_BUCKET_RAM_SIZE:-256}
+COUCHBASE_BUCKET_RAM_SIZE=${COUCHBASE_BUCKET_RAM_SIZE:-512}
 
 echo "Initializing cluster"
 /opt/couchbase/bin/couchbase-cli cluster-init \
   --cluster-name local \
   --cluster-username ${COUCHBASE_USERNAME} \
-  --cluster-password ${COUCHBASE_PASSWORD}
+  --cluster-password ${COUCHBASE_PASSWORD} \
+  --cluster-ramsize ${COUCHBASE_CLUSTER_RAM_SIZE} \
+  --cluster-index-ramsize ${COUCHBASE_INDEX_RAM_SIZE} \
+  --index-storage-setting default \
+  --services data,index,query
 
 echo "Creating bucket ${COUCHBASE_BUCKET_NAME}"
 /opt/couchbase/bin/couchbase-cli bucket-create \
   --bucket ${COUCHBASE_BUCKET_NAME} \
   --bucket-type couchbase \
   --bucket-ramsize ${COUCHBASE_BUCKET_RAM_SIZE} \
-  -c localhost -u couchbase -p couchbase
-
+  -c localhost -u couchbase -p couchbase \
+  --wait
 
 echo "Create bucket ${COUCHBASE_BUCKET_NAME} primary index"
 cbq \
@@ -29,3 +36,5 @@ cbq \
   -u ${COUCHBASE_USERNAME} -p ${COUCHBASE_PASSWORD} \
   --script "CREATE PRIMARY INDEX \`${COUCHBASE_BUCKET_NAME}_primary\` ON \`${COUCHBASE_BUCKET_NAME}\`" \
   -exit-on-error
+
+wait
