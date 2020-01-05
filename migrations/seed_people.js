583228059714fd2a80b73e46862781ed26d937b9
Jorge Velasco <zen.chen@gmail.com>
2020-01-05 14:21:11 +0100

    Pass people files directly instead of all files

--- migrations/seed_people.js
+++ migrations/seed_people.js
@@ -3,24 +3,24 @@ import {readCodigosPostalesMunicipios} from '../csv/codigos_postales_municipios'
 import {MigratePersonModel} from '../src/migration/lib/migrate-person';
 import {defaultFiles} from './defaults';
 
-async function init(files) {
+async function init(people) {
   const app = {
     locals: {
       bucket: await couchbase()
     }
   };
 
-  await processPeople(files, app);
+  await processPeople(people, app);
 }
 
-async function processPeople({people}, app) {
+async function processPeople(people, app) {
   const codes = await readCodigosPostalesMunicipios();
   const peopleMigration = new MigratePersonModel(people, codes, app);
   return peopleMigration.run();
 }
 
 if (require.main === module) {
-  init(defaultFiles)
+  init(defaultFiles.people)
     .then(() => process.exit(0))
     .catch(err => {
       console.error(err);
