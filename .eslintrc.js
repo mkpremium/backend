ec47f73e010e0ffe0d0c86b06cb8bdc03d1f8350
Jorge Velasco <zen.chen@gmail.com>
2020-01-12 16:42:40 +0100

    Move eslint config to js file

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: 'standard',
+  env: {
+    node: true
+  },
+  overrides: [
+    {
+      files: [
+        'test/**/*.js',
+        'test-e2e/**/*.js',
+      ],
+      rules: {
+        'no-unused-expressions': 'off'
+      },
+      env: {
+        mocha: true
+      }
+    }
+  ]
+}
