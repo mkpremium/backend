22ff1b5fa7e316a93f8ee8ae74fc98675d97a6ba
Jorge Velasco <zen.chen@gmail.com>
2020-01-19 09:25:34 +0100

    Rename Sales Agent to Property Manager

---
+++ src/property-manager/PropertyManagerRankingService.js
@@ -0,0 +1,42 @@
+import moment from 'moment-timezone'
+
+export class PropertyManagerRankingService {
+  constructor (propertyManagersRepository, stockRepository, now = () => moment()) {
+    this.propertyManagersRepository = propertyManagersRepository
+    this.stockRepository = stockRepository
+    this.now = now
+  }
+
+  async ranking () {
+    const propertyManagers = await this.propertyManagersRepository.getActivePropertyManagers()
+
+    const now = this.now()
+    const propertyManagerStockPerformanceForCurrentYear = await this.stockRepository.getStatsByPropertyManagerInPeriod(
+      now.clone().startOf('year'), now.clone().endOf('year')
+    )
+
+    return propertyManagers.map((pm) => {
+      const stockPerformance = propertyManagerStockPerformanceForCurrentYear.find(sp => sp.propertyManagerId === pm.id)
+
+      const goal = propertyManagerProfitGoalAmount(pm)
+      const profit = stockPerformance ? stockPerformance.profitAmount : 0
+
+      return {
+        userId: pm.id,
+        userName: pm.name,
+        userCity: pm.city,
+        goal,
+        currentProfit: profit,
+        percentageGoal: profit / goal,
+        awards: []
+      }
+    }).sort((a, b) => b.percentageGoal - a.percentageGoal)
+      .map((elem, idx) => ({...elem, rank: idx + 1}))
+  }
+}
+
+const propertyManagerProfitGoalAmount = pm => {
+  if (pm.profitGoal === undefined) return pm.city === 'Lisboa' ? 700000 : 500000
+
+  return pm.profitGoal.amount
+}
---
+++ test/property-manager/PropertyManagerRankingService.spec.js
@@ -0,0 +1,170 @@
+import { PropertyManagerRankingService } from '../../src/property-manager/PropertyManagerRankingService'
+import { Promise } from 'bluebird'
+import { expect } from 'chai'
+import sinon from 'sinon'
+import moment from 'moment-timezone'
+
+const matchingMoment = (momentToMatch) => sinon.match(
+  (actual) => actual.toString() === momentToMatch.toString()
+)
+
+describe.only('PropertyManagerRankingService', () => {
+  const now = () => moment('2020-01-12') // January 12, 2020
+  const firstMomentCurrentYear = moment('2020-01-01')
+  const lastMomentCurrentYear = moment('2020-12-31').endOf('day')
+
+  const barcelonaPropertyManagerWithoutProfitGoal = {
+    id: 'property-manager-user-id',
+    name: 'Property Manager Full Name',
+    city: 'Barcelona'
+  }
+
+  const propertyManagersRepository = {
+    getActivePropertyManagers: null
+  }
+  const stockRepository = {
+    getStatsByPropertyManagerInPeriod: null
+  }
+
+  let rankingService
+
+  beforeEach(() => {
+    propertyManagersRepository.getActivePropertyManagers = sinon.stub().returns(Promise.resolve([]))
+    stockRepository.getStatsByPropertyManagerInPeriod = sinon.stub().returns(Promise.resolve([]))
+
+    rankingService = new PropertyManagerRankingService(
+      propertyManagersRepository,
+      stockRepository,
+      now
+    )
+  })
+
+  it('gets all active property managers from users repository', async () => {
+    await rankingService.ranking()
+
+    expect(propertyManagersRepository.getActivePropertyManagers).to.have.been.calledWith()
+  })
+
+  it('gets closed stock grouped by property manager', async () => {
+    await rankingService.ranking()
+
+    expect(stockRepository.getStatsByPropertyManagerInPeriod).to.have.been
+      .calledWith(matchingMoment(firstMomentCurrentYear), matchingMoment(lastMomentCurrentYear))
+  })
+
+  it('returns property manager user information', async () => {
+    propertyManagersRepository.getActivePropertyManagers.returns(Promise.resolve([barcelonaPropertyManagerWithoutProfitGoal]))
+
+    const ranking = await rankingService.ranking()
+
+    expect(ranking[0]).to.include({
+      userId: barcelonaPropertyManagerWithoutProfitGoal.id,
+      userName: barcelonaPropertyManagerWithoutProfitGoal.name,
+      userCity: barcelonaPropertyManagerWithoutProfitGoal.city
+    })
+  })
+
+  describe('property manager profit goal', () => {
+    const generalProfitGoal = 500000
+    const lisbonProfitGoal = 700000
+
+    it(
+      'applies general default goal for property manager without goal in a city different than Lisbon',
+      async () => {
+        propertyManagersRepository.getActivePropertyManagers.returns(Promise.resolve([barcelonaPropertyManagerWithoutProfitGoal]))
+        const ranking = await rankingService.ranking()
+
+        expect(ranking[0].goal).to.be.equal(generalProfitGoal)
+      }
+    )
+
+    it('applies Lisbon default goal for property manager without goal in Lisbon', async () => {
+      const lisbonPropertyManagerWithoutProfitGoal = {...barcelonaPropertyManagerWithoutProfitGoal, city: 'Lisboa'}
+      propertyManagersRepository.getActivePropertyManagers.returns(Promise.resolve([lisbonPropertyManagerWithoutProfitGoal]))
+
+      const ranking = await rankingService.ranking()
+
+      expect(ranking[0].goal).to.be.equal(lisbonProfitGoal)
+    })
+
+    it('applies property manager profit goal', async () => {
+      const propertyManagerProfitGoal = {amount: 100000}
+      const propertyManagerWithProfitGoal = {...barcelonaPropertyManagerWithoutProfitGoal, profitGoal: propertyManagerProfitGoal}
+      propertyManagersRepository.getActivePropertyManagers.returns(Promise.resolve([propertyManagerWithProfitGoal]))
+
+      const ranking = await rankingService.ranking()
+
+      expect(ranking[0].goal).to.be.equal(propertyManagerProfitGoal.amount)
+    })
+  })
+
+  describe('profit calculation', () => {
+    it('gets profits from property manager stocks total gains', async () => {
+      propertyManagersRepository.getActivePropertyManagers.returns(Promise.resolve([barcelonaPropertyManagerWithoutProfitGoal]))
+      stockRepository.getStatsByPropertyManagerInPeriod.returns(Promise.resolve([
+        {
+          propertyManagerId: barcelonaPropertyManagerWithoutProfitGoal.id,
+          profitAmount: 50000
+        }
+      ]))
+
+      const ranking = await rankingService.ranking()
+
+      expect(ranking[0].currentProfit).to.be.equal(50000)
+    })
+  })
+
+  describe('ranking calculation', () => {
+    it('calculates percentageGoal based on profit calculation and property manager profit goal', async () => {
+      propertyManagersRepository.getActivePropertyManagers.returns(Promise.resolve([
+        {...barcelonaPropertyManagerWithoutProfitGoal, profitGoal: {amount: 120}}
+      ]))
+      stockRepository.getStatsByPropertyManagerInPeriod.returns(Promise.resolve([
+        {
+          propertyManagerId: barcelonaPropertyManagerWithoutProfitGoal.id,
+          profitAmount: 60
+        }
+      ]))
+
+      const ranking = await rankingService.ranking()
+
+      expect(ranking[0].percentageGoal).to.be.closeTo(0.5, 0.001)
+    })
+
+    it('calculates ranking based on achieved percentage goal', async () => {
+      const propertyManagerWith20PercentAchievedProfitGoal = {
+        ...barcelonaPropertyManagerWithoutProfitGoal,
+        id: '20%',
+        profitGoal: {amount: 120}
+      }
+      const propertyManagerWith50PercentAchievedProfitGoal = {
+        ...barcelonaPropertyManagerWithoutProfitGoal,
+        id: '50%',
+        profitGoal: {amount: 120}
+      }
+
+      propertyManagersRepository.getActivePropertyManagers.returns(Promise.resolve([
+        propertyManagerWith20PercentAchievedProfitGoal, propertyManagerWith50PercentAchievedProfitGoal
+      ]))
+
+      stockRepository.getStatsByPropertyManagerInPeriod.returns(Promise.resolve([
+        {
+          propertyManagerId: propertyManagerWith20PercentAchievedProfitGoal.id,
+          profitAmount: 24
+        },
+        {
+          propertyManagerId: propertyManagerWith50PercentAchievedProfitGoal.id,
+          profitAmount: 60
+        }
+      ]))
+
+      const ranking = await rankingService.ranking()
+
+      expect(ranking[0].rank).to.be.deep.equal(1)
+      expect(ranking[0].percentageGoal).to.be.deep.equal(0.5)
+
+      expect(ranking[1].rank).to.be.deep.equal(2)
+      expect(ranking[1].percentageGoal).to.be.deep.equal(0.2)
+    })
+  })
+})
