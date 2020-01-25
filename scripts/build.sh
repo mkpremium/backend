3828fa46579ac54bbcef65071c2b5f3365dd2136
Jorge Velasco <zen.chen@gmail.com>
2020-01-25 16:45:11 +0100

    Remove duplication in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,22 +2,6 @@
 
 set -ex
 
-# Clean current build
-rm -rf build/
-
-# Build assets
-npx babel ./ \
-  --out-dir ./build \
-  -s \
-  --copy-files \
-  --ignore node_modules,docker,test,test-e2e
-
-# Build the package.json
-scripts/package.js
-
+scripts/build-without-dependencies.sh
 cd build
-
-git rev-parse HEAD > GIT_COMMIT
-npm i
-
-rm -r .idea .circleci conf docker docs test test-e2e
+npm install
