be8237dd762c1ad455fddc9065026c7f42b8ac03
Jorge Velasco <zen.chen@gmail.com>
2020-03-28 10:54:58 +0100

    Add failing acceptance test for favorites functionality

---
+++ test-e2e/property-agent-favourites-buildings.spec.js
@@ -0,0 +1,25 @@
+import { expect } from 'chai'
+import { operatorCreateBusiness } from '../test/common'
+import { authenticatedGet, authenticatedPost, initApplication } from './rest-api-helper'
+
+describe('Property agent favourite buildings', () => {
+  it('stores property agent favourite buildings', async () => {
+    const app = await initApplication()
+
+    const businessUser = await operatorCreateBusiness()
+
+    await authenticatedPost('/favorites', businessUser, app, {
+      buildingId: 'building-id'
+    }).then(response => expect(response.status).to.be.equal(201))
+
+    return authenticatedGet('/me', businessUser, app)
+      .then(response => {
+        expect(response.status).to.be.equal(200)
+        expect(response.body).to.be.deep.contains({
+          favouriteBuildings: [
+            'building-id'
+          ]
+        })
+      })
+  })
+})
