59e54aa425e3981e2f747ed31a04cc16f35fa8ef
Jorge Velasco <zen.chen@gmail.com>
2020-04-13 12:01:44 +0200

    Include purchased stock in building listing

--- src/building/CommercialsBuildingRepository.js
+++ src/building/CommercialsBuildingRepository.js
@@ -1,10 +1,15 @@
 import { N1qlQuery } from 'couchbase'
+import moment from 'moment'
 
 const listBuildingsByIdQuery = `
-SELECT id, metadata
-FROM mkpremium
-WHERE _documentType = 'building'
-AND id IN $1
+SELECT
+    building.id,
+    building.metadata,
+    stock
+FROM mkpremium building
+LEFT JOIN mkpremium stock ON stock.buildingId = building.id AND stock._documentType = 'stock'
+WHERE building._documentType = 'building'
+AND building.id IN $1
 `
 
 export class CommercialsBuildingRepository {
@@ -16,13 +21,23 @@ export class CommercialsBuildingRepository {
     return this.couchbaseAdapter.queryAsync(
       N1qlQuery.fromString(listBuildingsByIdQuery), [ ids ]
     ).then(buildings => buildings.map(
-      ({ id, metadata }) => ({
-        id,
-        metadata: metadata.map(({ mimeType, previewUrl }) => ({
-          mimeType,
-          thumbnailUrl: previewUrl
-        }))
-      })
+      ({ id, metadata, stock }) => {
+        return ({
+          id,
+          metadata: metadata.map(({ mimeType, previewUrl }) => ({
+            mimeType,
+            thumbnailUrl: previewUrl
+          })),
+          stock: {
+            purchase: stock && stock.purchase ? {
+              reservationAmount: stock.purchase.reservationAmount,
+              reservationDate: moment(stock.purchase.reservationDate).unix(),
+              transactionAmount: stock.purchase.transactionAmount,
+              transactionDate: moment(stock.purchase.transactionDate).unix()
+            } : undefined
+          }
+        })
+      }
     ))
   }
 }
--- test-e2e/building/building-listing-endpoint.spec.js
+++ test-e2e/building/building-listing-endpoint.spec.js
@@ -1,7 +1,22 @@
 import { expect } from 'chai'
+import moment from 'moment'
+import { createPurchaseStock } from '../../src/stock/application'
 import { operatorCreateBusiness } from '../../test/common'
 import { createBuilding, createOwner } from '../helper/mother-of-objects'
 import { authenticatedGet, initApplication } from '../helper/rest-api-helper'
+
+const testPurchaseReservationAmount = 50000
+const testPurchaseAmount = 100000
+
+const purchaseBuilding = async (app, {buildingId, propertyAgentId}) => {
+  return createPurchaseStock({
+    buildingId,
+    reservationAmount: testPurchaseReservationAmount,
+    reservationDate: new Date(),
+    transactionAmount: testPurchaseAmount,
+    transactionDate: new Date()
+  }, propertyAgentId)
+}
 
 describe('Building listing endpoint', () => {
   let app, businessUser
@@ -22,6 +37,11 @@ describe('Building listing endpoint', () => {
         previewUrl: 'https://mkpremium-files.s3.eu-west-2.amazonaws.com/preview/ffe6fa34-28bf-4da8-9695-53b7bf421648.jpg'
       }]
     })
+    const building1Purchase = (await purchaseBuilding(app, {
+      buildingId: building1.id,
+      propertyAgentId: businessUser.id
+    })).purchase
+
     const building2 = await createBuilding(app, owner, { id: 'test-building2' })
 
     await authenticatedGet(`/buildings?id=${building1.id}&id=${building2.id}`, businessUser, app)
@@ -35,11 +55,20 @@ describe('Building listing endpoint', () => {
                 mimeType: 'image/jpeg',
                 thumbnailUrl: 'https://mkpremium-files.s3.eu-west-2.amazonaws.com/preview/ffe6fa34-28bf-4da8-9695-53b7bf421648.jpg'
               }
-            ]
+            ],
+            stock: {
+              purchase: {
+                reservationAmount: building1Purchase.reservationAmount,
+                reservationDate: moment(building1Purchase.reservationDate).unix(),
+                transactionAmount: building1Purchase.transactionAmount,
+                transactionDate: moment(building1Purchase.transactionDate).unix()
+              }
+            }
           },
           {
             id: building2.id,
-            metadata: []
+            metadata: [],
+            stock: {}
           }
         ])
       })
