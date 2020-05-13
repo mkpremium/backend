33c88a16fd0a3faf5ed45c6e03b4d706d9293f4b
Jorge Velasco <zen.chen@gmail.com>
2020-05-13 08:09:41 +0200

    Add one shot script.

---
+++ scripts/firebase-to-couchbase-one-shot-sync/sync-featured-contact.js
@@ -0,0 +1,32 @@
+const { resolve } = require('path')
+const { config } = require('dotenv')
+config({ path: resolve(__dirname, '../..', '.env') })
+
+const appMod = require('../../src/app')
+const app = appMod.default
+const { dependenciesPromise } = appMod
+
+dependenciesPromise.then(async () => {
+  const { ownerRepository } = app.locals.dependenciesContainer
+  const ownerContactToSave = require('/tmp/featured-contacts.json')
+  const errors = (await Promise.all(
+    ownerContactToSave.map(async ({ownerId, contact}) => {
+      try {
+        await ownerRepository.setOwnerFeaturedContact(ownerId, contact)
+      } catch (e) {
+        return { error: e, ownerId, contact }
+      }
+    })
+  )).filter(res => res !== undefined)
+
+  if (0 < errors.length) {
+    console.error(JSON.stringify(errors))
+    process.exit(1)
+  }
+
+  console.info("Success!🤸🏾")
+  process.exit(0)
+}).catch(err => {
+  console.error(err)
+  process.exit(1)
+})
