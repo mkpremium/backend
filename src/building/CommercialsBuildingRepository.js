c8d990a2aecc98b523ee1a8759502f3e90327050
Jorge Velasco <zen.chen@gmail.com>
2020-06-01 15:06:56 +0200

    Change way to get all buildings assigned to a sales agent.

--- src/building/CommercialsBuildingRepository.js
+++ src/building/CommercialsBuildingRepository.js
@@ -26,40 +26,12 @@ LEFT NEST mkpremium verifiedOwners ON verifiedOwners.status = "VERIFICADO"
     AND verifiedOwners._documentType = 'owner'
 LEFT NEST mkpremium personOwners ON personOwners.id IN ARRAY o.personId FOR o IN verifiedOwners END
     AND personOwners._documentType = 'person'
+    AND personOwners.active = true
 
 LEFT NEST mkpremium buildingMeetings ON buildingMeetings.event.buildingId = building.id
     AND buildingMeetings._documentType = 'scheduled-event' AND buildingMeetings.type = 'MEETINGS'
 WHERE building._documentType = 'building'
 AND building.id IN $1
-`
-
-const listBuildingsByAssignedPropertyAgentQuery = `
-SELECT
-    building.id,
-    MIN(building.metadata) metadata,
-    MIN(stock) stock,
-    MIN(building.address) address,
-    MIN(building.cadastre.reference) cadastreReference,
-    MIN(building.floorArea) floorArea,
-    MIN(building.location) location,
-    MIN(building.recentProposal.proposal) lastProposal,
-    MIN(building.\`use\`) \`use\`,
-    MIN(owner.business.status) negotiationStatus,
-    MIN(owner.id) ownerId,
-    MIN(person.firstName) ownerFirstName,
-    MIN(person.name) ownerFullName,
-    MIN(person.contacts) contacts,
-    MIN(owner.featuredContact) featuredContact,
-    MAX(buildingMeetings.eventDate) lastMeetingAt
-FROM mkpremium building
-LEFT JOIN mkpremium stock ON stock.buildingId = building.id AND stock._documentType = 'stock'
-INNER JOIN mkpremium owner ON building.ownerId = owner.id AND owner._documentType = 'owner'
-    AND owner.business.meetingWithOperatorId = $1
-LEFT JOIN mkpremium person ON person.id = owner.personId AND person._documentType = 'person'
-LEFT JOIN mkpremium buildingMeetings ON buildingMeetings.event.buildingId = building.id
-    AND buildingMeetings._documentType = 'scheduled-event' AND buildingMeetings.type = 'MEETINGS'
-WHERE building._documentType = 'building' AND building.ownerId IS NOT NULL
-GROUP BY building.id
 `
 
 const listProposalsForBuildingIdQuery = `
@@ -74,6 +46,15 @@ WHERE _documentType = 'building-proposal'
 AND buildingId = $1
 `
 
+const assignedBuildingsIdForAgentQuery = `
+SELECT
+    owner.buildingId
+FROM mkpremium owner
+WHERE owner._documentType = 'owner'
+    AND owner.status = 'VERIFICADO'
+    AND owner.business.meetingWithOperatorId = $1
+`
+
 export class CommercialsBuildingRepository {
   constructor (couchbaseAdapter) {
     this.couchbaseAdapter = couchbaseAdapter
@@ -82,13 +63,12 @@ export class CommercialsBuildingRepository {
   listById (ids) {
     return this.couchbaseAdapter.queryAsync(
       N1qlQuery.fromString(listBuildingsByIdQuery), [ ids ]
-    ).then(this.mapToPropertyAgentBuildingView2())
+    ).then(this.mapToPropertyAgentBuildingView())
   }
 
   listAssignedToPropertyAgentOfId (agentId) {
-    return this.couchbaseAdapter.queryAsync(
-      N1qlQuery.fromString(listBuildingsByAssignedPropertyAgentQuery), [ agentId ]
-    ).then(this.mapToPropertyAgentBuildingView())
+    return this.allAssignedBuildingsId(agentId)
+      .then(buildingsId => this.listById(buildingsId))
   }
 
   listProposalsForBuilding (buildingId) {
@@ -97,81 +77,20 @@ export class CommercialsBuildingRepository {
     )
   }
 
-  mapToPropertyAgentBuildingView () {
-    return buildings => buildings.map(
-      ({
-        id, metadata, stock, lastProposal, cadastreReference, negotiationStatus, address, location, use, floorArea,
-        ownerId, ownerFirstName, ownerFullName, contacts, lastMeetingAt, featuredContact
-      }) => {
-        return ({
-          id,
-          metadata: metadata.map(({ id, mimeType, previewUrl }) => ({
-            id,
-            mimeType,
-            thumbnailUrl: previewUrl
-          })),
-          stock: {
-            purchase: stock && stock.purchase ? {
-              reservationAmount: stock.purchase.reservationAmount,
-              reservationDate: stock.purchase.reservationDate ? moment(stock.purchase.reservationDate).format() : undefined,
-              transactionAmount: stock.purchase.transactionAmount,
-              transactionDate: moment(stock.purchase.transactionDate).format()
-            } : undefined,
-            sell: stock && stock.sell ? {
-              reservationAmount: stock.sell.reservationAmount,
-              reservationDate: stock.sell.reservationDate ? moment(stock.sell.reservationDate).format() : undefined,
-              transactionAmount: stock.sell.transactionAmount,
-              transactionDate: moment(stock.sell.transactionDate).format()
-            } : undefined,
-            close: stock && stock.close ? {
-              gain: stock.close.gain,
-              transactionDate: moment(stock.close.transactionDate).format()
-            } : undefined
-          },
-          latestProposal: lastProposal ? {
-            amount: lastProposal
-          } : undefined,
-          address: address ? {
-            neighborhood: address.neighborhood ? address.neighborhood : undefined,
-            type: address.type ? address.type : undefined,
-            street: address.street ? address.street : undefined,
-            number: address.number ? address.number : undefined,
-            postalCode: address.postalCode && address.postalCode.number ? {
-              number: address.postalCode.number
-            } : undefined,
-            city: address.city ? address.city : undefined
-          } : undefined,
-          geolocation: location && (location.lat || location.lng) ? {
-            latitude: location.lat ? location.lat : undefined,
-            longitude: location.lng ? location.lng : undefined
-          } : undefined,
-          cadastreReference: cadastreReference || undefined,
-          negotiationStatus: negotiationStatus || undefined,
-          floorArea,
-          usage: use !== null ? use : undefined,
-          owner: (ownerId && {
-            id: ownerId,
-            firstName: ownerFirstName,
-            name: ownerFullName,
-            contacts: (contacts && contacts.map(({ id, status, type, value }) => ({ id, status, type, value })))
-          }) || undefined,
-          lastMeeting: (lastMeetingAt && {
-            dateMeeting: moment(lastMeetingAt).format()
-          }) || undefined,
-          featuredContact: featuredContact || undefined
-        })
-      }
-    )
+  allAssignedBuildingsId (agentId) {
+    return this.couchbaseAdapter
+      .queryAsync(N1qlQuery.fromString(assignedBuildingsIdForAgentQuery), [ agentId ])
+      .then(result => result.map(({ buildingId }) => buildingId))
   }
 
-  mapToPropertyAgentBuildingView2 () {
+  mapToPropertyAgentBuildingView () {
     return buildings => buildings.map(
       ({
         id, metadata, stock, lastProposal, cadastreReference, address, location, use, floorArea,
         ownerId, buildingMeetings = [], verifiedOwners, personOwners
       }) => {
         buildingMeetings.sort((a, b) => moment(a.eventDate).unix() - moment(b.eventDate).unix())
-        const lastMeeting = buildingMeetings.length > 0 ? buildingMeetings[buildingMeetings.length - 1] : undefined
+        const lastMeeting = buildingMeetings.length > 0 ? buildingMeetings[ buildingMeetings.length - 1 ] : undefined
         const featuredOwnerId = ownerId || (lastMeeting ? lastMeeting.ownerId : undefined)
         const featuredOwner = featuredOwnerId ? verifiedOwners.find(o => o.id === featuredOwnerId) : undefined
         const featuredOwnerPerson = featuredOwner ? personOwners.find(p => p.id === featuredOwner.personId) : undefined
