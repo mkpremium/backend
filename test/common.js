1365058916f5a5b50eb1b1dd9bbc12d95dab1f81
Jorge Velasco <zen.chen@gmail.com>
2020-06-07 21:19:44 +0200

    Delete migrations that delete all data.

--- test/common.js
+++ test/common.js
@@ -1,18 +1,16 @@
 import Promise from 'bluebird'
 import request from 'supertest'
 import { OperatorRepository } from '../src/operator/models'
-import { cleanFirebase } from '../migrations/firebase-clean'
 import { cleanQueue } from '../cli/lib/migrate-utils'
 import { CouchbaseModel } from '../src/db/model'
 import initCouchbase from '../src/db/couchbase'
 
 export async function deleteAll () {
-  initCouchbase()
+  await initCouchbase()
 
   await CouchbaseModel.prototype._promiseBucket
 
   return Promise.all([
-    cleanFirebase(),
     CouchbaseModel.prototype._bucket.removeAll(),
     cleanQueue()
   ])
