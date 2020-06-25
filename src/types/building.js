9022b803308472c870fd41e3417b7e05190bb9ed
Jorge Velasco <zen.chen@gmail.com>
2020-06-25 10:54:40 +0200

    Delete rest of migration models.

--- src/types/building.js
+++ src/types/building.js
@@ -1,69 +1,32 @@
 import t from 'tcomb'
 import uuid from 'uuid/v4'
-import { buildingEntitiesDefaultStatus, buildingEntitiesStatus } from '../migration/models/building_entity'
 import { Address, SimpleAddress, SimplePhoneNumber } from './common'
 import { BuildingStateEnum } from './enums'
 
-/**
- * @swagger
- * definitions:
- *   Cadastre:
- *     properties:
- *       refenrece:
- *         type: string
- *       address:
- *         type: string
- */
+const buildingEntitiesDefaultStatus = 'SIN DATOS'
+const buildingEntitiesStatus = {
+  2: 'VACIO',
+  4: 'INDEFINIDO',
+  5: 'A TERMINO',
+  6: 'OKUPAS'
+}
+
 export const BuildingCadastre = t.struct({
   reference: t.String,
   address: t.String
 }, 'Cadastre')
 
-/**
- * @swagger
- * definitions:
- *   Location:
- *     properties:
- *       lat:
- *         type: number
- *       lng:
- *         type: number
- */
 export const BuildingLocation = t.Location = t.struct({
   lat: t.maybe(t.Number),
   lng: t.maybe(t.Number)
 }, 'Location')
 
-/**
- * @swagger
- * definitions:
- *   Elements:
- *     properties:
- *       number:
- *         type: number
- *       average:
- *         type: number
- *       commons:
- *         type: number
- */
 t.Elements = t.struct({
   number: t.Number,
   average: t.Number,
   commons: t.Number
 }, 'Elements')
 
-/**
- * @swagger
- * definitions:
- *   BuildingOwner:
- *     properties:
- *       name:
- *         type: number
- *       address:
- *         $ref: "#/definitions/SimpleAddress"
- *       phones:
- *          $ref: "#/definitions/SimplePhoneNumber"
- */
 const BuildingOwner = t.struct(
   {
     name: t.maybe(t.String),
@@ -86,57 +49,6 @@ const BuildingProposalStatus = {
 
 t.BuildingProposalStatus = t.enums.of(Object.values(BuildingProposalStatus))
 
-/**
- * @swagger
- * definitions:
- *   BuildingProposalBody:
- *     properties:
- *       ownerId:
- *         type: string
- *         format: uuid/v4
- *         description: Id del Propietario a quien se realizó la propuesta
- *       state:
- *         type: string
- *         enum: [aceptada, enviada, pendiente]
- *       aspiration:
- *         type: number
- *       proposal:
- *         type: number
- *   BuildingProposal:
- *     properties:
- *       id:
- *         type: string
- *         format: uuid/v4 (asignado por el sistema)
- *       ownerId:
- *         type: string
- *         format: uuid/v4
- *         description: Id del Propietario a quien se realizó la propuesta
- *       buildingId:
- *         type: string
- *         format: uuid/v4
- *         description: Id del edificio (asignado por el sistema)
- *       createdBy:
- *         type: string
- *         format: uuid/v4
- *         description: Id del operator que realizo la propuesta  (asignado por el sistema)
- *       createdAt:
- *         type: string
- *         description: Fecha de creación de la propuesta *  (asignado por el sistema)
- *       updatedBy:
- *         type: string
- *         format: uuid/v4
- *         description: Id del operator que actualizó  (asignado por el sistema)
- *       updatedAt:
- *         type: string
- *         description: Fecha de actualización de la propuesta  (asignado por el sistema)
- *       state:
- *         type: string
- *         enum: [aceptada, enviada, pendiente]
- *       aspiration:
- *         type: number
- *       proposal:
- *         type: number
- */
 export const BuildingProposal = t.struct(
   {
     id: t.String,
@@ -172,51 +84,6 @@ export const BuildingProposal = t.struct(
 
 t.BuildingEntityStatus = t.enums.of(Object.values(buildingEntitiesStatus).concat(buildingEntitiesDefaultStatus))
 
-/**
- * @swagger
- * definitions:
- *   BuildingEntityBody:
- *     properties:
- *       status:
- *         type: string
- *         enum: [VACIO, INDEFINIDO, A TERMINO, OKUPAS, SIN DATOS]
- *         description: "Estado de la situación arrendataria"
- *       name:
- *         type: string
- *         description: Nombre o identificador
- *       type:
- *         type: string
- *       surface:
- *         type: number
- *         description: Area en metros
- *       rent:
- *         type: number
- *       expiration:
- *         type: string
- *         format: YYYY-MM-DDTHH:MM:SSZ
- *   BuildingEntity:
- *     properties:
- *       id:
- *         type: string
- *         format: uuid/v4
- *       status:
- *         type: string
- *         enum: [vacio, indefinido, venta arriendo, no vende, okupas]
- *         description: "Estado de la situación arrendataria"
- *       name:
- *         type: string
- *         description: Nombre o identificador
- *       type:
- *         type: string
- *       surface:
- *         type: number
- *         description: Area en metros
- *       rent:
- *         type: number
- *       expiration:
- *         type: string
- *         format: YYYY-MM-DDTHH:MM:SSZ
- */
 t.BuildingEntity = t.struct(
   {
     id: t.String,
@@ -249,21 +116,6 @@ t.BuildingEntity = t.struct(
   }
 )
 
-/**
- * @swagger
- * definitions:
- *   BuildingMetadataPreview:
- *     properties:
- *         id:
- *           type: string
- *           format: uuid/v4
- *         name:
- *           type: string
- *           description: Nombre a mostrar
- *         previewUrl:
- *           type: string
- *           description: "Url publica de amazon con un thumbnail del archivo (images, pdf)"
- */
 export const BuildingMetadataPreview = t.struct({
   id: t.String,
   name: t.maybe(t.String),
