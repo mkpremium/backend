7a6704ee3a33d82dad23ad1f997ec2da15ed663f
Jorge Velasco <zen.chen@gmail.com>
2020-07-21 12:23:51 +0200

    Add missing information for admin stock stats.

--- src/building/repository/AdminBuildingRepository.js
+++ src/building/repository/AdminBuildingRepository.js
@@ -5,8 +5,11 @@ const ALL_AGENTS_STOCK_STATS_QUERY = `
 SELECT
     building.id buildingId,
     (agent.profile.firstName || " " || agent.profile.lastName) AS agentName,
+    building.\`use\`,
+    building.landArea,
+    building.address.fullAddress as buildingAddress,
     agent.id AS agentId,
-    { 'sell': stock.sell, 'purchase': stock.purchase } as stock
+    {'sell': stock.sell, 'purchase': stock.purchase} as stock
 FROM mkpremium building
 INNER JOIN mkpremium stock ON stock.buildingId = building.id AND stock._documentType = 'stock' AND (stock.close IS MISSING OR stock.close IS NULL)
 INNER JOIN mkpremium agent ON agent._documentType = 'operator' AND building.assignedAgentId = agent.id
@@ -14,32 +17,39 @@ WHERE building._documentType = 'building'
 `
 
 export class AdminBuildingRepository {
-  constructor(couchbaseAdapter) {
+  constructor (couchbaseAdapter) {
     this.couchbaseAdapter = couchbaseAdapter
   }
 
-  allAgentsStockStats() {
+  allAgentsStockStats () {
     return this.couchbaseAdapter
       .queryAsync(N1qlQuery.fromString(ALL_AGENTS_STOCK_STATS_QUERY))
       .then(result =>
-        result.map(({buildingId, agentName, agentId, stock}) => ({
-          buildingId,
-          agentName,
-          agentId,
-          stock: {
-            purchase: stock.purchase ? {
-              reservationAmount: stock.purchase.reservationAmount,
-              reservationDate: stock.purchase.reservationDate ? moment(stock.purchase.reservationDate).format() : undefined,
-              transactionAmount: stock.purchase.transactionAmount,
-              transactionDate: moment(stock.purchase.transactionDate).format()
-            } : undefined,
-            sell: stock && stock.sell ? {
-              reservationAmount: stock.sell.reservationAmount,
-              reservationDate: stock.sell.reservationDate ? moment(stock.sell.reservationDate).format() : undefined,
-              transactionAmount: stock.sell.transactionAmount,
-              transactionDate: moment(stock.sell.transactionDate).format()
-            } : undefined
-          }
-        })))
+        result.map(({
+          buildingId, use, landArea, buildingAddress, stock, agentName, agentId
+        }) =>
+          ({
+            buildingId,
+            agentName,
+            agentId,
+            use,
+            landArea,
+            buildingAddress,
+            stock: {
+              purchase: stock.purchase ? {
+                reservationAmount: stock.purchase.reservationAmount,
+                reservationDate: stock.purchase.reservationDate ? moment(stock.purchase.reservationDate).format() : undefined,
+                transactionAmount: stock.purchase.transactionAmount,
+                transactionDate: moment(stock.purchase.transactionDate).format()
+              } : undefined,
+              sell: stock && stock.sell ? {
+                reservationAmount: stock.sell.reservationAmount,
+                reservationDate: stock.sell.reservationDate ? moment(stock.sell.reservationDate).format() : undefined,
+                transactionAmount: stock.sell.transactionAmount,
+                transactionDate: moment(stock.sell.transactionDate).format()
+              } : undefined
+            }
+          })
+        ))
   }
 }
