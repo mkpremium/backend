5b0f9206a85c11274d9a167249e6711032fbe976
Jorge Velasco <zen.chen@gmail.com>
2020-10-12 18:15:29 +0200

    Remove swagger annotations.

--- src/worksheet/building/types.js
+++ src/worksheet/building/types.js
@@ -2,16 +2,6 @@ import * as t from 'tcomb'
 import { BuildingCadastre } from '../../types/building'
 import { Address } from '../../types/common'
 
-/**
- * @swagger
- * definitions:
- *   BuildingLocation:
- *     properties:
- *       lat:
- *         type: number
- *       lng:
- *         type: number
- */
 const BuildingLocation = t.struct({
   lat: t.Number,
   lng: t.Number
@@ -29,18 +19,6 @@ export const BuildingByCadastre = BaseBuilding
 
 export const BuildingByAddress = BaseBuilding.extend({}, 'BuildingByAddress')
 
-/**
- * @swagger
- * definitions:
- *   CreateBuildingInput:
- *     properties:
- *       location:
- *         $ref: "#/definitions/BuildingLocation"
- *       address:
- *         $ref: "#/definitions/Address"
- *       cadastre:
- *         $ref: "#/definitions/Cadastre"
- */
 export const CreateBuildingInput = t.union([BuildingCadastre, BuildingByAddress])
 CreateBuildingInput.dispatch = function (input) {
   if (input.cadastre) {
