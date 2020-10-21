d2a9f779153d67c8967d2a3ff98bbdd223a8fac3
Jorge Velasco <zen.chen@gmail.com>
2020-10-21 12:23:17 +0200

    Remove old freezer cronjob.

--- bin/cronjobs.js
+++
@@ -1,7 +0,0 @@
-import '../src/types'
-import { logger } from '../src/infrastructure/logger'
-import worksheetFreezerCron from '../src/cron/worksheets/freezer-cron'
-
-logger.info('starting cron jobs')
-
-worksheetFreezerCron.start()
