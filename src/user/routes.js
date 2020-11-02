1f036ce9e48f900bb3de0884f42941f61a8f68e3
Jorge Velasco <zen.chen@gmail.com>
2020-11-02 17:07:57 +0100

    Wrap user endpoints with express wrapper.

--- src/user/routes.js
+++ src/user/routes.js
@@ -1,12 +1,13 @@
 import { Router } from 'express'
 import { createMeController, createAddFavoritesController, createDeleteFavoriteBuildingController } from './controllers'
+import { wrap } from 'express-promise-wrap'
 
 export const userRoutes = (usersRepository, addFavoriteBuildingService, deleteFavoriteBuildingService) => {
   const router = new Router()
 
-  router.get('/me', createMeController(usersRepository))
-  router.post('/favorites', createAddFavoritesController(addFavoriteBuildingService))
-  router.delete('/favorites/:buildingId', createDeleteFavoriteBuildingController(deleteFavoriteBuildingService))
+  router.get('/me', wrap(createMeController(usersRepository)))
+  router.post('/favorites', wrap(createAddFavoritesController(addFavoriteBuildingService)))
+  router.delete('/favorites/:buildingId', wrap(createDeleteFavoriteBuildingController(deleteFavoriteBuildingService)))
 
   return router
 }
