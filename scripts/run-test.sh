e2aab153683ee573d5f56bfbd6de1df76e29edbd
Jorge Velasco <jorge.velasco.silva@gmail.com>
2020-11-12 19:03:08 +0100

    Retry failing tests.

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -5,4 +5,4 @@ set -e
 readonly test_dir=$1
 
 export NODE_ENV=test
-npx mocha --config "${PWD}/${test_dir}/.mocharc.js" "${test_dir}/**/*.spec.js"
+npx mocha --config "${PWD}/${test_dir}/.mocharc.js" "${test_dir}/**/*.spec.js" --retries 2
