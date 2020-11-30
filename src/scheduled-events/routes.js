d78fd1651c1afccdf885c684c8a9dcf80c7e9ce6
Jorge Velasco <jorge.velasco.silva@gmail.com>
2020-11-30 19:29:03 +0100

    Group `scheduled-events` routes.

--- src/scheduled-events/routes.js
+++ src/scheduled-events/routes.js
@@ -12,23 +12,16 @@ import {
 } from './controllers'
 import { createGetUserScheduledCallsController } from './controller/get-user-scheduled-calls.controller'
 
-const router = Router()
-
-router.get('/week', weekScheduleEventMeetingsController)
-
-router.get('/', listScheduledEventController)
-
-router.post('/call', addScheduledCallEventController)
-
-router.put('/:id', updateScheduledEventController)
-
-router.delete('/:id', deleteScheduledEventController)
-
 export const createScheduleEventsRoutes = (createMeetingService, scheduledCallsService) => {
+  const router = Router()
+
+  router.get('/week', weekScheduleEventMeetingsController)
+  router.get('/', listScheduledEventController)
+  router.post('/call', addScheduledCallEventController)
   router.post('/meeting', createAddScheduledMeetingEventController(createMeetingService))
-
   router.get('/calls', createGetUserScheduledCallsController(scheduledCallsService))
-
+  router.put('/:id', updateScheduledEventController)
+  router.delete('/:id', deleteScheduledEventController)
   router.get('/:id', findScheduledEventController)
 
   return router
