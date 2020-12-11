5755e97b01ed8e501bafcfcb369c83b130428222
Jorge Velasco <jorge.velasco.silva@gmail.com>
2020-12-11 11:14:26 +0100

    Create multiple contacts for owner.

--- src/test-harness/fake-data-generator.js
+++ src/test-harness/fake-data-generator.js
@@ -6,6 +6,7 @@ import _ from 'lodash'
 export const createOwnerCmd = buildingId => {
   const ownerFirstName = faker.name.firstName()
   const ownerLastName = faker.name.lastName()
+  const nbOfContacts = 1 + ((Math.random() * 10) % 5) // [1, 6]
 
   return {
     buildingId,
@@ -13,14 +14,12 @@ export const createOwnerCmd = buildingId => {
     firstName: ownerFirstName,
     status: faker.helpers.shuffle([ 'VERIFICADO', 'NO_VERIFICADO' ])[ 0 ],
     type: faker.helpers.shuffle(Object.values(OwnerType))[ 0 ],
-    contacts: [
-      {
-        id: uuid(),
-        type: 'TELEFONO',
-        value: faker.phone.phoneNumber('9########'),
-        status: faker.helpers.shuffle([ 'UNDEFINED', 'GOOD' ])[ 0 ]
-      }
-    ]
+    contacts: _.times(nbOfContacts, () => ({
+      id: uuid(),
+      type: 'TELEFONO',
+      value: faker.phone.phoneNumber('9########'),
+      status: faker.helpers.shuffle([ 'UNDEFINED', 'GOOD' ])[ 0 ]
+    }))
   }
 }
 
