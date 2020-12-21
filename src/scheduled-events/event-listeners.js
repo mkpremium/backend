9c543532cad6fc0b6e8bb5ae96300d7e690a3e0a
Jorge Velasco <jorge.velasco.silva@gmail.com>
2020-12-21 17:05:08 +0100

    Delete scheduled calls when building is mark as not for sale.

--- src/scheduled-events/event-listeners.js
+++ src/scheduled-events/event-listeners.js
@@ -1,5 +1,14 @@
+import { BUILDING_NEGOTIATION_STATUS_CHANGED } from '../building/service/UpdateBuildingNegotiationStatusService'
+
 export function setupEventListeners (eventBus, { scheduledCallRepository }) {
   eventBus.on('meeting.created', ({ buildingId }) => {
+    return scheduledCallRepository.removeScheduledCallsForBuilding(buildingId)
+  })
+
+  eventBus.on(BUILDING_NEGOTIATION_STATUS_CHANGED, ({ buildingId, negotiationStatus }) => {
+    if (negotiationStatus !== 'NO VENDE') {
+      return Promise.resolve()
+    }
     return scheduledCallRepository.removeScheduledCallsForBuilding(buildingId)
   })
 }
--- test/scheduled-events/event-listeners.spec.js
+++ test/scheduled-events/event-listeners.spec.js
@@ -2,16 +2,39 @@ import { setupEventListeners } from '../../src/scheduled-events/event-listeners'
 import { InMemorySyncEventBus } from '../../src/infrastructure/event-bus/in-memory-sync-event-bus'
 import { spy } from 'sinon'
 import { expect } from 'chai'
+import { BUILDING_NEGOTIATION_STATUS_CHANGED } from '../../src/building/service/UpdateBuildingNegotiationStatusService'
 
 describe('scheduled-events.setupEventListeners', () => {
-  it('deletes scheduled calls when a visit is scheduled for the building', () => {
-    const eventBus = new InMemorySyncEventBus()
-    const scheduledCallRepositoryMock = {
+  let eventBus
+  let scheduledCallRepositoryMock
+
+  beforeEach(() => {
+    eventBus = new InMemorySyncEventBus()
+    scheduledCallRepositoryMock = {
       removeScheduledCallsForBuilding: spy()
     }
     setupEventListeners(eventBus, { scheduledCallRepository: scheduledCallRepositoryMock })
+  })
 
-    eventBus.publish({name: 'meeting.created', buildingId: 'test-building-id'})
+  it('deletes scheduled calls when a visit is scheduled for the building', () => {
+    eventBus.publish({ name: 'meeting.created', buildingId: 'test-building-id' })
+
+    expect(scheduledCallRepositoryMock.removeScheduledCallsForBuilding).to.have.been.calledWith('test-building-id')
+  })
+
+  it('deletes scheduled calls when building is mark as not for sale', () => {
+    eventBus.publish({
+      name: BUILDING_NEGOTIATION_STATUS_CHANGED,
+      buildingId: 'test-building-id',
+      negotiationStatus: 'COMPRADO'
+    })
+    expect(scheduledCallRepositoryMock.removeScheduledCallsForBuilding).not.to.have.been.called
+
+    eventBus.publish({
+      name: BUILDING_NEGOTIATION_STATUS_CHANGED,
+      buildingId: 'test-building-id',
+      negotiationStatus: 'NO VENDE'
+    })
 
     expect(scheduledCallRepositoryMock.removeScheduledCallsForBuilding).to.have.been.calledWith('test-building-id')
   })
