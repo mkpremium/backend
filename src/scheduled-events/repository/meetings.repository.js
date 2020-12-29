dc6caebf1d67aa712ec5124df707055222694d89
Jorge Velasco <jorge.velasco.silva@gmail.com>
2020-12-29 20:00:31 +0100

    Query scheduled events to get future meetings for a user.

--- src/scheduled-events/repository/meetings.repository.js
+++ src/scheduled-events/repository/meetings.repository.js
@@ -2,6 +2,8 @@ import { CouchbaseRepository } from '../../db/couchbase.repository'
 import t from 'tcomb'
 import { Meeting } from '../domain/meeting'
 import moment from 'moment'
+import { N1qlQuery } from 'couchbase'
+import fromJSON from 'tcomb/lib/fromJSON'
 
 const DbMeeting = t.struct({
   id: t.String,
@@ -10,10 +12,12 @@ const DbMeeting = t.struct({
   event: t.struct({
     buildingId: t.String
   }),
+  type: t.irreducible('MeetingDbEventType', dt => dt === 'MEETINGS'),
   _documentType: t.irreducible('MeetingDbDocumentType', dt => dt === 'scheduled-event')
 }, {
   name: 'DbMeeting',
   defaultProps: {
+    type: 'MEETINGS',
     _documentType: 'scheduled-event'
   }
 })
@@ -27,8 +31,39 @@ DbMeeting.prototype.couchbaseToDomain = function () {
   })
 }
 
+DbMeeting.fromMeeting = meeting => DbMeeting({
+  id: meeting.id,
+  notifyTo: meeting.withAgentOfId,
+  eventDate: meeting.meetingAt.toDate(),
+  event: {
+    buildingId: meeting.buildingId
+  }
+})
+
+const futureMeetingsForQuery = bucketName => `
+SELECT
+  id,
+  notifyTo,
+  eventDate,
+  {"buildingId": event.buildingId} event
+FROM ${bucketName}
+WHERE _documentType = 'scheduled-event' AND type = 'MEETINGS'
+  AND notifyTo = $1 AND eventDate > NOW_UTC()
+`
+
 export class MeetingsRepository extends CouchbaseRepository {
   struct () {
     return DbMeeting
+  }
+
+  save (meeting) {
+    return super.save(DbMeeting.fromMeeting(meeting))
+  }
+
+  futureMeetingsFor (userId) {
+    return this.couchbaseAdapter.queryAsync(
+      N1qlQuery.fromString(futureMeetingsForQuery(this.bucketName)),
+      [ userId ]
+    ).then(rows => rows.map(r => fromJSON(r, DbMeeting).couchbaseToDomain()))
   }
 }
--- test/scheduled-events/repository/meetings.repository.spec.js
+++ test/scheduled-events/repository/meetings.repository.spec.js
@@ -33,12 +33,35 @@ describe('MeetingsRepository', () => {
     const fetchedMeeting = await repository.get('test-meeting-id')
 
     expect(fetchedMeeting).not.to.be.undefined
-    expect(Meeting.is(fetchedMeeting)).to.be.true
-    expect(fetchedMeeting).to.be.eql({
+    expect(fetchedMeeting).to.be.eql(Meeting({
       id: testScheduledEvent.id,
       buildingId: testScheduledEvent.event.buildingId,
       withAgentOfId: testScheduledEvent.notifyTo,
       meetingAt: moment(testScheduledEvent.eventDate)
+    }))
+  })
+
+  describe('futureMeetingsFor', () => {
+    it('returns  only future meetigs for user', async () => {
+      const pastMeeting = Meeting({
+        id: 'test-past-meeting',
+        buildingId: 'test-building-id',
+        withAgentOfId: 'test-user-id',
+        meetingAt: moment().add(-1, 'minute')
+      })
+      const futureMeeting = Meeting({
+        id: 'test-future-meeting',
+        buildingId: 'test-building-id',
+        withAgentOfId: 'test-user-id',
+        meetingAt: moment().add(1, 'minute')
+      })
+      await Promise.all([ repository.save(pastMeeting), repository.save(futureMeeting) ])
+
+      return repository.futureMeetingsFor('test-user-id')
+        .then(result => {
+          expect(result).to.have.length(1)
+          expect(result[ 0 ].id).to.be.equal('test-future-meeting')
+        })
     })
   })
 })
