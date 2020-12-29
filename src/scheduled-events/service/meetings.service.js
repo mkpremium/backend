8ca5c874269a70dea839c716126f826703ed1244
Jorge Velasco <jorge.velasco.silva@gmail.com>
2020-12-29 20:02:06 +0100

    Use meetings repository to get future meetings from service.

--- src/scheduled-events/service/meetings.service.js
+++ src/scheduled-events/service/meetings.service.js
@@ -1,9 +1,12 @@
+/**
+ * @property {MeetingsRepository} meetingsRepository
+ */
 export class MeetingsService {
   constructor ({ meetingsRepository }) {
     this.meetingsRepository = meetingsRepository
   }
 
   futureMeetingsFor (userId) {
-    return Promise.resolve([])
+    return this.meetingsRepository.futureMeetingsFor(userId)
   }
 }
