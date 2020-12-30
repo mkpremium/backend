3b7bed93aef23f2d8e48a29c8ed9589a128356dd
Jorge Velasco <jorge.velasco.silva@gmail.com>
2020-12-30 10:55:16 +0100

    Map user's `restringedHours` to user's blocked availability

--- src/user/service/user-blocked-availability.service.js
+++ src/user/service/user-blocked-availability.service.js
@@ -1,5 +1,22 @@
+import _ from 'lodash'
+import moment from 'moment'
+
+/**
+ * @property {UserRepository} usersRepository
+ */
 export class UserBlockedAvailabilityService {
+  constructor ({ usersRepository }) {
+    this.usersRepository = usersRepository
+  }
+
   blockedAvailabilityForUser (userId) {
-    return Promise.resolve([])
+    return this.usersRepository.get(userId)
+      .then(({ restringedHours }) => {
+        return _.flatMap(
+          (Object.keys(restringedHours || {})).map(day => restringedHours[ day ].map(({ start, end }) => ({
+            startsAt: moment(`${day}T${start}`),
+            endsAt: moment(`${day}T${end}`)
+          }))))
+      })
   }
 }
---
+++ test/user/service/user-blocked-availability.service.spec.js
@@ -0,0 +1,40 @@
+import { expect } from 'chai'
+import { UserBlockedAvailabilityService } from '../../../src/user/service/user-blocked-availability.service'
+import { stub } from 'sinon'
+import moment from 'moment'
+
+describe('UserBlockedAvailabilityService', () => {
+  /** @var {UserBlockedAvailabilityService} service **/
+  let service
+  let usersRepositoryStub = {
+    get: stub()
+  }
+
+  beforeEach(() => {
+    service = new UserBlockedAvailabilityService({
+      usersRepository: usersRepositoryStub
+    })
+  })
+
+  it('maps restrictions to blocked availability', () => {
+    usersRepositoryStub.get.withArgs('test-user-id').resolves({
+      restringedHours: {
+        '2020-12-23': [
+          {
+            start: '10:00',
+            end: '11:00'
+          }
+        ]
+      }
+    })
+
+    return service.blockedAvailabilityForUser('test-user-id')
+      .then(userBlockedAvailability => {
+        expect(userBlockedAvailability).to.have.length(1)
+        expect(userBlockedAvailability[ 0 ]).to.be.eql({
+          startsAt: moment('2020-12-23T10:00'),
+          endsAt: moment('2020-12-23T11:00')
+        })
+      })
+  })
+})
