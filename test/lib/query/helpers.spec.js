d919bf0da4943b881613f2b0dd9cfc8123af4095
Jorge Velasco <jorge.velasco.silva@gmail.com>
2021-01-21 19:30:36 +0100

    Remove exploratory test.

--- test/lib/query/helpers.spec.js
+++ test/lib/query/helpers.spec.js
@@ -1,15 +1,9 @@
 import { expect } from 'chai'
 import { addBetweenQueryToBuilder } from '../../../src/lib/query/helpers'
 import squel from 'squel'
-import { utc } from '../../../src/lib/date'
 import moment from 'moment'
 
 describe('addBetweenQueryToBuilder', () => {
-  it('explorations', () => {
-    expect(moment.utc('2021-01-21').format('YYYY-MM-DD')).to.be.equal('2021-01-21')
-    expect(utc('2021-01-21').format('YYYY-MM-DD')).to.be.equal('2021-01-20') // TODO document bug and deprecate method?
-  })
-
   it('adds between query to query builder', () => {
     const expectedQueryBuilder = squel.select()
       .where('createdAt >= ?', moment.utc('2021-01-21').startOf('day').toDate())
