4016a98f70ad522a8fff50047e801c3051ed291d
Jorge Velasco <jorge.velasco.silva@gmail.com>
2021-01-22 18:11:14 +0100

    Add BuildingV2 type and its mapper.

--- src/building/repository/commercials-building.repository.js
+++ src/building/repository/commercials-building.repository.js
@@ -1,6 +1,8 @@
 import { N1qlQuery } from 'couchbase'
 import _ from 'lodash'
 import moment from 'moment'
+import t from 'tcomb'
+import fromJSON from 'tcomb/lib/fromJSON'
 
 const listBuildingsByIdQuery = bucketName => `
 SELECT
@@ -161,7 +163,7 @@ export class CommercialsBuildingRepository {
   }
 
   static getOwner (featuredOwnerId, lastMeeting, owners) {
-    const validatedOwners = (owners || []).filter(({contacts}) => (contacts || []).find(({status}) => status === 'GOOD'))
+    const validatedOwners = (owners || []).filter(({ contacts }) => (contacts || []).find(({ status }) => status === 'GOOD'))
     if (!validatedOwners || validatedOwners.length === 0) {
       return
     }
@@ -175,3 +177,94 @@ export class CommercialsBuildingRepository {
     return validatedOwners.find(o => o.id === ownerId)
   }
 }
+
+export const mapToBuildingV2 = buildings => buildings.map(
+  b => {
+    const { buildingMeetings, ownerId, owners, floorArea } = b
+    buildingMeetings.sort((a, b) => moment(a.eventDate).unix() - moment(b.eventDate).unix())
+
+    const lastMeeting = buildingMeetings.length > 0 ? buildingMeetings[ buildingMeetings.length - 1 ] : undefined
+    const owner = CommercialsBuildingRepository.getOwner(ownerId, lastMeeting, owners)
+
+    const v2Building = {
+      ...b,
+      floorArea: floorArea ? parseInt(floorArea) : undefined,
+      owner
+    }
+    try {
+      return fromJSON(v2Building, BuildingV2)
+    } catch (error) {
+      error.message = `[building:${b.id}] ${error.message}`
+      throw error
+    }
+  }
+)
+
+const DateTimeString = t.irreducible('DateTimeString', dts => moment.utc(dts).isValid())
+
+const BuildingV2 = t.struct({
+  id: t.String,
+  files: t.list(t.struct({
+    id: t.String,
+    mimeType: t.String,
+    thumbnailUrl: t.String
+  })),
+  stock: t.maybe(t.struct({
+    purchase: t.maybe(t.struct({
+      reservationAmount: t.Number,
+      reservationDate: DateTimeString,
+      transactionAmount: t.Number,
+      transactionDate: t.maybe(DateTimeString)
+    })),
+    sell: t.maybe(t.struct({
+      reservationAmount: t.Number,
+      reservationDate: t.maybe(DateTimeString),
+      transactionAmount: t.Number,
+      transactionDate: t.maybe(DateTimeString)
+    })),
+    close: t.maybe(t.struct({
+      operatorId: t.String,
+      gain: t.Number,
+      transactionDate: DateTimeString
+    }))
+  })),
+  latestProposal: t.maybe(t.struct({
+    amount: t.Number
+  })),
+  address: t.maybe(t.struct({
+    neighborhood: t.maybe(t.String),
+    type: t.maybe(t.String),
+    street: t.maybe(t.String),
+    number: t.maybe(t.union([ t.String, t.Number ])),
+    postalCode: t.maybe(t.struct({
+      number: t.String
+    })),
+    city: t.maybe(t.String)
+  })),
+  geolocation: t.maybe(t.struct({
+    latitude: t.maybe(t.Number),
+    longitude: t.maybe(t.Number)
+  })),
+  cadastreReference: t.maybe(t.String),
+  negotiationStatus: t.maybe(t.String),
+  floorArea: t.maybe(t.Number),
+  usage: t.maybe(t.String),
+  owner: t.maybe(t.struct({
+    id: t.String,
+    firstName: t.maybe(t.String),
+    fullName: t.String,
+    contacts: t.maybe(t.list(t.struct({
+      id: t.String,
+      status: t.enums.of([ 'GOOD', 'BAD', 'UNDEFINED' ]),
+      type: t.enums.of([ 'TELEFONO', 'MOVIL', 'EMAIL' ])
+    }))),
+    featuredContact: t.maybe(t.struct({
+      phoneId: t.maybe(t.String),
+      emailId: t.maybe(t.String)
+    }))
+  })),
+  lastMeeting: t.maybe(t.struct({
+    dateMeeting: DateTimeString
+  })),
+  salePrice: t.maybe(t.Number)
+})
