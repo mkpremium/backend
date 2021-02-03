6b3ec72bea2cc3b369ee5c29e81dde6e3621ce33
Jorge Velasco <jorge.velasco.silva@gmail.com>
2021-02-03 15:05:53 +0100

    Exclude owners with wrong status when getting worksheet.

--- src/worksheet/repository/worksheet.repository.js
+++ src/worksheet/repository/worksheet.repository.js
@@ -85,7 +85,7 @@ FROM ${bucketName} worksheet
 JOIN ${bucketName} building ON building._documentType = 'building' AND
 building.id = worksheet.relatedBuildingIds[0]
 NEST ${bucketName} owners ON owners._documentType = 'owner' AND owners.buildingId = building.id
-                          AND owners.status != 'WITHOUT_CONTACT'
+                          AND owners.status NOT IN ['WITHOUT_CONTACT', 'ERRONEO']
 
 WHERE worksheet._documentType = 'worksheet' AND ${conditions.join(' AND ')}
 `
