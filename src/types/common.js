eb1b0a3cc378536bd64cb73b17dd3f8065f337d0
Jorge Velasco <jorge.velasco.silva@gmail.com>
2021-02-11 17:38:48 +0100

    Remove swagger annotation.

--- src/types/common.js
+++ src/types/common.js
@@ -2,16 +2,6 @@ import t from 'tcomb'
 import uuid from 'uuid/v4'
 import { TypeContact } from './enums'
 
-/**
- * @swagger
- * definitions:
- *   PostalCode:
- *     properties:
- *       number:
- *         type: number
- *       verified:
- *         type: boolean
- */
 const PostalCode = t.struct(
   {
     number: t.maybe(t.union([t.String, t.Number])),
@@ -25,16 +15,6 @@ const PostalCode = t.struct(
   }
 )
 
-/**
- * @swagger
- * definitions:
- *   SimpleAddress:
- *     properties:
- *       fullAddress:
- *         type: string
- *       city:
- *         type: string
- */
 export const SimpleAddress = t.SimpleAddress = t.struct({
   fullAddress: t.maybe(t.String),
   floor: t.maybe(t.String),
@@ -43,16 +23,6 @@ export const SimpleAddress = t.SimpleAddress = t.struct({
   postalCode: t.maybe(t.String)
 }, 'SimpleAddress')
 
-/**
- * @swagger
- * definitions:
- *   SimplePhoneNumber:
- *     properties:
- *       number:
- *         type: string
- *       note:
- *         type: string
- */
 export const SimplePhoneNumber = t.SimplePhoneNumber = t.struct(
   {
     number: t.String,
@@ -90,22 +60,6 @@ export const TypedContactInfo = t.struct(
   }
 )
 
-/**
- * @swagger
- * definitions:
- *   TypedContactInfoUpdate:
- *     properties:
- *       type:
- *         type: string
- *         enum: [TELEFONO, FAX, MOVIL, EMAIL, SITIO_WEB]
- *       value:
- *         type: string
- *       note:
- *         type: string
- *       status:
- *         type: string
- *         enum: [UNDEFINED, GOOD, BAD]
- */
 t.TypedContactInfoUpdate = t.struct(
   {
     type: t.maybe(TypeContact),
@@ -118,30 +72,6 @@ t.TypedContactInfoUpdate = t.struct(
   }
 )
 
-/**
- * @swagger
- * definitions:
- *   Address:
- *     properties:
- *       type:
- *         type: string
- *       street:
- *         type: string
- *       number:
- *         type: number
- *       fullAddress:
- *         type: string
- *       registerNumber:
- *         type: number
- *       postalCode:
- *         $ref: "#/definitions/PostalCode"
- *       city:
- *         type: string
- *       province:
- *         type: string
- *       zone:
- *         type: string
- */
 export const Address = t.Address = t.struct(
   {
     type: t.maybe(t.String),
