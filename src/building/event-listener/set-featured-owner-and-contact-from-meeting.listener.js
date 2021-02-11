34a21845f64344cdb54de2d72638052e428d867c
Jorge Velasco <jorge.velasco.silva@gmail.com>
2021-02-11 16:39:51 +0100

    Set featured owner and featured contact from created meeting.

--- src/building/event-listener/set-featured-owner-and-contact-from-meeting.listener.js
+++ src/building/event-listener/set-featured-owner-and-contact-from-meeting.listener.js
@@ -1,2 +1,16 @@
-export const createSetFeaturedOwnerAndContactFromMeetingListener = () => () => {
+/**
+ * @param {FeaturedOwnerService} featuredOwnerService
+ * @param {SetOwnerFeaturedContactService} setOwnerFeaturedContactService
+ */
+export const createSetFeaturedOwnerAndContactFromMeetingListener = ({
+  featuredOwnerService, setOwnerFeaturedContactService
+}) => ({
+  buildingId,
+  ownerId,
+  contactId
+}) => {
+  return Promise.all([
+    featuredOwnerService.setBuildingFeaturedOwner(buildingId, ownerId),
+    setOwnerFeaturedContactService.setFeaturedContact(ownerId, { phoneId: contactId })
+  ])
 }
---
+++ test/building/event-listener/set-featured-owner-and-contact-from-meeting.listener.spec.js
@@ -0,0 +1,39 @@
+import { createSetFeaturedOwnerAndContactFromMeetingListener } from '../../../src/building/event-listener/set-featured-owner-and-contact-from-meeting.listener'
+import { expect } from 'chai'
+import { stub } from 'sinon'
+
+const testBuildingId = 'test-building-id'
+const testOwnerId = 'test-owner-id'
+const testContactId = 'test-contact-id'
+describe('set-featured-owner-and-contact-from-meeting.listener', () => {
+  let listener
+  let featuredOwnerServiceStub
+  let setOwnerFeaturedContactServiceStub
+  let result
+
+  beforeEach(() => {
+    featuredOwnerServiceStub = {
+      setBuildingFeaturedOwner: stub().resolves()
+    }
+    setOwnerFeaturedContactServiceStub = {
+      setFeaturedContact: stub().resolves()
+    }
+    listener = createSetFeaturedOwnerAndContactFromMeetingListener({
+      featuredOwnerService: featuredOwnerServiceStub,
+      setOwnerFeaturedContactService: setOwnerFeaturedContactServiceStub
+    })
+    result = listener({ ownerId: testOwnerId, buildingId: testBuildingId, contactId: testContactId })
+  })
+
+  it('sets meeting owner as building featured owner', () => {
+    return result.then(
+      () => expect(featuredOwnerServiceStub.setBuildingFeaturedOwner).to.have.been.calledWith(testBuildingId, testOwnerId)
+    )
+  })
+
+  it('marks contact as featured', () => {
+    return result.then(
+      () => expect(setOwnerFeaturedContactServiceStub.setFeaturedContact).to.have.been.calledWith(testOwnerId, { phoneId: testContactId })
+    )
+  })
+})
