e7ee019ed5adc59071db8041022e275151e2a678
Jorge Velasco <jorge.velasco.silva@gmail.com>
2021-02-22 15:38:12 +0100

    Include discarded buildings and wrong contacts in search by contact.

--- src/owner/repository/owner.repository.js
+++ src/owner/repository/owner.repository.js
@@ -15,10 +15,9 @@ worksheet.id worksheetId,
 owner.name
 FROM ${bucketName} owner
 JOIN ${bucketName} building ON building._documentType = 'building' AND building.id = owner.buildingId
-  AND building.negotiationStatus != 'DESCARTADO'
 JOIN ${bucketName} worksheet ON worksheet._documentType = 'worksheet' AND worksheet.relatedBuildingIds[0] = building.id
 WHERE owner._documentType = 'owner'
-AND ANY c IN owner.person.contacts SATISFIES c.\`value\` = $1 AND c.status != 'BAD' END
+AND ANY c IN owner.person.contacts SATISFIES c.\`value\` = $1 END
 `
 
 const buildingOwnersQuery = bucketName => `
