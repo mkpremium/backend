6f4b9eb17b2b65462678c9e0e9d10fa5b2314a6a
Jorge Velasco <jorge.velasco.silva@gmail.com>
2021-03-25 10:32:33 +0100

    Don't init full application from stock repository test.

--- test/stock/StockRepository.spec.js
+++ test/stock/StockRepository.spec.js
@@ -1,20 +1,20 @@
-import { initApplication } from '../../test-e2e/helper/rest-api-helper'
 import { operatorCreateBusiness } from '../common'
 import moment from 'moment-timezone'
 import { expect } from 'chai'
 import { closeSellStock } from '../../src/stock/application'
 import { LegacyBuildingRepository } from '../../src/building/models'
 import { buildingData } from './stock.mock'
+import { createTestContainer } from '../create-test-container'
 
 describe('StockRepository', () => {
-  let app, stockRepository, stockService
+  let container, stockRepository, stockService
   const now = moment()
   const tomorrow = now.clone().add(1, 'day')
 
   beforeEach(async () => {
-    app = await initApplication()
-    stockService = app.locals.dependenciesContainer.stockService
-    stockRepository = app.locals.dependenciesContainer.stockRepository
+    container = await createTestContainer()
+    stockService = container.resolve('stockService')
+    stockRepository = container.resolve('stockRepository')
   })
 
   describe('getTotalProfitInPeriodByPropertyManager', () => {
@@ -25,16 +25,16 @@ describe('StockRepository', () => {
       const buildingPurchaseAmount = 1000
       const buildingSellingAmount = 1200
       await purchaseBuildingBySalesAgent(stockService, testBuilding, propertyManager, buildingPurchaseAmount)
-      await sellBuilding(app, testBuilding, propertyManager, buildingSellingAmount)
+      await sellBuilding(container, testBuilding, propertyManager, buildingSellingAmount)
 
-      await closeSellStock({buildingId: testBuilding.id}, propertyManager.id)
+      await closeSellStock({ buildingId: testBuilding.id }, propertyManager.id)
 
       const result = await stockRepository.getTotalProfitInPeriodByPropertyManager(now, tomorrow)
 
-      expect(result).to.be.deep.equal([{
+      expect(result).to.be.deep.equal([ {
         propertyManagerId: propertyManager.id,
         profitAmount: 200
-      }])
+      } ])
     })
   })
 })
@@ -50,7 +50,7 @@ function purchaseBuildingBySalesAgent (createPurchaseStockService, building, age
   return createPurchaseStockService.purchaseBuilding(params, agent.id)
 }
 
-function sellBuilding (app, building, agent, transactionAmount) {
+function sellBuilding (container, building, agent, transactionAmount) {
   const params = {
     buildingId: building.id,
     reservationAmount: 2000.00,
@@ -58,7 +58,7 @@ function sellBuilding (app, building, agent, transactionAmount) {
     transactionAmount: transactionAmount,
     transactionDate: '2019-07-11T13:00:00.000Z'
   }
-  const { stockSalesService } = app.locals.dependenciesContainer
+  const stockSalesService = container.resolve('stockSalesService')
 
   return stockSalesService.sellStock(params, agent.id)
 }
