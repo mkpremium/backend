df3debde28ff1a29fe4aba3a4c2a5646480e7a7e
Jorge Velasco <jorge.velasco.silva@gmail.com>
2021-03-30 10:45:44 +0200

    Revert unintentional change.

--- scripts/test-ci.sh
+++ scripts/test-ci.sh
@@ -3,10 +3,10 @@
 set -e
 
 
-docker rm -f ci-debug
-docker create --name ci-debug -v /tmp/couchbase_logs:/opt/couchbase/var/lib/couchbase/logs \
-  -p 8091:8091 -p 8092:8092 -p 8093:8093 -p 8094:8094 -p 11210:11210 couchase-dev
-docker start ci-debug
+#docker rm -f ci-debug
+#docker create --name ci-debug -v /tmp/couchbase_logs:/opt/couchbase/var/lib/couchbase/logs \
+#  -p 8091:8091 -p 8092:8092 -p 8093:8093 -p 8094:8094 -p 11210:11210 couchase-dev
+#docker start ci-debug
 node scripts/couchbase/init-db.js
-#npm run test
-#npm run test:e2e
+npm run test
+npm run test:e2e
