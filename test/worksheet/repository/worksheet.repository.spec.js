619158bfbb38e154cfb4f5a6ff55ca2ca7edef48
Jorge Velasco <jorge.velasco.silva@gmail.com>
2021-03-31 11:13:44 +0200

    Add test to check that worksheet repository matches expected schema.

--- test/worksheet/repository/worksheet.repository.spec.js
+++ test/worksheet/repository/worksheet.repository.spec.js
@@ -1,9 +1,38 @@
 import { CallcenterView } from '../../../src/worksheet/repository/worksheet.repository'
-import fromJSON from 'tcomb/lib/fromJSON'
-import t from 'tcomb'
+import { createTestContainer } from '../../create-test-container'
+import { expect } from 'chai'
+import { worksheetBuilder } from '../worksheet.builder'
+import { buildingBuilder } from '../../building/building.builder'
+import { ownerBuilder } from '../../owner/owner.builder'
 
 describe('worksheet.repository', () => {
-  it('parses callcenter view', () => {
-    fromJSON([], t.list(CallcenterView))
+  let repository, buildingsRepository, ownersRepository
+
+  beforeEach(async () => {
+    const container = await createTestContainer()
+    repository = container.resolve('worksheetRepository')
+    buildingsRepository = container.resolve('buildingsRepository')
+    ownersRepository = container.resolve('ownersRepository')
+  })
+
+  it('gets worksheet with callcenter view', () => {
+    const testWorksheetId = 'test-worksheet-id'
+    const testBuilding = buildingBuilder().build()
+    const testOwner = ownerBuilder({ buildingId: testBuilding.id }).build()
+    const testWorksheet = worksheetBuilder({
+      id: testWorksheetId,
+      relatedBuildingIds: [ testBuilding.id ]
+    }).build()
+
+    return Promise.all([
+      buildingsRepository.save(testBuilding),
+      repository.save(testWorksheet),
+      ownersRepository.save(testOwner)
+    ]).then(() =>
+      repository.getForCallcenterView(testWorksheetId)
+        .then(result => {
+          expect(() => CallcenterView(result)).not.throw
+        })
+    )
   })
 })
