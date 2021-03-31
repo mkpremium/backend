3d49e6c90ea1941b27c6e8407a96eb4d6811d6f2
Jorge Velasco <jorge.velasco.silva@gmail.com>
2021-03-31 12:22:23 +0200

    Fail fast on auth error.

--- scripts/couchbase/init-db.js
+++ scripts/couchbase/init-db.js
@@ -94,7 +94,7 @@ retry(createBucket, { max_tries: 3, interval: ONE_MINUTE / 6 })
       {
         interval: ONE_MINUTE / 4,
         timeout: 2 * ONE_MINUTE,
-        // predicate: error => error.code !== couchbase.errors.authError
+        predicate: error => error.code !== couchbase.errors.authError
       }
     )
       .catch(error => {
