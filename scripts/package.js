da2905377bb7488ed9905b91010efcdcdde16a94
Jorge Velasco <jorge.velasco.silva@gmail.com>
2021-03-31 16:32:40 +0200

    Fix package.js script.

--- scripts/package.js
+++ scripts/package.js
@@ -1,14 +1,13 @@
 #!/usr/bin/env node
 
-const pickBy = require('lodash/pickBy');
-const {resolve} = require('path');
-const fs = require('fs-extra');
-const packageJson = require('../package');
-const buildPackage = require('./build-package');
+const pickBy = require('lodash/pickBy')
+const { resolve } = require('path')
+const fs = require('fs-extra')
+const packageJson = require('../package')
 
-const builtPackage = Object.assign({}, packageJson, buildPackage);
-const output = resolve(`${process.env.BUILD_FOLDER}/package.json`);
+const builtPackage = { ...packageJson, devDependencies: null, scripts: null, }
+const output = resolve(`${process.env.BUILD_FOLDER}/package.json`)
 
 fs.writeJsonSync(output, pickBy(builtPackage), {
   spaces: 2
-});
+})
