9e81770ad88b0aba30c3d6999c103076933a39ed
Jorge Velasco <jorge.velasco.silva@gmail.com>
2021-04-07 19:10:54 +0200

    Init cluster from `init-db` script.

--- scripts/couchbase/init-db.js
+++ scripts/couchbase/init-db.js
@@ -1,3 +1,4 @@
+const axios = require('axios')
 const Promise = require('bluebird')
 const retry = require('bluebird-retry')
 const couchbase = require('couchbase')
@@ -5,120 +6,147 @@ const _ = require('lodash')
 const { exec } = require('child_process')
 
 const ONE_MINUTE = 60000
-const config = {
-  connString: process.env.COUCHBASE_URI || 'couchbase://127.0.0.1?detailed_errcodes=1&operation_timeout=180.0',
-  username: process.env.COUCHBASE_USER || 'couchbase',
-  password: process.env.COUCHBASE_PASS || 'couchbase',
-  bucketName: process.env.COUCHBASE_BUCKET || 'mkpremium_test'
-}
 
-const bucketName = config.bucketName
-const cluster = Promise.promisifyAll(new couchbase.Cluster(config.connString))
-
-cluster.authenticate(config.username, config.password)
-const clusterManager = Promise.promisifyAll(cluster.manager())
-
-const createBucket = () => clusterManager.createBucketAsync(bucketName, {
-  flushEnabled: 1,
-  ramQuotaMB: 100,
-  authType: 'none'
-}).catch(error => {
-  if (error.statusCode === 400 && _.get(error, 'response.errors.name') === 'Bucket with given name already exists') {
-    console.warn('Guessing error means that bucket already exists', {
-      error,
-      responseErrors: _.get(error, 'response.errors')
-    })
-    return 'EXISTING_BUCKET'
+function initializeBucket () {
+  const config = {
+    connString: 'couchbase://127.0.0.1?detailed_errcodes=1&operation_timeout=180.0',
+    username: 'couchbase',
+    password: 'couchbase',
+    bucketName: 'mkpremium_test'
   }
-  throw error
-}).then(existingBucket => (existingBucket ? existingBucket : 'NEW_BUCKET'))
 
-let bucketConnection
-const getBucketConnection = () => {
-  if (bucketConnection) {
-    if (bucketConnection.connected) {
-      return Promise.resolve(bucketConnection)
+  const bucketName = config.bucketName
+  const cluster = Promise.promisifyAll(new couchbase.Cluster(config.connString))
+
+  cluster.authenticate(config.username, config.password)
+  const clusterManager = Promise.promisifyAll(cluster.manager())
+
+  const createBucket = () => clusterManager.createBucketAsync(bucketName, {
+    flushEnabled: 1,
+    ramQuotaMB: 100,
+    authType: 'none'
+  }).catch(error => {
+    if (error.statusCode === 400 && _.get(error, 'response.errors.name') === 'Bucket with given name already exists') {
+      console.warn('Guessing error means that bucket already exists', {
+        error,
+        responseErrors: _.get(error, 'response.errors')
+      })
+      return 'EXISTING_BUCKET'
     }
+    throw error
+  }).then(existingBucket => (existingBucket ? existingBucket : 'NEW_BUCKET'))
+
+  let bucketConnection
+  const getBucketConnection = () => {
+    if (bucketConnection) {
+      if (bucketConnection.connected) {
+        return Promise.resolve(bucketConnection)
+      }
+    }
+
+    return new Promise((resolve, reject) => {
+      bucketConnection = cluster.openBucket(bucketName, (error) => {
+        if (error) {
+          console.error('Error opening bucket', {
+            error,
+            bucket: JSON.stringify(bucketConnection),
+            cluster: JSON.stringify(cluster),
+            clusterManager: JSON.stringify(clusterManager),
+          })
+          reject(error)
+        } else {
+          resolve(bucketConnection)
+        }
+      })
+    })
   }
 
-  return new Promise((resolve, reject) => {
-    bucketConnection = cluster.openBucket(bucketName, (error) => {
-      if (error) {
-        console.error('Error opening bucket', {
-          error,
-          bucket: JSON.stringify(bucketConnection),
-          cluster: JSON.stringify(cluster),
-          clusterManager: JSON.stringify(clusterManager),
+  console.info(`Initializating bucket with name ${bucketName}`)
+  const isBucketReadyCommand = 'docker exec  `docker ps --format \'{{.Names}}\'` grep -rq "The following buckets became ready on node" /opt/couchbase/var/lib/couchbase/logs/info.log'
+
+  return retry(createBucket, { max_tries: 3, interval: ONE_MINUTE / 6 })
+    .then((bucketSource) => {
+      console.info('Waiting for bucket to be ready', { bucketSource })
+      return retry(() => {
+        return new Promise((resolve, reject) => {
+          const child = exec(isBucketReadyCommand, (error) => {
+            if (error || child.exitCode !== 0) {
+              reject(error || child.exitCode)
+            } else {
+              resolve()
+            }
+          })
         })
-        reject(error)
-      } else {
-        resolve(bucketConnection)
-      }
+      }, {
+        interval: ONE_MINUTE / 4,
+        timeout: 5 * ONE_MINUTE,
+      })
     })
-  })
+    .then(() => {
+      let bucket, bucketManager
+      return retry(() => getBucketConnection()
+          .then(conn => {
+            bucket = conn
+            bucketManager = Promise.promisifyAll(bucket.manager())
+            console.log('Trying to create primary index')
+            return bucketManager.createPrimaryIndexAsync({ name: `${bucketName}_primary`, ignoreIfExists: true })
+              .catch(error => {
+                console.error('Error on primary index creation attempt', { error })
+                throw error
+              })
+          }),
+        {
+          interval: ONE_MINUTE / 4,
+          timeout: 2 * ONE_MINUTE,
+          predicate: error => error.code !== couchbase.errors.authError
+        }
+      )
+        .catch(error => {
+          console.error('Primary index creation failed', { error })
+          throw error
+        })
+        .then(() => {
+          console.info('Creating application indexes')
+          return bucketManager.createIndexAsync(
+            `${bucketName}_document-type`,
+            [ '_documentType' ],
+            {
+              ignoreIfExists: true, deferred: false
+            }
+          )
+        })
+    })
+    .then(() => {
+      console.info('Done!')
+      return process.exit()
+    })
+    .catch(error => {
+      console.error({ error, stack: error.stack })
+      process.exit(1)
+    })
 }
 
-console.info(`Initializating bucket with name ${bucketName}`)
-const isBucketReadyCommand = 'docker exec  `docker ps --format \'{{.Names}}\'` grep -rq "The following buckets became ready on node" /opt/couchbase/var/lib/couchbase/logs/info.log'
-retry(createBucket, { max_tries: 3, interval: ONE_MINUTE / 6 })
-  .then((bucketSource) => {
-    console.info('Waiting for bucket to be ready', { bucketSource })
-    return retry(() => {
-      return new Promise((resolve, reject) => {
-        const child = exec(isBucketReadyCommand, (error) => {
-          if (error || child.exitCode !== 0) {
-            reject(error || child.exitCode)
-          } else {
-            resolve()
-          }
-        })
-      })
-    }, {
-      interval: ONE_MINUTE / 4,
-      timeout: 5 * ONE_MINUTE,
-    })
+function createCluster () {
+  const httpClient = axios.create({
+    baseURL: 'http://localhost:8091/',
+    auth: {
+      username: 'couchbase',
+      password: 'couchbase'
+    },
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
   })
+
+  return httpClient.post('pools/default', `memoryQuota=512&indexMemoryQuota=512`)
+    .then(() => httpClient.post('node/controller/setupServices', 'services=kv%2cn1ql%2Cindex'))
+    .then(() => httpClient.post('settings/web', 'port=8091&username=couchbase&password=couchbase'))
+    .then(() => httpClient.post('settings/indexes', 'storageMode=forestdb'))
+}
+
+createCluster()
+  .then(() => initializeBucket())
   .then(() => {
-    let bucket, bucketManager
-    return retry(() => getBucketConnection()
-        .then(conn => {
-          bucket = conn
-          bucketManager = Promise.promisifyAll(bucket.manager())
-          console.log('Trying to create primary index')
-          return bucketManager.createPrimaryIndexAsync({ name: `${bucketName}_primary`, ignoreIfExists: true })
-            .catch(error => {
-              console.error('Error on primary index creation attempt', { error })
-              throw error
-            })
-        }),
-      {
-        interval: ONE_MINUTE / 4,
-        timeout: 2 * ONE_MINUTE,
-        predicate: error => error.code !== couchbase.errors.authError
-      }
-    )
-      .catch(error => {
-        console.error('Primary index creation failed', { error })
-        throw error
-      })
-      .then(() => {
-        console.info('Creating application indexes')
-        return bucketManager.createIndexAsync(
-          `${bucketName}_document-type`,
-          [ '_documentType' ],
-          {
-            ignoreIfExists: true, deferred: false
-          }
-        )
-      })
-  })
-  .then(() => {
-    console.info('Done!')
-    return process.exit()
+    console.log('Done!')
   })
   .catch(error => {
-    console.error({ error, stack: error.stack })
-    process.exit(1)
+    console.error('Something failed', { error })
   })
-
-
