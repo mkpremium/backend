d0127ec5f30c368b626b1f4ecc4201e1dfaf61bf
Jorge Velasco <jorge.velasco.silva@gmail.com>
2021-04-16 18:46:55 +0200

    Retry on temporal connection errors.

--- src/db/couchbase.adapter.js
+++ src/db/couchbase.adapter.js
@@ -1,11 +1,12 @@
 import t from 'tcomb'
 import fromJSON from 'tcomb/lib/fromJSON'
 import uuid from 'uuid/v4'
-import { errors, N1qlQuery } from 'couchbase'
+import { errors as couchbaseErrors, errors, N1qlQuery } from 'couchbase'
 import { EntityNotFound, QueryError, QueryTimeout } from './errors'
 import { CouchbaseRecordToDomain } from '../infrastructure/couchbase/record-to-domain'
 import { validate } from 'tcomb-validation'
 import { WrongStructRecord } from '../infrastructure/wrong-struct-record.error'
+import retry from 'bluebird-retry'
 
 export class CouchbaseAdapter {
   constructor (couchbaseBucket, forceMaxQueryConsistency) {
@@ -26,34 +27,37 @@ export class CouchbaseAdapter {
       throw new WrongStructRecord(data._documentType, validationResult.errors, data)
     }
 
-    const result = await this.couchbaseBucket.upsertToDb(dataWithId.id, dataWithId)
+    const result = await this.withRetry(() => this.couchbaseBucket.upsertToDb(dataWithId.id, dataWithId))
 
     return fromJSON(result, structType)
   }
 
   getEntity (structType, entityId) {
-    return this.couchbaseBucket.getAsync(entityId)
-      .catch(error => {
-        if (error.code === errors.keyNotFound) {
-          throw new EntityNotFound(entityId, structType)
-        }
-        throw error
-      })
-      .then(result => {
-        const parsedRecord = fromJSON(result.value, structType)
-        if (CouchbaseRecordToDomain.is(parsedRecord)) {
-          return parsedRecord.couchbaseToDomain()
-        }
+    return this.withRetry(
+      () => this.couchbaseBucket.getAsync(entityId)
+        .catch(error => {
+          if (error.code === errors.keyNotFound) {
+            throw new EntityNotFound(entityId, structType)
+          }
+          throw error
+        })
+        .then(result => {
+          const parsedRecord = fromJSON(result.value, structType)
+          if (CouchbaseRecordToDomain.is(parsedRecord)) {
+            return parsedRecord.couchbaseToDomain()
+          }
 
-        return parsedRecord
-      })
+          return parsedRecord
+        })
+    )
   }
 
   queryAsync (query, params) {
     if (this.forceMaxQueryConsistency) {
       query = query.consistency(N1qlQuery.Consistency.STATEMENT_PLUS)
     }
-    return this.couchbaseBucket.queryAsync(query, params)
+
+    return this.withRetry(() => this.couchbaseBucket.queryAsync(query, params)
       .catch(error => {
         if (error.responseBody === '') {
           throw new QueryTimeout(query)
@@ -64,6 +68,7 @@ export class CouchbaseAdapter {
           throw error
         }
       })
+    )
   }
 
   throwCouchbaseError (error, query) {
@@ -73,5 +78,13 @@ export class CouchbaseAdapter {
     } else {
       throw error
     }
+  }
+
+  withRetry (fn) {
+    return retry(fn, {
+      maxTries: 3,
+      interval: 100,
+      predicate: ({ code }) => code === couchbaseErrors.temporaryError
+    })
   }
 }
