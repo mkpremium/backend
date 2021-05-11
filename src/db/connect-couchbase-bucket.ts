12fc4a776b31c749b440f1431c97b0715afe19a1
Jorge Velasco <jorge.velasco.silva@gmail.com>
2021-05-10 20:04:50 -0400

    Add retry for connecting to Couchbase.

--- src/db/connect-couchbase-bucket.ts
+++ src/db/connect-couchbase-bucket.ts
@@ -1,21 +1,28 @@
 import { Cluster } from 'couchbase'
 import { CouchbaseModel } from './model'
+import retry from 'bluebird-retry'
 
 const config = {
-    uri: process.env.COUCHBASE_URI || 'couchbase://127.0.0.1?detailed_errcodes=1',
-    bucketName: process.env.COUCHBASE_BUCKET || 'mkpremium',
-    username: process.env.COUCHBASE_USER || 'couchbase',
-    password: process.env.COUCHBASE_PASS || 'couchbase',
+  uri: process.env.COUCHBASE_URI || 'couchbase://127.0.0.1?detailed_errcodes=1',
+  bucketName: process.env.COUCHBASE_BUCKET || 'mkpremium',
+  username: process.env.COUCHBASE_USER || 'couchbase',
+  password: process.env.COUCHBASE_PASS || 'couchbase',
 }
 
 
 export function connectCouchbaseBucket() {
-    return Cluster.connect(config.uri, {
-        username: config.username,
-        password: config.password
+  return retry(
+    () => Cluster.connect(config.uri, {
+      username: config.username,
+      password: config.password
     }).then(cluster => cluster.bucket(config.bucketName))
       .then(bucket => {
-          CouchbaseModel.setCouchbaseBucket(bucket)
-          return bucket
-      })
+        CouchbaseModel.setCouchbaseBucket(bucket)
+        return bucket
+      }),
+    {
+      maxTries: 3,
+      interval: 100,
+    }
+  )
 }
