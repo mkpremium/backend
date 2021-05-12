5f33eb0b8d9cf5394937f5f9d59c5f77b7780720
Jorge Velasco <jorge.velasco.silva@gmail.com>
2021-05-12 11:07:00 -0400

    Extract http client to create couchbase cluster.

--- scripts/couchbase/create-cluster.js
+++ scripts/couchbase/create-cluster.js
@@ -1,15 +1,6 @@
-const axios = require('axios')
+const { httpClient } = require('./http-client');
 
 module.exports = () => {
-  const httpClient = axios.create({
-    baseURL: 'http://localhost:8091/',
-    auth: {
-      username: 'couchbase',
-      password: 'couchbase'
-    },
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-  })
-
   return httpClient.post('pools/default', `memoryQuota=512&indexMemoryQuota=512`)
     .then(() => httpClient.post('node/controller/setupServices', 'services=kv%2cn1ql%2Cindex'))
     .then(() => httpClient.post('settings/web', 'port=8091&username=couchbase&password=couchbase'))
