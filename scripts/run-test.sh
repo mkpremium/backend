ad92de0c69135927e37c7bc039014894068a0ea5
Jorge Velasco <jorge.velasco.silva@gmail.com>
2021-05-15 13:25:57 -0400

    Accept arguments in run test script.

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-set -e
+set -xe
 
 readonly test_dir=$1
+
+ALL_ARGS=("$@")
+TEST_ARGS=("${ALL_ARGS[@]:1}")
 
 export NODE_ENV=test
 npx mocha --config "${PWD}/${test_dir}/.mocharc.js" \
   --retries 3 \
   -r ts-node/register \
-  "${test_dir}/**/*.spec.[j|t]s"
+  --extension js --extension ts \
+  "${TEST_ARGS[@]}" "${test_dir}/**/*.spec.[j|t]s"
