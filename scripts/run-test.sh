7dcca39ae95cb5a939c4e6dec2eb4728a1b83aa9
Jorge Velasco <jorge.velasco.silva@gmail.com>
2021-05-20 19:44:25 -0400

    Trace warning when running app locally.

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -10,6 +10,7 @@ TEST_ARGS=("${ALL_ARGS[@]:1}")
 export NODE_ENV=test
 npx mocha --config "${PWD}/${test_dir}/.mocharc.js" \
   --retries 3 \
+  --trace-warnings \
   -r ts-node/register \
   --extension js --extension ts \
   "${TEST_ARGS[@]}" "${test_dir}/**/*.spec.[j|t]s"
