58c52d69030a2272c532b0f987a521b92a4d4f46
Jorge Velasco <jorge.velasco.silva@gmail.com>
2021-05-27 15:47:11 -0400

    Change delete scheduled event controller to Typescript.

--- src/scheduled-events/controller/delete-scheduled-event.controller.ts
+++ src/scheduled-events/controller/delete-scheduled-event.controller.ts
@@ -1,18 +1,15 @@
 import { ScheduledEventsRepository } from '../repository/schedule-events.repository'
 import { SCHEDULED_EVENT_DELETED } from '../controllers'
+import { EventBus } from '../../infrastructure/event-bus'
 
-class ScheduledEventDeleted {
-  constructor (id) {
-    this.id = id
-    this.name = SCHEDULED_EVENT_DELETED
-  }
-}
-
-const deleteScheduledEvent = eventBus => async (req, res) => {
+const deleteScheduledEvent = (eventBus: EventBus) => async (req, res) => {
   const id = req.params.id
   const repo = new ScheduledEventsRepository()
   await repo.delete(id)
-  eventBus.publish(new ScheduledEventDeleted(id))
+  eventBus.publish({
+    id,
+    name: SCHEDULED_EVENT_DELETED,
+  })
   res.status(204).send()
 }
 
