f72d7e44c8e85011e3d40ed80efa802e7ba21b02
Jorge Velasco <jorge.velasco.silva@gmail.com>
2021-06-04 11:35:39 -0400

    Check for proposal to be send right after a proposal is scheduled.

--- src/building/event-listener/proposal-added.listener.ts
+++ src/building/event-listener/proposal-added.listener.ts
@@ -1,15 +1,20 @@
 import { ProposalForBuildingScheduled } from '../service/add-proposal-for-building.service'
 import { UpdateBuildingNegotiationStatusService } from '../service/update-building-negotiation-status.service'
+import { ProposalsSenderService } from '../service/proposals-sender.service'
 
 interface Deps {
-  updateBuildingNegotiationStatusService: UpdateBuildingNegotiationStatusService
+  updateBuildingNegotiationStatusService: UpdateBuildingNegotiationStatusService,
+  proposalsSenderService: ProposalsSenderService,
 }
 
-export const createProposalScheduledListener = ({ updateBuildingNegotiationStatusService }: Deps) => (event: ProposalForBuildingScheduled) => {
+export const createProposalScheduledListener = ({
+                                                  updateBuildingNegotiationStatusService,
+                                                  proposalsSenderService
+                                                }: Deps) => (event: ProposalForBuildingScheduled) => {
   return updateBuildingNegotiationStatusService.updateBuildingStatus(event.buildingId, {
     status: 'PROPOSAL_SCHEDULED',
     sourceOwnerId: event.ownerId,
     userId: event.createdBy,
-  })
+  }).then(() => proposalsSenderService.checkAndSendProposals())
 }
 
