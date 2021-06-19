697eb6d185d52a80eb15ce635232e1c38b2fc061
Jorge Velasco <jorge.velasco.silva@gmail.com>
2021-06-19 11:57:36 -0400

    Remove `system-preferences` module.

--- src/system-preferences/index.js
+++
@@ -1,8 +0,0 @@
-import './types'
-import routes from './routes'
-import jwt, { permissions } from '../middleware/jwt'
-
-export default (app) => {
-  const secured = jwt()
-  app.use('/system-preferences', secured, permissions.admin, routes)
-}
