3b44605f49ff4ea293bf3b2863422200a07a72f6
Jorge Velasco <jorge.velasco.silva@gmail.com>
2021-07-04 18:46:23 -0400

    Cache DI container and Couchbase connection to speed up tests.

--- test/create-test-container.ts
+++ test/create-test-container.ts
@@ -1,13 +1,30 @@
 import { createDiContainer } from '../src/infrastructure/dependencies'
 import { connectCouchbaseBucket } from '../src/db/connect-couchbase-bucket'
+import { AwilixContainer } from 'awilix'
+import { Bucket } from 'couchbase'
+
+let cachedContainer: AwilixContainer
+let cachedBucket: Bucket
 
 export const createTestContainer = () => {
-  return connectCouchbaseBucket()
-    .then(bucket => Promise.all([
-      new Promise(resolve => {
-        bucket.manager().flush(() => setTimeout(resolve, 500))
-      }),
-      createDiContainer(bucket)
-    ]))
-    .then(([ _, container ]) => container)
+  const bucketPromise = new Promise(async resolve => {
+    if (!cachedBucket) {
+      cachedBucket = await connectCouchbaseBucket()
+    }
+    resolve(cachedBucket)
+  })
+  return bucketPromise
+    .then((bucket: Bucket) => Promise.all([
+        new Promise(resolve => {
+          bucket.manager().flush(() => setTimeout(resolve, 500))
+        }),
+        new Promise(async resolve => {
+          if (!cachedContainer) {
+            cachedContainer = await createDiContainer(bucket)
+          }
+          resolve(cachedContainer)
+        })
+      ])
+    )
+    .then(([ _, container ]) => container as AwilixContainer)
 }
