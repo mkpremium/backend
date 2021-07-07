ee63e8f9875273270457c14663460400c3942177
Jorge Velasco <jorge.velasco.silva@gmail.com>
2021-07-07 10:59:49 -0400

    Increase bucket RAM quota.

--- scripts/couchbase/create-bucket.js
+++ scripts/couchbase/create-bucket.js
@@ -19,7 +19,7 @@ module.exports = (bucketName = 'mkpremium_test') => {
 
   const createBucket = () => clusterManager.createBucketAsync(bucketName, {
     flushEnabled: 1,
-    ramQuotaMB: 100,
+    ramQuotaMB: 256,
     authType: 'none'
   }).catch(error => {
     if (error.statusCode === 400 && _.get(error, 'response.errors.name') === 'Bucket with given name already exists') {
