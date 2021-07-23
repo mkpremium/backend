7910a50bfa834d9c90a3386b981bbe12a18a34a0
Jorge Velasco <jorge.velasco.silva@gmail.com>
2021-07-23 15:22:56 -0400

    Change assigned caller to virtual caller.

--- src/calls/dependencies.ts
+++ src/calls/dependencies.ts
@@ -34,7 +34,7 @@ const sayAttributes = {
 
 export const setupCallsDependencies = (container: AwilixContainer) => {
   const config: VirtualCallerConfig = {
-    assignedCallerIdForVirtualCalls: 'ba7966fc-f05d-48a2-bb49-e17a08a6a038',
+    assignedCallerIdForVirtualCalls: '56ecc194-b998-43b5-a118-62e40b69aa84',
     virtualCallerQueueId: 'e1748e7d-8714-45c0-a831-c0f42d6d564f',
     virtualCallerId: 'virtual-caller-barcelona-2',
     maxWorksheets: undefined,
