5362dfb4f258d108421dfd72708d039353c65672
Jorge Velasco <jorge.velasco.silva@gmail.com>
2021-07-24 19:23:59 -0400

    Get virtual caller from repository in call finished listener.

--- src/calls/event-listener/call-finished.listener.ts
+++ src/calls/event-listener/call-finished.listener.ts
@@ -1,21 +1,18 @@
 import { VirtualCallerSupervisorService } from '../service/virtual-caller-supervisor.service'
-import { VirtualCallerConfig } from '../virtual-caller.config'
 import { Logger } from 'winston'
 import { CallDone } from '../controller/call-done-webhook.controller'
-import { VirtualCaller } from '../domain/virtual-caller'
+import { VirtualCallersRepository } from '../repository/virtual-callers.repository'
 
 interface Deps {
-  virtualCallerSupervisor: VirtualCallerSupervisorService;
-  virtualCallerConfig: VirtualCallerConfig;
-  logger: Logger;
-  virtualCallerPhoneNumber: string
+  virtualCallerSupervisor: VirtualCallerSupervisorService
+  virtualCallersRepository: VirtualCallersRepository
+  logger: Pick<Logger, 'info'>
 }
 
 export const createCallFinishedListener = ({
                                              virtualCallerSupervisor,
-                                             virtualCallerConfig,
                                              logger,
-                                             virtualCallerPhoneNumber,
+                                             virtualCallersRepository,
                                            }: Deps) => async (evt: CallDone) => {
   logger.info('Call finished, checking for more work', { callId: evt.callId })
   // Failed calls respond quickly, so wait to avoid multiple calls to same number.
@@ -24,17 +21,7 @@ export const createCallFinishedListener = ({
   await waitPromise
 
   return virtualCallerSupervisor.check({
-    caller: VirtualCaller({
-      assignCallsTo: virtualCallerConfig.assignedCallerIdForVirtualCalls,
-      id: virtualCallerConfig.virtualCallerId,
-      isEnabled: true,
-      language: 'spanish',
-      name: virtualCallerConfig.virtualCallerId,
-      phoneNumber: virtualCallerPhoneNumber,
-      queueId: virtualCallerConfig.virtualCallerQueueId,
-      timezone: 'Europe/Madrid'
-    }),
-    maxWorksheets: virtualCallerConfig.maxWorksheets,
+    caller: await virtualCallersRepository.get(evt.callerId),
     lastWorksheetId: evt.worksheetId,
     lastOwnerResponse: evt.ownerResponse,
   })
