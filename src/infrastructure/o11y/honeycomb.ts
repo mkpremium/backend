9e23b3dad3e12aabceed5f2dfb9da98a0ee435bd
Jorge Velasco <jorge.velasco.silva@gmail.com>
2021-07-29 18:45:52 -0400

    Disable Honeycomb instrumentation in e2e tests.

--- src/infrastructure/o11y/honeycomb.ts
+++ src/infrastructure/o11y/honeycomb.ts
@@ -3,5 +3,6 @@ import honeycomb from 'honeycomb-beeline'
 honeycomb({
   writeKey: process.env.HONEYCOMB_WRITE_KEY,
   dataset: 'backend',
-  serviceName: 'api'
+  serviceName: 'api',
+  disableInstrumentation: process.env.NODE_ENV === 'test'
 })
