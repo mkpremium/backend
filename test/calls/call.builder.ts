76d760978844b246fbe40aae8fc3b4b73fab120b
Jorge Velasco <jorge.velasco.silva@gmail.com>
2021-08-11 17:25:17 -0400

    Add default call values in call builder prototype.

--- test/calls/call.builder.ts
+++ test/calls/call.builder.ts
@@ -1,14 +1,18 @@
 import { VirtualAgentCall, VirtualAgentCallProps } from '../../src/calls/virtual-agent-call'
 
 const callPrototype: VirtualAgentCallProps = {
+  id: 'test-call-id',
   createdAt: new Date(),
   status: 'FAILED',
-  id: '',
-  callerId: '',
-  contactId: '',
-  ownerId: '',
-  phoneNumber: '',
-  worksheetId: '',
+  callerId: 'test-caller-id',
+  contactId: 'test-contact-id',
+  ownerId: 'test-owner-id',
+  phoneNumber: '+34611111111',
+  worksheetId: 'test-worksheet-id',
+  error: null,
+  ownerResponse: null,
+  gatheredAt: null,
+  finishedAt: null,
 }
 
 export function callBuilder (overrides: Partial<VirtualAgentCallProps> = {}) {
