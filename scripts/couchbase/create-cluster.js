7144abc3859fa2476666a6c47e96352d8bb9e6fa
Jorge Velasco <jorge.velasco.silva@gmail.com>
2021-08-12 18:21:08 -0400

    Increase cluster memory for local couchbase.

--- scripts/couchbase/create-cluster.js
+++ scripts/couchbase/create-cluster.js
@@ -1,7 +1,7 @@
 const { httpClient } = require('./http-client');
 
 module.exports = () => {
-  return httpClient.post('pools/default', `memoryQuota=1024&indexMemoryQuota=512`)
+  return httpClient.post('pools/default', `memoryQuota=2048&indexMemoryQuota=1024`)
     .then(() => httpClient.post('node/controller/setupServices', 'services=kv%2cn1ql%2Cindex'))
     .then(() => httpClient.post('settings/web', 'port=8091&username=couchbase&password=couchbase'))
     .then(() => httpClient.post('settings/indexes', 'storageMode=forestdb'))
