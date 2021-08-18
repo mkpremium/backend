a5d294a3fb0cd9cfc8551ec67ef0127a1d64b0c8
Jorge Velasco <jorge.velasco.silva@gmail.com>
2021-08-18 17:41:43 -0400

    Send message in phone number language.

--- src/calls/service/sms-message.service.ts
+++ src/calls/service/sms-message.service.ts
@@ -1,3 +1,5 @@
+import { Twilio } from 'twilio'
+
 interface SendMessageToUnreachedOwner {
   to: string
   callId: string
@@ -8,6 +10,20 @@ interface SendMessageToUnreachedOwner {
 }
 
 export class SmsMessageSender {
+  constructor (
+    private twilioClient: Twilio,
+    private publicUrl: string,
+  ) {
+  }
+
   async sendMessageToUnreachedOwner (cmd: SendMessageToUnreachedOwner) {
+    const message = cmd.to.startsWith('+351') ?
+      'Olá, eu chamei você para o seu imóvel do "endereço" da "cidade". Se você está interessado em vender, responda a esta mensagem. Obrigado.' :
+      'Hola, le he llamado por su propiedad de la “direccion“ de “ciudad”. Si le interesa vender conteste a este mensaje. Gracias.'
+    await this.twilioClient.messages.create({
+      body: message,
+      from: '',
+      to: cmd.to,
+    })
   }
 }
--- test/calls/service/sms-message.service.spec.ts
+++ test/calls/service/sms-message.service.spec.ts
@@ -1,14 +1,41 @@
 import { expect } from 'chai'
+import { stub } from 'sinon'
 import { SmsMessageSender } from '../../../src/calls/service/sms-message.service'
 
 describe('SmsMessageSender', () => {
   let service: SmsMessageSender
+  let twilioClientStub
+  let testPublicUrl = 'https://api.mkpremium.net'
+  const testCmd = {
+    to: '',
+    callId: '',
+    callerId: '',
+    contactId: '',
+    ownerId: '',
+    worksheetId: '',
+  }
 
   beforeEach(() => {
-    service = new SmsMessageSender()
+    twilioClientStub = {
+      messages: {
+        create: stub().resolves(),
+      }
+    }
+    service = new SmsMessageSender(
+      twilioClientStub,
+      testPublicUrl
+    )
   })
 
-  it('works', () => {
-    expect(service).to.be.ok
+  it('sends Spanish message to Spain numbers', async () => {
+    await service.sendMessageToUnreachedOwner({ ...testCmd, to: '+34666666666' })
+
+    expect(twilioClientStub.messages.create).to.have.been.calledWithMatch(({ body }) => body.startsWith('Hola,'))
+  })
+
+  it('sends Portuguese message to Portugal numbers', async () => {
+    await service.sendMessageToUnreachedOwner({ ...testCmd, to: '+351999999999' })
+
+    expect(twilioClientStub.messages.create).to.have.been.calledWithMatch(({ body }) => body.startsWith('Olá,'))
   })
 })
