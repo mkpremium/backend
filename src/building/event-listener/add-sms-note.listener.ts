8932036d818ef1cb7bdb29f9230b41ada7578d00
Jorge Velasco <jorge.velasco.silva@gmail.com>
2021-08-22 19:16:45 -0400

    Add owner note to building.

--- src/building/event-listener/add-sms-note.listener.ts
+++ src/building/event-listener/add-sms-note.listener.ts
@@ -1,6 +1,12 @@
 import { SmsReceived } from '../../calls/service/sms-webhook.processor'
+import { BuildingNotesRepository } from '../repository/building-notes.repository'
 
-export function addSmsNoteListener() {
+export function addSmsNoteListener ({ buildingNotesRepository }: { buildingNotesRepository: BuildingNotesRepository }) {
   return async function (evt: SmsReceived): Promise<void> {
+    await buildingNotesRepository.save({
+      note: `${evt.message}[SMS del propietario]`,
+      createdBy: evt.ownerId,
+      buildingId: evt.buildingId,
+    })
   }
 }
--- test/building/event-listener/add-sms-note.listener.spec.ts
+++ test/building/event-listener/add-sms-note.listener.spec.ts
@@ -1,15 +1,34 @@
 import { SmsReceived } from '../../../src/calls/service/sms-webhook.processor'
 import { addSmsNoteListener } from '../../../src/building/event-listener/add-sms-note.listener'
 import { expect } from 'chai'
+import { stub } from 'sinon'
 
 describe('addSmsNoteListener', () => {
   let listener: (evt: SmsReceived) => Promise<void>
+  let buildingNotesRepositoryStub
 
   beforeEach(() => {
-    listener = addSmsNoteListener()
+    buildingNotesRepositoryStub = {
+      save: stub().resolves()
+    }
+
+    listener = addSmsNoteListener({ buildingNotesRepository: buildingNotesRepositoryStub })
   })
 
-  it('works', () => {
-    expect(listener).to.be.ok
+  it('adds note to building', async () => {
+    const testSmsReceivedEvent: SmsReceived = {
+      name: 'virtual-caller.sms-received',
+      buildingId: 'test-building-id',
+      message: 'test message',
+      ownerId: 'test-owner-id'
+    }
+    await listener(testSmsReceivedEvent)
+
+    expect(buildingNotesRepositoryStub.save).to.have.been.called
+    const savedNote = buildingNotesRepositoryStub.save.lastCall.firstArg
+    expect(savedNote.note).to.include(testSmsReceivedEvent.message)
+    expect(savedNote.note).to.include('SMS')
+    expect(savedNote.createdBy).to.be.equal(testSmsReceivedEvent.ownerId)
+    expect(savedNote.buildingId).to.be.equal(testSmsReceivedEvent.buildingId)
   })
 })
