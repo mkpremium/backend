6b8bf7337bced56997312ae9b47944ddfe2046cf
Jorge Velasco <jorge.velasco.silva@gmail.com>
2021-08-27 11:28:55 -0400

    Hide Twilio incoming SMS endpoint.

--- src/calls/routing.ts
+++ src/calls/routing.ts
@@ -13,7 +13,7 @@ export const callsRoutes = (container: AwilixContainer, app: Express) => {
   router.post('/twilio/:callId/gather', wrap(container.resolve('inputGatheredWebhookController')))
   router.post('/twilio/:callId/done', wrap(container.resolve('callFinishedWebhookController')))
   router.post('/twilio/:callId/machine-detection', wrap(container.resolve('machineDetectionWebhookController')))
-  router.post('/twilio/sms', wrap(container.resolve('twilioSMSWebhookController')))
+  // router.post('/twilio/sms', wrap(container.resolve('twilioSMSWebhookController')))
   router.post('/twilio/incoming', wrap(container.resolve('twilioIncomingCallController')))
   router.post('/virtual-caller/start', wrap(container.resolve('startVirtualCallerController')))
   router.post('/virtual-caller/today', wrap(container.resolve('virtualCallerTodayStatsController')))
@@ -29,7 +29,7 @@ export const callsRoutes = (container: AwilixContainer, app: Express) => {
       /^\/calls\/twilio\/[0-9a-z-]+\/gather$/,
       /^\/calls\/twilio\/[0-9a-z-]+\/done/,
       /^\/calls\/twilio\/[0-9a-z-]+\/machine-detection$/,
-      /^\/calls\/twilio\/sms$/,
+      // /^\/calls\/twilio\/sms$/,
       /^\/calls\/twilio\/incoming$/,
     ]
   })
