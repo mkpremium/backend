37aea1449115cf90d31369ff0025f8b0afc773dc
Jorge Velasco <jorge.velasco.silva@gmail.com>
2021-09-29 16:47:21 -0300

    Don't return `scheduledCalls` in owner search.
    Is not use and makes query unnecessarly slow.

--- src/owner/repository/owner.repository.ts
+++ src/owner/repository/owner.repository.ts
@@ -32,8 +32,7 @@ meta(worksheet).id worksheetId,
     "inPerson": lastEvent.event.inPerson,
     "ownerId": lastEvent.event.ownerId,
     "flipperName": lastEventFlipper.profile.firstName || ' ' || lastEventFlipper.profile.lastName
-} AS lastEvent,
-ARRAY {"at": sc.eventDate} FOR sc IN  scheduledCalls END as scheduledCalls
+} AS lastEvent
 
 FROM ${bucketName} owner
 JOIN ${bucketName} building ON building._documentType = 'building'
@@ -47,9 +46,6 @@ LEFT JOIN ${bucketName} lastEventFlipper ON
     (lastEvent IS NOT NULL AND lastEvent IS NOT MISSING)
     AND lastEventFlipper._documentType = 'operator'
     AND meta(lastEventFlipper).id = lastEvent.notifyTo
-LEFT NEST ${bucketName} scheduledCalls ON scheduledCalls._documentType = 'scheduled-event'
-    AND scheduledCalls.type = 'CALLS'
-    AND scheduledCalls.event.ownerId = meta(owner).id
 
 WHERE owner._documentType = 'owner'
 AND ANY c IN owner.person.contacts SATISFIES c.\`value\` = $1 AND c.status != 'BAD' END
@@ -70,9 +66,6 @@ const FoundOwner = t.struct({
   buildingId: t.String,
   negotiationStatus: NegotiationStatus,
   worksheetId: t.String,
-  scheduledCalls: t.list(t.struct({
-    at: DateTimeString
-  })),
   matchingContactId: t.String,
   name: t.String,
   contacts: t.list(t.struct({
