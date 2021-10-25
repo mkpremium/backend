33d069d0a7e7ae5d2e20a2c003d5e103980b7ed2
Jorge Velasco <jorge.velasco.silva@gmail.com>
2021-10-25 16:37:45 -0300

    Expose list leads endpoint.

--- src/flipper/dependencies.ts
+++ src/flipper/dependencies.ts
@@ -3,12 +3,17 @@ import { FlipperAvailabilityService } from './service/flipper-availability.servi
 import { SetFlipperMaxLineService } from './service/set-flipper-max-line.service'
 import { createFlipperBlockedAvailabilityController } from './controller/flipper-availability.controller'
 import { createSetFlipperMaxLineController } from './controller/set-flipper-max-line.controller'
+import { listLeadsController } from './controller/list-leads.controller'
+import { FlipperLeadsService } from './service/flipper-leads.service'
 
 export const setupFlipperDependencies = (container: AwilixContainer) => {
   container.register({
     flipperAvailabilityService: asClass(FlipperAvailabilityService).singleton(),
     setFlipperMaxLineService: asClass(SetFlipperMaxLineService).singleton(),
     flipperBlockedAvailabilityController: asFunction(createFlipperBlockedAvailabilityController).singleton(),
-    setFlipperMaxLineController: asFunction(createSetFlipperMaxLineController).singleton()
+    setFlipperMaxLineController: asFunction(createSetFlipperMaxLineController).singleton(),
+
+    listLeadsController: asFunction(listLeadsController).singleton(),
+    flipperLeadsService: asClass(FlipperLeadsService).classic().singleton(),
   })
 }
