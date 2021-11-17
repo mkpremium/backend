2b91dc3e22ab4a8ed80de362ae7ff2d13a8725fa
Jorge Velasco <jorge.velasco.silva@gmail.com>
2021-11-17 18:26:45 -0300

    Start callers asynchronously.

--- src/calls/controller/virtual-caller-start.controller.ts
+++ src/calls/controller/virtual-caller-start.controller.ts
@@ -1,25 +1,33 @@
 import { VirtualCallerSupervisorService } from '../service/virtual-caller-supervisor.service'
 import { VirtualCallersRepository } from '../repository/virtual-callers.repository'
+import { Logger } from '../../infrastructure/logger'
 
 interface Deps {
   virtualCallerSupervisor: VirtualCallerSupervisorService
   virtualCallersRepository: VirtualCallersRepository
+  logger: Logger
 }
 
 export const createStartVirtualCallerController = ({
                                                      virtualCallerSupervisor,
                                                      virtualCallersRepository,
+                                                     logger,
                                                    }: Deps) => async (req, res) => {
-  const enabledCallers = await virtualCallersRepository.enabledCallers()
+  res.sendStatus(202)
 
-  return Promise.all(enabledCallers.map(caller => {
-      return virtualCallerSupervisor.check({
+  const enabledCallers = await virtualCallersRepository.enabledCallers()
+  logger.info('Virtual callers to check', { count: enabledCallers.length })
+
+  for (const caller of enabledCallers) {
+    try {
+      await virtualCallerSupervisor.check({
         caller,
         lastWorksheetId: undefined,
         lastOwnerResponse: undefined,
       })
-        .then(() => `${caller.id} => success`)
-        .catch(error => `${caller.id} => failed (${error.message})`)
-    })
-  ).then(result => res.json(result))
+      logger.info('Caller started', { callerId: caller.id })
+    } catch (error) {
+      logger.error('Caller start failed', { callerId: caller.id, error: error.message })
+    }
+  }
 }
