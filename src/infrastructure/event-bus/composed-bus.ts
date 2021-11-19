4e694518c5fd4e1bee0934b9fe0a63e672d16bd7
Jorge Velasco <jorge.velasco.silva@gmail.com>
2021-11-19 18:55:38 -0300

    Implement proxy methods in composed bus.

--- src/infrastructure/event-bus/composed-bus.ts
+++ src/infrastructure/event-bus/composed-bus.ts
@@ -1,20 +1,31 @@
 import { EventBus } from '../event-bus'
 import { EventEmitterBus } from './event-emitter-bus'
 import { SqsBus } from './sqs-bus'
+import { Logger } from '../logger'
 
 export class ComposedBus implements EventBus {
   info: Record<string, number>
 
   constructor (
     private sqsEventBus: SqsBus,
-    private eventEmitterBus: EventEmitterBus
+    private eventEmitterBus: EventEmitterBus,
+    private logger: Logger,
   ) {
   }
 
-  on (eventName: string, subscriberName: string, subscriber: (event: any) => Promise<any>) {
+  on (eventName: string, listenerName: string, subscriber: (event: any) => Promise<any>) {
+    this.eventEmitterBus.on(eventName, listenerName, subscriber)
+    try {
+      this.sqsEventBus.on(eventName, listenerName)
+    } catch (error) {
+      this.logger.error('Error registering listener in SQS bus', { eventName, listenerName, error: error.message })
+    }
   }
 
   publish<T extends { name: string }> (event: T): Promise<void> {
-    return Promise.resolve(undefined)
+    this.sqsEventBus.publish(event)
+      .catch((error) => this.logger.error('Error publishing event in SQS bus', { event, error: error.message }))
+
+    return this.eventEmitterBus.publish(event)
   }
 }
