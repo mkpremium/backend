3d75c789f833c232df7b99edcf30a45bd24f1a37
Jorge Velasco <jorge.velasco.silva@gmail.com>
2021-12-02 17:07:02 -0300

    Add worker to read from queue to save portugal raw buildings.

---
+++ scripts/manual-interventions/save-portugal-raw-buildings.ts
@@ -0,0 +1,69 @@
+import { connectCouchbaseBucket } from '../../src/db/connect-couchbase-bucket'
+import aws from 'aws-sdk'
+import { initLogger } from '../../src/infrastructure/logger'
+import { CouchbaseAdapter } from '../../src/db/couchbase.adapter'
+
+const logger = initLogger()
+const QueueUrl = process.env.QUEUE_URL
+
+exec()
+  .then(() => {
+    process.exit()
+  })
+  .catch(error => {
+    console.error('Oops', { error: error.message, stack: error.stack })
+    process.exit(1)
+  })
+
+async function exec () {
+  const couchbaseBucket = await connectCouchbaseBucket()
+  const sqsClient = new aws.SQS({ region: 'eu-west-1' })
+
+  return loop(sqsClient, new CouchbaseAdapter(couchbaseBucket))
+}
+
+function loop (sqsClient, couchbaseAdapter: CouchbaseAdapter, retries = 2) {
+  if (retries <= 0) {
+    return Promise.resolve()
+  }
+
+  return sqsClient.receiveMessage({
+    MaxNumberOfMessages: 1,
+    QueueUrl,
+  }).promise()
+    .then(async ({ Messages }) => {
+      if (!Messages || Messages.length === 0) {
+        return new Promise(resolve => setTimeout(resolve, 500))
+          .then(() => loop(sqsClient, couchbaseAdapter, retries - 1))
+      }
+
+      const message = Messages[ 0 ]
+      const building = JSON.parse(message.Body)
+
+      try {
+        await couchbaseAdapter.save(building.id, {
+          ...building,
+          status: 'INBOX',
+          statusChangedAt: new Date(),
+          _documentType: 'new-portugal-data',
+        })
+        logger.info('Building saved', { buildingId: building.id })
+      } catch (error) {
+        logger.error('Error processing message', {
+          building,
+          error: error,
+          errorMessage: error.message,
+          messageId: message.MessageId,
+        })
+      }
+
+      await sqsClient.deleteMessage({
+        QueueUrl: QueueUrl,
+        ReceiptHandle: message.ReceiptHandle,
+      }).promise()
+
+      return loop(sqsClient, couchbaseAdapter)
+    })
+}
+
+
