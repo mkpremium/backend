8408369d2de7d308c724f380292e70e1f36486d5
Jorge Velasco <jorge.velasco.silva@gmail.com>
2022-03-31 17:25:28 -0300

    Make `wait-for-it.sh` work in Mac.

--- docker/wait-for-it.sh
+++ docker/wait-for-it.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 #   Use this script to test if a given TCP host/port are available
 
+timeout() { perl -e 'alarm shift; exec @ARGV' "$@"; }
+
 WAITFORIT_cmdname=${0##*/}
 
 echoerr() { if [[ $WAITFORIT_QUIET -ne 1 ]]; then echo "$@" 1>&2; fi }
