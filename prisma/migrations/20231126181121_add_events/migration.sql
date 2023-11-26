eba829a62b955a09fa8cbadd3b146520a441cce3
Jorge Velasco <jorge.velasco.silva@gmail.com>
2023-11-26 15:16:42 -0300

    Add createdAt column to `DomainEvent`.

--- prisma/migrations/20231126181121_add_events/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "DomainEvent" (
-    "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "body" JSONB NOT NULL,
-
-    CONSTRAINT "DomainEvent_pkey" PRIMARY KEY ("id")
-);
