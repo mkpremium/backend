257f432b63ad796f8d86df1eff4a64d5c80e9c60
Jorge Velasco <jorge.velasco.silva@gmail.com>
2023-12-03 13:44:30 -0300

    Recreate worksheet migrations.

--- migrations/1701621756146-add_worksheet_entities.ts
+++ migrations/1701621756146-add_worksheet_entities.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from 'typeorm'
 
-export class AddWorksheetEntities1701619618438 implements MigrationInterface {
-  name = 'AddWorksheetEntities1701619618438'
+export class AddWorksheetEntities1701621756146 implements MigrationInterface {
+  name = 'AddWorksheetEntities1701621756146'
 
   public async up (queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`CREATE TABLE "worksheet_queue"
@@ -11,21 +11,22 @@ export class AddWorksheetEntities1701619618438 implements MigrationInterface {
                                  "updatedAt" TIMESTAMP NOT NULL DEFAULT now(),
                                  CONSTRAINT "PK_7af060c122da5083bba75dae8b7" PRIMARY KEY ("id")
                              )`)
-    await queryRunner.query(`CREATE TABLE "worksheet"
-                             (
-                                 "id"                  uuid                     NOT NULL DEFAULT uuid_generate_v4(),
-                                 "createdAt"           TIMESTAMP                NOT NULL DEFAULT now(),
-                                 "updatedAt"           TIMESTAMP                NOT NULL DEFAULT now(),
-                                 "status"              text                     NOT NULL DEFAULT 'LOOKING_MEETING',
-                                 "statusChangeReason"  text                     NOT NULL DEFAULT '',
-                                 "lastStatusChangedAt" TIMESTAMP WITH TIME ZONE NOT NULL,
-                                 "lastViewedAt"        TIMESTAMP WITH TIME ZONE NOT NULL,
-                                 "buildingId"          uuid,
-                                 "queueId"             uuid,
-                                 "lastViewedById"      uuid,
-                                 CONSTRAINT "REL_d41832400de343577945fb92a5" UNIQUE ("buildingId"),
-                                 CONSTRAINT "PK_4288372d711457f58abb7dd90c5" PRIMARY KEY ("id")
-                             )`)
+    await queryRunner.query(`ALTER TABLE "worksheet"
+        ADD "status" text NOT NULL DEFAULT 'LOOKING_MEETING'`)
+    await queryRunner.query(`ALTER TABLE "worksheet"
+        ADD "statusChangeReason" text NOT NULL DEFAULT ''`)
+    await queryRunner.query(`ALTER TABLE "worksheet"
+        ADD "lastStatusChangedAt" TIMESTAMP WITH TIME ZONE`)
+    await queryRunner.query(`ALTER TABLE "worksheet"
+        ADD "lastViewedAt" TIMESTAMP WITH TIME ZONE`)
+    await queryRunner.query(`ALTER TABLE "worksheet"
+        ADD "buildingId" uuid`)
+    await queryRunner.query(`ALTER TABLE "worksheet"
+        ADD CONSTRAINT "UQ_d41832400de343577945fb92a5f" UNIQUE ("buildingId")`)
+    await queryRunner.query(`ALTER TABLE "worksheet"
+        ADD "queueId" uuid`)
+    await queryRunner.query(`ALTER TABLE "worksheet"
+        ADD "lastViewedById" uuid`)
     await queryRunner.query(`ALTER TABLE "worksheet"
         ADD CONSTRAINT "FK_d41832400de343577945fb92a5f" FOREIGN KEY ("buildingId") REFERENCES "building" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION`)
     await queryRunner.query(`ALTER TABLE "worksheet"
@@ -38,7 +39,14 @@ export class AddWorksheetEntities1701619618438 implements MigrationInterface {
     await queryRunner.query(`ALTER TABLE "worksheet" DROP CONSTRAINT "FK_f7f612460e14a027386750806ba"`)
     await queryRunner.query(`ALTER TABLE "worksheet" DROP CONSTRAINT "FK_943bfad047bdb6d2f67c92253cb"`)
     await queryRunner.query(`ALTER TABLE "worksheet" DROP CONSTRAINT "FK_d41832400de343577945fb92a5f"`)
-    await queryRunner.query(`DROP TABLE "worksheet"`)
+    await queryRunner.query(`ALTER TABLE "worksheet" DROP COLUMN "lastViewedById"`)
+    await queryRunner.query(`ALTER TABLE "worksheet" DROP COLUMN "queueId"`)
+    await queryRunner.query(`ALTER TABLE "worksheet" DROP CONSTRAINT "UQ_d41832400de343577945fb92a5f"`)
+    await queryRunner.query(`ALTER TABLE "worksheet" DROP COLUMN "buildingId"`)
+    await queryRunner.query(`ALTER TABLE "worksheet" DROP COLUMN "lastViewedAt"`)
+    await queryRunner.query(`ALTER TABLE "worksheet" DROP COLUMN "lastStatusChangedAt"`)
+    await queryRunner.query(`ALTER TABLE "worksheet" DROP COLUMN "statusChangeReason"`)
+    await queryRunner.query(`ALTER TABLE "worksheet" DROP COLUMN "status"`)
     await queryRunner.query(`DROP TABLE "worksheet_queue"`)
   }
 
