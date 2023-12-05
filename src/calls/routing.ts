dd9f5d40039725b95ad1a2eaf8210536f2f1e447
Jorge Velasco <jorge.velasco.silva@gmail.com>
2023-12-05 19:14:11 -0300

    Comment unused `Twilio` endpoints.

--- src/calls/routing.ts
+++ src/calls/routing.ts
@@ -10,13 +10,13 @@ export const callsRoutes = (container: AwilixContainer, app: Express) => {
   // router.post('/token', wrap(container.resolve('callTokenGeneratorController')))
   // router.post('/twilio/voice', wrap(container.resolve('outgoingCallWebhookController')))
 
-  router.post('/twilio/:callId/gather', wrap(container.resolve('inputGatheredWebhookController')))
-  router.post('/twilio/:callId/done', wrap(container.resolve('callFinishedWebhookController')))
-  router.post('/twilio/:callId/machine-detection', wrap(container.resolve('machineDetectionWebhookController')))
-  // router.post('/twilio/sms', wrap(container.resolve('twilioSMSWebhookController')))
-  router.post('/twilio/incoming', wrap(container.resolve('twilioIncomingCallController')))
-  router.post('/virtual-caller/start', wrap(container.resolve('startVirtualCallerController')))
-  router.post('/virtual-caller/today', wrap(container.resolve('virtualCallerTodayStatsController')))
+  // router.post('/twilio/:callId/gather', wrap(container.resolve('inputGatheredWebhookController')))
+  // router.post('/twilio/:callId/done', wrap(container.resolve('callFinishedWebhookController')))
+  // router.post('/twilio/:callId/machine-detection', wrap(container.resolve('machineDetectionWebhookController')))
+  // // router.post('/twilio/sms', wrap(container.resolve('twilioSMSWebhookController')))
+  // router.post('/twilio/incoming', wrap(container.resolve('twilioIncomingCallController')))
+  // router.post('/virtual-caller/start', wrap(container.resolve('startVirtualCallerController')))
+  // router.post('/virtual-caller/today', wrap(container.resolve('virtualCallerTodayStatsController')))
 
   // VirtualCaller CRUD
   router.get('/virtual-callers', wrap(container.resolve('listVirtualCallersController')))
