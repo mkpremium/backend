0d9bbf473a53d9b0db5385d5aea41fd3db9e0fa8
Jorge Velasco <jorge.velasco.silva@gmail.com>
2023-12-07 19:13:04 -0300

    Pass missing parameter.

--- test/infrastructure/listeners.spec.ts
+++ test/infrastructure/listeners.spec.ts
@@ -1,21 +1,17 @@
 import { startListeners } from '../../src/infrastructure/listeners'
-import { EventBus } from '../../src/infrastructure/event-bus'
 import { expect } from 'chai'
-import { createDiContainer, setupContainer } from '../../src/infrastructure/dependencies'
+import { setupContainer } from '../../src/infrastructure/dependencies'
 import { Logger } from 'winston'
 import { createContainer } from 'awilix'
 
 describe('startListeners', () => {
-  let container
-  beforeEach(() => {
-    container = createContainer()
-    setupContainer(container, null, null)
-  })
+  [ true, false ].forEach(usePostgres => it(`start all listeners without errors (usePostgres=${usePostgres}`, () => {
+    const container = createContainer()
+    setupContainer(container, null, null, usePostgres)
 
-  it('start all listeners without errors', () => {
     const logger: Logger = container.resolve('logger')
     logger.silent = true
 
     expect(() => startListeners(container)).to.not.throw
-  })
+  }))
 })
