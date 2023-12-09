1904774faf161d01e44c790e4c5cb812b540d715
Jorge Velasco <jorge.velasco.silva@gmail.com>
2023-12-09 18:35:17 -0300

    Fix query for getting meetings.

--- src/scheduled-events/repository/self-meetings.repository.js
+++ src/scheduled-events/repository/self-meetings.repository.js
@@ -8,12 +8,12 @@ building.recentProposal.proposal proposalValue,
 meeting.event.contactId,
 building.metadata,
 
-owner.name as contactName,
+owner.person.name as contactName,
 owner.person.contacts
 FROM ${bucketName} meeting
 LEFT JOIN ${bucketName} building ON meta(building).id = meeting.event.buildingId AND building._documentType = 'building'
 
-LEFT JOIN ${bucketName} owner ON meta(owner).id = meeting.event.owner.id AND owner._documentType = 'owner'
+LEFT JOIN ${bucketName} owner ON meta(owner).id = meeting.event.ownerId AND owner._documentType = 'owner'
 
 WHERE meeting._documentType = 'scheduled-event'
       AND meeting.type = 'MEETINGS'
