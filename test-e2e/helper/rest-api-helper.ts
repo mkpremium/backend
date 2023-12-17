3d548df364796a73c524d813feb7b5d5d93a690f
Jorge Velasco <jorge.velasco.silva@gmail.com>
2023-12-17 18:38:02 -0300

    Return container when starting app with postgres.

--- test-e2e/helper/rest-api-helper.ts
+++ test-e2e/helper/rest-api-helper.ts
@@ -2,14 +2,20 @@ import { createApp } from '../../src/app'
 import { defaultPassword, operatorLogin } from '../../test/common'
 import request from 'supertest'
 import { Database } from '../../src/infrastructure/database'
+import { AwilixContainer } from 'awilix'
+import { Bucket } from 'couchbase'
+import { Application } from 'express'
 
 const DEFAULT_MILLISECONDS_TO_WAIT = 1000
 
-export const initApplication = (database: Database = 'couchbase') => createApp(database)
+export const initApplication = (database: Database = 'couchbase'): Promise<Application | [ Application, AwilixContainer ]> => createApp(database)
   .then(app => {
-    const bucket = app.locals.diContainer.resolve('couchbaseBucket')
+    const diContainer = app.locals.diContainer as AwilixContainer
+    const bucket = diContainer.resolve('couchbaseBucket') as Bucket
     return new Promise(resolve => {
-      bucket.manager().flush(() => setTimeout(() => resolve(app), 500))
+      bucket.manager().flush(
+        () => setTimeout(() => resolve(database === 'postgres' ? [ app, diContainer ] : app), 500)
+      )
     })
   })
 
