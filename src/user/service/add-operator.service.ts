9e65cdcbd1a12ec9675ef8b410a0b9ae16afb7bd
Jorge Velasco <jorge.velasco.silva@gmail.com>
2023-12-19 20:48:44 -0300

    Inject repository into service.

--- src/user/service/add-operator.service.ts
+++ src/user/service/add-operator.service.ts
@@ -2,9 +2,17 @@ import { OperatorRepository } from '../../operator/models'
 import { History } from '../../history/models'
 
 export class AddOperatorService {
+  constructor (
+    private operatorRepository: OperatorRepository,
+  ) {
+  }
+
   async addOperator (body: any, user: any) {
-    const repo = new OperatorRepository()
-    const operator = await repo.createOperator(body)
+    return await this.saveInCouchbase(body, user)
+  }
+
+  private async saveInCouchbase (body: any, user: any) {
+    const operator = await this.operatorRepository.createOperator(body)
     await History.registerCreate({
       contextModel: operator,
       user: user
