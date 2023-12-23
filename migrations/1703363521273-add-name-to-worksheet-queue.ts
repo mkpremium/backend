eb96fd76eabfbd737afc3b4d2ab216d111641184
Jorge Velasco <jorge.velasco.silva@gmail.com>
2023-12-23 17:32:39 -0300

    Add worksheet queue name.

---
+++ migrations/1703363521273-add-name-to-worksheet-queue.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm'
+
+export class AddNameToWorksheetQueue1703363521273 implements MigrationInterface {
+  name = 'AddNameToWorksheetQueue1703363521273'
+
+  public async up (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "worksheet_queue"
+        ADD "name" character varying NOT NULL`)
+  }
+
+  public async down (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "worksheet_queue" DROP COLUMN "name"`)
+  }
+
+}
--- src/worksheet/worksheet-queue.entity.ts
+++ src/worksheet/worksheet-queue.entity.ts
@@ -1,9 +1,12 @@
-import { Entity, OneToMany } from 'typeorm'
+import { Column, Entity, OneToMany } from 'typeorm'
 import { BaseEntity } from '../infrastructure/entity'
 import { Worksheet } from './worksheet.entity'
 
 @Entity()
 export class WorksheetQueue extends BaseEntity {
+  @Column()
+  name: string
+
   @OneToMany(() => Worksheet, ws => ws.queue)
   worksheets: Worksheet[]
 }
