f6ef536dde0d26fd49206b096797c4833a8eb0c0
Jorge Velasco <jorge.velasco.silva@gmail.com>
2023-12-29 19:01:04 -0300

    Move worksheet routes to single file.

--- src/worksheet/routing.ts
+++ src/worksheet/routing.ts
@@ -1,7 +1,38 @@
-import { worksheetRoutes } from './routes'
 import { AwilixContainer } from 'awilix'
-import { Express } from 'express'
+import { Express, Router } from 'express'
+import {
+  createQueueController,
+  queueListController,
+  updateQueueController, updateWorksheetStatusController,
+  worksheetListController
+} from './controllers'
+import { WorksheetQueueRepository } from './repository/worksheet-queue.repository'
+import { permissions } from '../middleware/jwt'
+import { wrap } from 'express-promise-wrap'
+import { createTakeWorksheetIntoQueueController } from './controller/take-worksheet.controller'
+import { createStatusChangedController } from './controller/status-changed.controller'
 
 export const worksheetsRoutes = (app: Express, container: AwilixContainer, secured) => {
-  app.use('/worksheets', secured, worksheetRoutes(container))
+  const router = Router()
+
+  router.get('/', worksheetListController)
+
+  const worksheetQueueRepository = container.resolve('worksheetQueueRepository') as WorksheetQueueRepository
+  router.get('/queues', permissions.manager, queueListController(worksheetQueueRepository))
+
+  router.post('/queues', permissions.manager, createQueueController(worksheetQueueRepository))
+
+  router.post('/queues/:queueId/worksheets/:worksheetId', permissions.operator, wrap(
+    createTakeWorksheetIntoQueueController(container.resolve('worksheetQueueActionsService'))
+  ))
+
+  router.put('/queues/:id', permissions.manager, updateQueueController(worksheetQueueRepository))
+
+  router.put('/:id/status', updateWorksheetStatusController(container.resolve('worksheetRepository')))
+
+  router.post('/status-changed', wrap(
+    container.resolve('worksheetStatusChangedController') as ReturnType<typeof createStatusChangedController>
+  ))
+
+  app.use('/worksheets', secured, router)
 }
