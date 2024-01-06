fd7ff088d1426d785f1e45dd2a47f843a5dcdc51
Jorge Velasco <jorge.velasco.silva@gmail.com>
2024-01-05 22:11:33 -0300

    Remove unused controllers.

--- src/scheduled-events/routes.js
+++ src/scheduled-events/routes.js
@@ -1,22 +1,15 @@
 import { Router } from 'express'
-
-import {
-  findScheduledEventController,
-  weekScheduleEventMeetingsController
-} from './controllers'
 import { wrap } from 'express-promise-wrap'
 
 export const createScheduleEventsRoutes = awilixContainer => {
   const router = Router()
 
-  router.get('/week', weekScheduleEventMeetingsController)
   router.post('/call', wrap(awilixContainer.resolve('addScheduledCallController')))
   router.post('/meeting', wrap(awilixContainer.resolve('addMeetingController')))
   router.get('/calls', wrap(awilixContainer.resolve('getUserScheduledCallsController')))
 
   router.put('/:id', wrap(awilixContainer.resolve('updateScheduledCallController')))
   router.delete('/:id', wrap(awilixContainer.resolve('deleteScheduledEventController')))
-  router.get('/:id', findScheduledEventController)
 
   return router
 }
