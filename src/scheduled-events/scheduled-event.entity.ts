bdb3fd3dceb7b5d693b74ef06d5e6152a2537984
Jorge Velasco <jorge.velasco.silva@gmail.com>
2024-01-06 10:38:53 -0300

    Add scheduled event entity.

--- src/scheduled-events/scheduled-event.entity.ts
+++ src/scheduled-events/scheduled-event.entity.ts
@@ -1,7 +1,27 @@
-import { Entity } from "typeorm";
-import { BaseEntity } from "../infrastructure/entity";
+import { Column, Entity, ManyToOne } from 'typeorm'
+import { Building } from '../building/building.entity'
+import { BaseEntity } from '../infrastructure/entity'
+import { Owner } from '../owner/owner.entity'
+import { User } from '../user/user.entity'
+import { Contact } from '../contacts/contact.entity'
 
 @Entity()
 export class ScheduledEvent extends BaseEntity {
+  @Column('timestamp')
+  scheduledFor: Date
 
+  @ManyToOne(() => User)
+  notifyTo: User
+
+  @ManyToOne(() => User)
+  createdBy: User
+
+  @ManyToOne(() => Building)
+  building: Building
+
+  @ManyToOne(() => Owner)
+  owner: Owner
+
+  @ManyToOne(() => Contact)
+  contact: Contact
 }
