6355ec7783e453bb7ce332197ddf233ac0f19539
Jorge Velasco <jorge.velasco.silva@gmail.com>
2024-01-08 19:37:53 -0300

    Commit pending changes.

--- scripts/manual-interventions/publish-command-to-save-documents-in-postgres.ts
+++ scripts/manual-interventions/publish-command-to-save-documents-in-postgres.ts
@@ -2,8 +2,7 @@ import { connectCouchbaseBucket } from '../../src/db/connect-couchbase-bucket'
 import { N1qlQuery } from 'couchbase'
 import { SQS } from 'aws-sdk'
 import { initLogger } from '../../src/infrastructure/logger'
-import { Id, Identifiable } from '../../src/infrastructure/postgres/save-documents-command-handler'
-import _ from 'lodash'
+import { CommandPublisher } from '../../src/infrastructure/event-bus/command-publisher'
 
 const logger = initLogger()
 const sqsClient = new SQS({
@@ -14,13 +13,18 @@ const DOCUMENTS_CONDITION = process.env["DOCUMENTS_CONDITION"]
 if (!DOCUMENTS_CONDITION) {
   throw new Error('Missing DOCUMENTS_CONDITION environment variable.')
 }
-const BATCH_SIZE = parseInt(process.env["BATCH_SIZE"]) || 1_000
+
 const QUEUE_URL = process.env.EVENTS_QUEUE_URL
 
 connectCouchbaseBucket()
   .then(bucket => {
     return new Promise((resolve, reject) => {
-      const commandPublisher = new CommandPublisher(sqsClient, QUEUE_URL)
+      const commandPublisher = new CommandPublisher(
+        sqsClient,
+        QUEUE_URL,
+        'postgres.save_documents_command_handler',
+        logger,
+      )
       const allDocumentsQuery = bucket.query(
         N1qlQuery.fromString(`SELECT mkpremium.id
                               FROM mkpremium
@@ -47,71 +51,5 @@ connectCouchbaseBucket()
     console.trace(error)
     process.exit(1)
   })
-
-class CommandPublisher {
-  private buffer: Id[] = []
-  private isBatchSendingInProgress = false
-
-  constructor (
-    private sqs: SQS,
-    private queueUrl: string
-  ) {
-  }
-
-  async push (obj: Identifiable) {
-    this.buffer.push(obj.id)
-    const bufferLength = this.buffer.length
-    if (bufferLength >= BATCH_SIZE) {
-      if (this.isBatchSendingInProgress) {
-        logger.warning('Batch sending in progress, not sending next batch')
-        return
-      }
-      logger.info('Publishing next batch', { bufferSize: bufferLength })
-      const nextBatch = this.buffer.splice(0, BATCH_SIZE)
-      await this.publishBatch(nextBatch)
-    }
-  }
-
-  async flushPendingMessages (retries = 3) {
-    if (this.isBatchSendingInProgress) {
-      if (retries === 0) {
-        throw new Error('Couldn\'t flush pending messages')
-      }
-
-      logger.warning('Batch sending in progress, waiting 500ms before trying again')
-      return new Promise(resolve => setTimeout(resolve, 500))
-        .then(() => this.flushPendingMessages(retries - 1))
-    }
-
-    while (this.buffer.length > 0) {
-      const nextBatch = this.buffer.splice(0, BATCH_SIZE)
-      await this.publishBatch(nextBatch)
-    }
-  }
-
-  async publishBatch (batch: Id[]) {
-    this.isBatchSendingInProgress = true
-    const chunks = _.chunk(batch, BATCH_SIZE / 10)
-    try {
-      return await this.sqs.sendMessageBatch({
-        QueueUrl: this.queueUrl,
-        Entries: chunks.map((chunk, idx) => ({
-          Id: `chunk-${idx}`,
-          MessageBody: JSON.stringify({
-            listener: 'postgres.save_documents_command_handler',
-            event: {
-              name: 'postgres.save_documents_command',
-              ids: chunk,
-            }
-          }),
-        }))
-      }).promise().then(batchResult => {
-        batchResult.Failed.forEach(console.error)
-      })
-    } finally {
-      this.isBatchSendingInProgress = false
-    }
-  }
-}
 
 
