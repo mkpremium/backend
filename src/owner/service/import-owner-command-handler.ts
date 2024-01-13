343a12e4a8b0fa2cdcb6c27527dbdf343b02cf75
Jorge Velasco <jorge.velasco.silva@gmail.com>
2024-01-13 10:51:49 -0300

    Don't import owners already imported.

--- src/owner/service/import-owner-command-handler.ts
+++ src/owner/service/import-owner-command-handler.ts
@@ -1,42 +1,61 @@
 import { OwnerProps } from '../owner'
 import { EventPublisher } from '../../infrastructure/event-bus'
 import { Logger } from 'winston'
-import { DataSource } from 'typeorm'
+import { DataSource, EntityManager } from 'typeorm'
 import { AddOwnerCommand, AddOwnerService } from './add-owner.service'
 import { DomainEventCatalog } from '../../infrastructure/postgres/domain-event.entity'
 import { CouchbaseDocument, CouchbaseDocumentType } from '../../infrastructure/postgres/couchbase-document.entity'
 import { BuildingProps } from '../../building/building'
 import { Building } from '../../building/building.entity'
+import {
+  getCouchbaseDocument,
+  markCouchbaseDocumentAsMigrated
+} from '../../infrastructure/postgres/get-couchbase-document'
 
 interface Deps {
-  ormDataSource: DataSource,
+  entityManager: EntityManager,
   logger: Logger,
   eventBus: EventPublisher,
   addOwnerService: AddOwnerService,
 }
 
-export function importOwnerCommandHandler ({ addOwnerService, eventBus, logger, ormDataSource }: Deps) {
+export function importOwnerCommandHandler ({ addOwnerService, eventBus, logger, entityManager }: Deps) {
   return async function ({ owner }: { owner: OwnerProps }) {
     logger.info('Importing owner', { owner })
+    const couchbaseDocument = await getCouchbaseDocument(entityManager, owner.id)
+    if (couchbaseDocument.migratedAt) {
+      logger.warning('Owner already migrated', { ownerId: owner.id })
+      return
+    }
+
     await addOwnerService.addOwner({ ...owner, note: 'Importado desde Couchase' } as AddOwnerCommand, 'system')
+    await markCouchbaseDocumentAsMigrated(entityManager, owner.id)
     await eventBus.publish({
       name: DomainEventCatalog.POSTGRES_MIGRATION__OWNER_IMPORTED,
       ownerId: owner.id,
+    }, entityManager)
+
+    if (!owner.buildingId) {
+      logger.warning('Owner has no building', { ownerId: owner.id })
+      return
+    }
+
+    const couchbaseBuilding = await entityManager.findOneBy(CouchbaseDocument, {
+      documentType: CouchbaseDocumentType.BUILDING,
+      id: owner.buildingId,
     })
-    if (owner.buildingId) {
-      const couchbaseBuilding = await ormDataSource.manager.findOneBy(CouchbaseDocument, {
-        documentType: CouchbaseDocumentType.BUILDING,
-        id: owner.buildingId,
-      })
-      if (!couchbaseBuilding) {
-        console.error('Building not found (importing owner)', { buildingId: owner.buildingId })
-        return
-      }
-      const building = couchbaseBuilding.document as BuildingProps
-      if (building.ownerId && building.ownerId === owner.id) {
-        logger.info('Setting featured building owner', { buildingId: building.id, ownerId: owner.id })
-        await ormDataSource.manager.update(Building, { id: building.id }, { featuredOwner: { id: owner.id } })
-      }
+    if (!couchbaseBuilding) {
+      console.error('Building not found (importing owner)', { buildingId: owner.buildingId })
+      return
+    }
+    const building = couchbaseBuilding.document as BuildingProps
+    if (building.ownerId && building.ownerId === owner.id) {
+      logger.info('Setting featured building owner', { buildingId: building.id, ownerId: owner.id })
+      await entityManager.update(Building, { id: building.id }, { featuredOwner: { id: owner.id } })
     }
   }
+}
+
+function ensureOwnerHasNames (owner: OwnerProps) {
+  if (owner.person.firstName && owner.person.firstSurname) return owner
 }
