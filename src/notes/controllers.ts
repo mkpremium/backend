3b2165d0485a7317b09607bd8ac7a8c25c10b7ce
Jorge Velasco <jorge.velasco.silva@gmail.com>
2024-01-14 21:07:47 +0000

    Don't save note added history.

--- src/notes/controllers.ts
+++ src/notes/controllers.ts
@@ -1,6 +1,5 @@
 import { wrap } from 'express-promise-wrap'
 import { BuildingNotesRepository } from '../building/repository/building-notes.repository'
-import { History } from '../history/models'
 
 export function listNotesControllerFactory ({ buildingNotesRepository }: {
   buildingNotesRepository: BuildingNotesRepository
@@ -19,7 +18,6 @@ export function addNoteControllerFactory  ({ buildingNotesRepository }: {
   return wrap(async function listNotes (req, res) {
     const note = await buildingNotesRepository.createNote(req.body, req.user.id)
 
-    await History.registerCreate({ contextModel: note, user: req.user })
     res.status(201).json(note)
     }
   )
