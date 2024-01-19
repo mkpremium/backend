153d64018b0cb861727221b7f9fb428e737b84b4
Jorge Velasco <jorge.velasco.silva@gmail.com>
2024-01-19 00:58:36 +0000

    Set featured building owner in Postgres.

--- src/building/service/featured-owner.service.ts
+++ src/building/service/featured-owner.service.ts
@@ -1,10 +1,20 @@
 import { CouchbaseBuildingsRepository } from '../repository/couchbase-building.repository'
+import { EntityManager } from "typeorm";
+import { Building } from "../building.entity";
 
 export class FeaturedOwnerService {
-  constructor (private couchbaseBuildingsRepository: CouchbaseBuildingsRepository) {
+  constructor(
+    private couchbaseBuildingsRepository: CouchbaseBuildingsRepository,
+    private usePostgres: boolean,
+    private entityManager: EntityManager,
+  ) {
   }
 
-  async setBuildingFeaturedOwner (buildingId, ownerId) {
-    await this.couchbaseBuildingsRepository.setBuildingFeaturedOwner(buildingId, ownerId)
+  async setBuildingFeaturedOwner(buildingId: string, ownerId: string) {
+    if (this.usePostgres) {
+      await this.entityManager.update(Building, {id: buildingId}, {featuredOwner: {id: ownerId}})
+    } else {
+      await this.couchbaseBuildingsRepository.setBuildingFeaturedOwner(buildingId, ownerId)
+    }
   }
 }
