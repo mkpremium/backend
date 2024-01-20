a84f6320e06d9dd3a8b3d5eaf818948f0aab07cb
Jorge Velasco <jorge.velasco.silva@gmail.com>
2024-01-20 23:40:55 +0000

    Add worksheet holder when taking a worksheet.

--- src/worksheet/service/worksheet-queue-actions-service.ts
+++ src/worksheet/service/worksheet-queue-actions-service.ts
@@ -6,35 +6,44 @@ import { removeScheduledCall } from '../domain/queue'
 import { DomainEventCatalog } from '../../infrastructure/postgres/domain-event.entity'
 import { CallcenterWorksheetService } from './callcenter-worksheet.service'
 import { CouchbaseWorksheetQueueRepository } from "../repository/couchbase-worksheet-queue.repository";
+import { EntityManager } from "typeorm";
+import { Worksheet } from "../worksheet.entity";
+import { Caller } from "../../caller/caller.entity";
 
 export class WorksheetQueueActionsService {
-  constructor (
+  constructor(
     private worksheetQueueRepository: WorksheetQueueRepository,
     private couchbaseWorksheetQueueRepository: CouchbaseWorksheetQueueRepository,
     private worksheetRepository: WorksheetRepository,
     private callcenterWorksheetService: CallcenterWorksheetService,
     private eventBus: EventPublisher,
     private usePostgres: boolean,
+    private entityManager: EntityManager,
   ) {
   }
 
-  async takeWorksheetInQueue (queueId: string, worksheetId: string, userId: string) {
-    const queue = await this.worksheetQueueRepository.get(queueId)
-    const worksheet = await this.worksheetRepository.get(worksheetId)
-
-    const [ queueWithWorksheet, worksheetInQueue ] = takeWorksheet(queue, worksheet, userId)
-
-    await this.worksheetRepository.save(worksheetInQueue)
-    // As we use the worksheet as source for the queue in Postgres there is no need to update the queue.
-    if (!this.usePostgres) {
-      await this.couchbaseWorksheetQueueRepository.save(queueWithWorksheet)
+  async takeWorksheetInQueue(queueId: string, worksheetId: string, userId: string) {
+    if (this.usePostgres) {
+      await this.doPostgres(queueId, worksheetId, userId);
+    } else {
+      await this.doCouchbase(queueId, worksheetId, userId);
     }
-    await this.eventBus.publish({ name: DomainEventCatalog.WORKSHEET__TAKEN, worksheetId, queueId, by: userId })
 
     return this.callcenterWorksheetService.getWorksheetForCallcenterView(worksheetId)
   }
 
-  async removeScheduledCallFromWorksheets (scheduledCallId: string) {
+  private async doCouchbase(queueId: string, worksheetId: string, userId: string) {
+    const queue = await this.worksheetQueueRepository.get(queueId)
+    const worksheet = await this.worksheetRepository.get(worksheetId)
+
+    const [queueWithWorksheet, worksheetInQueue] = takeWorksheet(queue, worksheet, userId)
+
+    await this.worksheetRepository.save(worksheetInQueue)
+    await this.couchbaseWorksheetQueueRepository.save(queueWithWorksheet)
+    await this.eventBus.publish({name: DomainEventCatalog.WORKSHEET__TAKEN, worksheetId, queueId, by: userId})
+  }
+
+  async removeScheduledCallFromWorksheets(scheduledCallId: string) {
     // As we use the worksheet as source for the queue in Postgres there is no need to update the queue.
     if (this.usePostgres) {
       return
@@ -48,5 +57,27 @@ export class WorksheetQueueActionsService {
 
         this.couchbaseWorksheetQueueRepository.save(removeScheduledCall(queue, scheduledCallId))
       })
+  }
+
+  private doPostgres(queueId: string, worksheetId: string, userOrCallerId: string) {
+    return this.entityManager.transaction(async entityManager => {
+      const caller = await entityManager.findOneByOrFail(Caller, [
+        {id: userOrCallerId},
+        {user: {id: userOrCallerId}}
+      ])
+      await entityManager.update(Worksheet, {id: worksheetId}, {
+        status: 'TAKEN',
+        queue: {id: queueId},
+        heldBy: {id: caller.id},
+        lastViewedAt: new Date(),
+        lastStatusChangedAt: new Date(),
+      })
+      await this.eventBus.publish({
+        name: DomainEventCatalog.WORKSHEET__TAKEN,
+        worksheetId,
+        queueId,
+        by: caller.id,
+      }, entityManager)
+    })
   }
 }
---
+++ test/worksheet/service/worksheet-queue-actions.service.spec.ts
@@ -0,0 +1,61 @@
+import { AddOperatorService } from "../../../src/user/service/add-operator.service";
+import { BuildingsRepository } from "../../../src/building/repository/buildings.repository";
+import {
+  PostgresWorksheetQueueRepository
+} from "../../../src/worksheet/repository/postgres-worksheet-queue.repository";
+import { PostgresWorksheetRepository } from "../../../src/worksheet/repository/postgres-worksheet.repository";
+import { createTestContainer } from "../../create-test-container";
+import { WorksheetQueueActionsService } from "../../../src/worksheet/service/worksheet-queue-actions-service";
+import { buildingFactory, worksheetFactory, worksheetQueueFactory } from "../../factories";
+import { addCaller, createOwnerWithPhoneContact } from "../../helpers";
+import { EntityManager } from "typeorm";
+import { Worksheet } from "../../../src/worksheet/worksheet.entity";
+import { expect } from "chai";
+import { AddContactService } from "../../../src/owner/service/add-contact.service";
+import { AddOwnerService } from "../../../src/owner/service/add-owner.service";
+
+describe('WorksheetQueueActionsService', () => {
+  it('stores the worksheet holder', async () => {
+    const deps = await buildDependencies()
+    const testQueue = await deps.postgresQueueRepository.save(worksheetQueueFactory.build())
+    const testBuilding = await deps.buildingsRepository.save(buildingFactory.build());
+    await createOwnerWithPhoneContact(testBuilding, deps)
+    const testWorksheet = await deps.worksheetRepository.save(
+      worksheetFactory.build(null, {buildingId: testBuilding.id}))
+    const testCallerUser = await addCaller(deps)
+
+    await deps.worksheetQueueActionsService.takeWorksheetInQueue(testQueue.id, testWorksheet.id, testCallerUser.callerId)
+
+    const refreshedWorksheet = await deps.entityManager.findOneOrFail(Worksheet, {
+      where: {id: testWorksheet.id},
+      relations: {heldBy: true}
+    })
+    expect(refreshedWorksheet.heldBy).to.include({id: testCallerUser.callerId})
+  })
+})
+
+interface Deps {
+  addContactService: AddContactService
+  addOperatorService: AddOperatorService
+  addOwnerService: AddOwnerService
+  buildingsRepository: BuildingsRepository
+  entityManager: EntityManager
+  postgresQueueRepository: PostgresWorksheetQueueRepository
+  worksheetRepository: PostgresWorksheetRepository
+  worksheetQueueActionsService: WorksheetQueueActionsService
+}
+
+async function buildDependencies(): Promise<Deps> {
+  const container = await createTestContainer({couchbase: false, postgres: true})
+
+  return {
+    addContactService: container.resolve('addContactService'),
+    addOwnerService: container.resolve('addOwnerService'),
+    addOperatorService: container.resolve('addOperatorService'),
+    buildingsRepository: container.resolve('buildingsRepository'),
+    entityManager: container.resolve('entityManager'),
+    postgresQueueRepository: container.resolve('postgresQueueRepository'),
+    worksheetRepository: container.resolve('worksheetRepository'),
+    worksheetQueueActionsService: container.resolve('worksheetQueueActionsService'),
+  }
+}
