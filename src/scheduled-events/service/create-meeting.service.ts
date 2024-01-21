64eff4ba160fd8bf0a9a30bfbb3ff28f8049137b
Jorge Velasco <jorge.velasco.silva@gmail.com>
2024-01-21 23:32:59 +0000

    Save meeting created event atomically.

--- src/scheduled-events/service/create-meeting.service.ts
+++ src/scheduled-events/service/create-meeting.service.ts
@@ -2,7 +2,7 @@ import { newHttpError } from '../../lib/http-error'
 import { isBusiness } from '../../lib/role-operators'
 import { EventPublisher } from '../../infrastructure/event-bus'
 import { CouchbaseScheduledEventsRepository } from '../repository/couchbase-schedule-events.repository'
-import { DataSource } from 'typeorm'
+import { EntityManager } from 'typeorm'
 import { UserProps } from '../../types/user'
 import { ScheduledEvent } from '../scheduled-event.entity'
 import { ScheduledEventId, ScheduledEventProps } from '../types'
@@ -28,26 +28,67 @@ interface AddMeetingCommand {
 }
 
 export class CreateMeetingService {
-  constructor (
+  constructor(
     private couchbaseScheduledEventsRepository: CouchbaseScheduledEventsRepository,
     private couchbaseBuildingsRepository: CouchbaseBuildingsRepository,
     private eventBus: EventPublisher,
     private usePostgres: boolean,
-    private ormDataSource: DataSource,
+    private entityManager: EntityManager,
   ) {
   }
 
-  async createMeeting (user: Pick<UserProps, 'id' | 'roles'>, cmd: AddMeetingCommand) {
+  async createMeeting(user: Pick<UserProps, 'id' | 'roles'>, cmd: AddMeetingCommand) {
     const meetingAgentId = cmd.notifyTo
     this.checkOperatorPermissions(user, meetingAgentId)
 
-    const createdMeeting = await (this.usePostgres ?
-      doPostgres(cmd, this.ormDataSource) :
-      doCouchbase(cmd, user, meetingAgentId, {
-        couchbaseScheduledEventsRepository: this.couchbaseScheduledEventsRepository,
-        couchbaseBuildingsRepository: this.couchbaseBuildingsRepository,
-      }))
+    return await (this.usePostgres ?
+      this.doPostgres(cmd) :
+      this.doCouchbase(cmd, user, meetingAgentId))
+  }
 
+  private checkOperatorPermissions(user: Pick<UserProps, 'id' | 'roles'>, meetingOperatorId) {
+    if (isBusiness(user.roles) && user.id !== meetingOperatorId) {
+      throw newHttpError(403, 'No tiene los permisos suficientes para esta operación')
+    }
+  }
+
+  private async doPostgres(cmd: AddMeetingCommand): Promise<ScheduledEventProps> {
+    return this.entityManager.transaction<ScheduledEventProps>(async entityManager => {
+      const meeting = await entityManager.save(ScheduledEvent, {
+        type: 'MEETING',
+        scheduledFor: cmd.eventDate,
+        notifyTo: {id: cmd.notifyTo},
+        createdBy: {id: cmd.createdBy},
+        contact: {id: cmd.event.contactId},
+        building: {id: cmd.event.buildingId},
+        owner: {id: cmd.event.ownerId},
+      })
+
+      const createdMeeting = {
+        id: meeting.id as ScheduledEventId,
+        type: 'MEETINGS',
+        eventDate: cmd.eventDate,
+        createdBy: cmd.createdBy,
+        createdAt: meeting.createdAt,
+        notifyTo: cmd.notifyTo,
+        event: cmd.event,
+      } as ScheduledEventProps
+      await this.publishEvent(createdMeeting, cmd, entityManager);
+
+      return createdMeeting
+    })
+  }
+
+  private async doCouchbase(requestBody, operator, meetingAgentId): Promise<ScheduledEventProps> {
+    const createdMeeting = await this.couchbaseScheduledEventsRepository
+      .addScheduledMeetingEvent(requestBody, operator.id)
+    await this.couchbaseBuildingsRepository.assignBuildingToAgent(createdMeeting.event.buildingId, meetingAgentId)
+    await this.publishEvent(createdMeeting, requestBody)
+
+    return createdMeeting
+  }
+
+  private async publishEvent(createdMeeting: ScheduledEventProps, cmd: AddMeetingCommand, entityManager?: EntityManager) {
     await this.eventBus.publish({
       name: DomainEventCatalog.SCHEDULED_EVENTS__MEETING_CREATED,
       meetingId: createdMeeting.id,
@@ -56,54 +97,7 @@ export class CreateMeetingService {
       contactId: createdMeeting.event.contactId,
       buildingId: createdMeeting.event.buildingId,
       note: cmd.note
-    } as MeetingCreated)
-
-    return createdMeeting
+    } as MeetingCreated, entityManager)
   }
 
-  checkOperatorPermissions (user: Pick<UserProps, 'id' | 'roles'>, meetingOperatorId) {
-    if (isBusiness(user.roles) && user.id !== meetingOperatorId) {
-      throw newHttpError(403, 'No tiene los permisos suficientes para esta operación')
-    }
-  }
-}
-
-
-async function doPostgres (cmd: AddMeetingCommand, ormDataSource: DataSource): Promise<ScheduledEventProps> {
-  return ormDataSource.transaction<ScheduledEventProps>(async entityManager => {
-    const meeting = await entityManager.save(ScheduledEvent, {
-      type: 'MEETING',
-      scheduledFor: cmd.eventDate,
-      notifyTo: { id: cmd.notifyTo },
-      createdBy: { id: cmd.createdBy },
-      contact: { id: cmd.event.contactId },
-      building: { id: cmd.event.buildingId },
-      owner: { id: cmd.event.ownerId },
-    })
-
-    return {
-      id: meeting.id as ScheduledEventId,
-      type: 'MEETINGS',
-      eventDate: cmd.eventDate,
-      createdBy: cmd.createdBy,
-      createdAt: meeting.createdAt,
-      notifyTo: cmd.notifyTo,
-      event: cmd.event,
-    }
-  })
-}
-
-interface CouchbaseDeps {
-  couchbaseScheduledEventsRepository: CouchbaseScheduledEventsRepository,
-  couchbaseBuildingsRepository: CouchbaseBuildingsRepository,
-}
-
-async function doCouchbase (requestBody, operator, meetingAgentId, {
-  couchbaseScheduledEventsRepository,
-  couchbaseBuildingsRepository
-}: CouchbaseDeps): Promise<ScheduledEventProps> {
-  const createdMeeting = await couchbaseScheduledEventsRepository.addScheduledMeetingEvent(requestBody, operator.id)
-  await couchbaseBuildingsRepository.assignBuildingToAgent(createdMeeting.event.buildingId, meetingAgentId)
-
-  return createdMeeting
 }
