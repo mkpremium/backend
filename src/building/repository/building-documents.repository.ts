bd4e6ea73599c4b22ca27c03fc834419709ad89c
Jorge Velasco <jorge.velasco.silva@gmail.com>
2024-01-22 00:29:21 +0000

    Add type hinting to repository method

--- src/building/repository/building-documents.repository.ts
+++ src/building/repository/building-documents.repository.ts
@@ -1,22 +1,22 @@
 import { CouchbaseAdapter } from '../../db/couchbase.adapter'
 
 const buildingDocumentsQuery = bucketName => `
-  SELECT
-    metadata.id,
-    metadata.url,
-    metadata.mimeType
+  SELECT metadata.id,
+         metadata.url,
+         metadata.mimeType
   FROM ${bucketName} metadata
-  WHERE metadata._documentType = 'metadata' AND metadata.buildingId = $1
+  WHERE metadata._documentType = 'metadata'
+    AND metadata.buildingId = $1
 `
 
 export class BuildingDocumentsRepository {
-    constructor(private couchbaseAdapter: CouchbaseAdapter) {
-    }
+  constructor(private couchbaseAdapter: CouchbaseAdapter) {
+  }
 
-    documentsOfBuilding(buildingId) {
-        return this.couchbaseAdapter.queryAsync(
-            buildingDocumentsQuery(this.couchbaseAdapter.bucketName),
-            [ buildingId ]
-        ).then(docs => docs.map(({ id, url, mimeType }) => ({ documentId: id, privateUrl: url, mimeType })))
-    }
+  documentsOfBuilding(buildingId: string): Promise<{ documentId: string, privateUrl: string, mimeType: string }[]> {
+    return this.couchbaseAdapter.queryAsync(
+      buildingDocumentsQuery(this.couchbaseAdapter.bucketName),
+      [buildingId]
+    ).then(docs => docs.map(({id, url, mimeType}) => ({documentId: id, privateUrl: url, mimeType})))
+  }
 }
