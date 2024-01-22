7e7e2091a401c6e58302a51ce5aa7521741a7766
Jorge Velasco <jorge.velasco.silva@gmail.com>
2024-01-22 01:42:50 +0000

    Get documents to sign from Postgres entity manager.

--- src/building/service/get-documents-signed-URL.service.ts
+++ src/building/service/get-documents-signed-URL.service.ts
@@ -1,19 +1,23 @@
 import S3 from 'aws-sdk/clients/s3';
 import url from 'url'
 import { BuildingDocumentsRepository } from "../repository/building-documents.repository";
+import { EntityManager } from "typeorm";
+import { BuildingDocument } from "../building-document.entity";
 
 export class GetDocumentsSignedURLService {
-  constructor (
+  constructor(
     private buildingDocumentsRepository: BuildingDocumentsRepository,
     private s3Client: S3,
     private documentBucket: string,
+    private usePostgres: boolean,
+    private entityManager: EntityManager,
   ) {
   }
 
-  async getDocumentsSignedURL (buildingId) {
-    const buildingDocuments = await this.buildingDocumentsRepository.documentsOfBuilding(buildingId)
+  async getDocumentsSignedURL(buildingId: string) {
+    const buildingDocuments = await this.getDocumentsOfBuilding(buildingId)
     return Promise.all(
-      buildingDocuments.map(({ documentId, privateUrl, mimeType }) => {
+      buildingDocuments.map(({documentId, privateUrl, mimeType}) => {
         return this.s3Client
           .getSignedUrlPromise('getObject', {
             Bucket: this.documentBucket,
@@ -27,5 +31,23 @@ export class GetDocumentsSignedURLService {
           })
       })
     )
+  }
+
+  private async getDocumentsOfBuilding(buildingId: string): Promise<{
+    documentId: string,
+    privateUrl: string,
+    mimeType: string
+  }[]> {
+    if (!this.usePostgres) {
+      return this.buildingDocumentsRepository.documentsOfBuilding(buildingId);
+    }
+    const buildingDocuments = await this.entityManager.find(BuildingDocument, {
+      where: {building: {id: buildingId}},
+    })
+    return buildingDocuments.map(doc => ({
+      documentId: doc.id,
+      mimeType: doc.mimeType,
+      privateUrl: doc.privateUrl,
+    }))
   }
 }
