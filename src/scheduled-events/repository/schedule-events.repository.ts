7c7bb26071a62867c930ec24ef46f203bed9acbc
Jorge Velasco <jorge.velasco.silva@gmail.com>
2024-01-22 23:45:50 +0000

    Remove unused struct and imports.

--- src/scheduled-events/repository/schedule-events.repository.ts
+++ src/scheduled-events/repository/schedule-events.repository.ts
@@ -1,11 +1,4 @@
-import t from 'tcomb'
-import { Event, ScheduledEventProps } from '../types'
-
-export const UpdateScheduledEvent = t.struct<ScheduledEventProps>({
-  eventDate: t.maybe(t.Date),
-  event: t.maybe(Event),
-  createdBy: t.String,
-}, 'UpdateScheduledEvent')
+import { ScheduledEventProps } from '../types'
 
 export interface ScheduledEventsRepository {
   update (id: string, data: Pick<ScheduledEventProps, 'eventDate'>): Promise<ScheduledEventProps>
