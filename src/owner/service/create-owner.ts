ecd53a8f55fd4032ba70f7a3fc224ee4fb7c7c24
Jorge Velasco <jorge.velasco.silva@gmail.com>
2024-01-23 22:30:21 -0300

    Use correct logging method.

--- src/owner/service/create-owner.ts
+++ src/owner/service/create-owner.ts
@@ -19,10 +19,10 @@ type CreateOwnerCommand = {
 
 export async function createOwner (entityManager: EntityManager, cmd: CreateOwnerCommand): Promise<[ Owner, Person ]> {
   if (cmd.person.name !== `${cmd.person.firstName} ${cmd.person.firstSurname}`) {
-    logger.warn(`Owner name ${cmd.person.name} does not match first name ${cmd.person.firstName} and first surname ${cmd.person.firstSurname}`, cmd)
+    logger.warning(`Owner name ${cmd.person.name} does not match first name ${cmd.person.firstName} and first surname ${cmd.person.firstSurname}`, cmd)
   }
   if (!cmd.buildingId) {
-    logger.warn('Owner does not have a building', cmd)
+    logger.warning('Owner does not have a building', cmd)
   }
 
   // TODO: review and consolidate owner names.
