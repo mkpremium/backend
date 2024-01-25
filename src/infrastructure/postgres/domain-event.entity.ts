650e098bae0fad30b4d3a89c33c14cf2fda27445
Jorge Velasco <jorge.velasco.silva@gmail.com>
2024-01-25 22:53:20 +0000

    Remove unused import.

--- src/infrastructure/postgres/domain-event.entity.ts
+++ src/infrastructure/postgres/domain-event.entity.ts
@@ -1,4 +1,4 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm'
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from 'typeorm'
 
 export enum DomainEventCatalog {
   BUILDING__LEAD_CAPTURED = 'building.lead_captured',
