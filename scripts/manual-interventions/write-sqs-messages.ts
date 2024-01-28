1667a4fa2ab3e977bf130269b47d81769ed8226c
Jorge Velasco <jorge.velasco.silva@gmail.com>
2024-01-28 12:19:13 -0300

    Add a script to write SQS messages.

---
+++ scripts/manual-interventions/write-sqs-messages.ts
@@ -0,0 +1,43 @@
+import { SQS } from 'aws-sdk';
+import fs from 'fs';
+import path from 'path';
+import util from 'util';
+
+const sqsClient = new SQS({ region: 'eu-west-1' });
+const QUEUE_URL = process.env.QUEUE_URL; // Replace with your SQS Queue URL
+const MESSAGES_DIR = process.env.MESSAGES_DIR;
+
+if (!QUEUE_URL) {
+  throw new Error('Missing QUEUE_URL environment variable.');
+}
+
+if (!MESSAGES_DIR) {
+  throw new Error('Missing MESSAGES_DIR environment variable.');
+}
+
+const readdir = util.promisify(fs.readdir);
+const readFile = util.promisify(fs.readFile);
+
+async function sendMessages() {
+  const files = await readdir(MESSAGES_DIR);
+
+  for (const file of files) {
+    const filePath = path.join(MESSAGES_DIR, file);
+    const content = await readFile(filePath, 'utf-8');
+    const message = JSON.parse(content);
+
+    const params = {
+      QueueUrl: QUEUE_URL,
+      MessageBody: JSON.stringify(message),
+    };
+
+    try {
+      const data = await sqsClient.sendMessage(params).promise();
+      console.log(`Message sent successfully: ${data.MessageId}`);
+    } catch (err) {
+      console.error(`An error occurred while sending the message: ${err}`);
+    }
+  }
+}
+
+sendMessages();
