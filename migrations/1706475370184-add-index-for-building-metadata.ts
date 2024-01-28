d7e3c2099335164ae2371c71f3dce2de9915657b
Jorge Velasco <jorge.velasco.silva@gmail.com>
2024-01-28 17:58:23 -0300

    Add index for building metadata.

---
+++ migrations/1706475370184-add-index-for-building-metadata.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class AddIndexForBuildingMetadata1706475370184 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+          `CREATE INDEX idx_metadata_buildingId ON couchbase_document((document ->> 'buildingId')) WHERE "documentType" = 'metadata'`)
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP INDEX "public"."idx_metadata_buildingId"`)
+    }
+
+}
