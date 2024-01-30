e6ac26182c9fc17efeadec62ccd9b027dfabd024
Jorge Velasco <jorge.velasco.silva@gmail.com>
2024-01-30 19:42:14 -0300

    Don't register proposal sent in operator stats.

--- src/building/service/add-proposal.service.ts
+++ src/building/service/add-proposal.service.ts
@@ -2,9 +2,6 @@ import { ProposalProps } from '../building'
 import { CouchbaseProposalsRepository } from '../repository/couchbase-proposals.repository'
 import t from 'tcomb'
 import { CouchbaseBuildingsRepository } from '../repository/couchbase-building.repository'
-import _get from 'lodash/get'
-import { OperatorStats } from '../../stats/models'
-import { OperatorActions } from '../../stats/types'
 import { PostgresProposalsRepository } from '../repository/postgres-proposals.repository'
 
 export class AddProposalService {
@@ -17,7 +14,9 @@ export class AddProposalService {
   }
 
   async addProposal (buildingId: string, userId: string, partialProposal: Omit<ProposalProps, 'id' | 'createdBy' | 'buildingId'>) {
-    return this.usePostgres ? this.addProposalToPostgres(buildingId, userId, partialProposal) : this.addProposalToCouchbase(buildingId, userId, partialProposal)
+    return this.usePostgres ?
+      this.addProposalToPostgres(buildingId, userId, partialProposal) :
+      this.addProposalToCouchbase(buildingId, userId, partialProposal)
   }
 
   private async addProposalToPostgres (buildingId: string, userId: string, partialProposal: Omit<ProposalProps, 'id' | 'createdBy' | 'buildingId'>) {
@@ -38,9 +37,6 @@ export class AddProposalService {
     })
 
     await this.couchbaseBuildingsRepository.save(updatedBuilding)
-    const { city, province } = _get(building, 'address', {})
-
-    await OperatorStats.registerAction(userId, OperatorActions.PROPOSAL_SENT, { city, province })
 
     return proposal
   }
