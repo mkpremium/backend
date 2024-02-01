515986d5f4b9dda70215d98e15e0bd985746ee10
Jorge Velasco <jorge.velasco.silva@gmail.com>
2024-02-01 20:33:23 -0300

    Remove scheduled call in Postgres on owner offer refusal.

--- src/scheduled-events/listeners/remove-scheduled-calls-on-owner-refusal.ts
+++ src/scheduled-events/listeners/remove-scheduled-calls-on-owner-refusal.ts
@@ -1,17 +1,28 @@
 import { ScheduledCallsRepository } from '../repository/scheduled-calls.repository'
+import { type EntityManager } from 'typeorm'
+import { ScheduledEvent } from '../scheduled-event.entity'
 
 const buildingStatusesThatCancelScheduledCalls = ['NO VENDE', 'DESCARTADO']
 
 interface Deps {
-  scheduledCallsRepository: ScheduledCallsRepository
+  scheduledCallsRepository: ScheduledCallsRepository,
+  usePostgres: boolean,
+  entityManager: EntityManager
 }
 
-export function removeScheduledCallsOnOwnerRefusal ({ scheduledCallsRepository }: Deps) {
-  return function ({ buildingId, negotiationStatus }) {
+export function removeScheduledCallsOnOwnerRefusal ({ scheduledCallsRepository, usePostgres, entityManager }: Deps) {
+  return async function ({ buildingId, negotiationStatus }) {
     if (!buildingStatusesThatCancelScheduledCalls.includes(negotiationStatus)) {
       return Promise.resolve()
     }
 
-    return scheduledCallsRepository.removeScheduledCallsForBuilding(buildingId)
+    if (usePostgres) {
+      await entityManager.delete(ScheduledEvent, {
+        building: { id: buildingId },
+        type: 'CALLS'
+      })
+    } else {
+      await scheduledCallsRepository.removeScheduledCallsForBuilding(buildingId)
+    }
   }
 }
