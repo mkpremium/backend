9c9dafea492a9461bbb909a1f8d41e13adac43e6
Jorge Velasco <jorge.velasco.silva@gmail.com>
2024-02-01 20:36:40 -0300

    Remove scheduled call in Postgres on discarded contact.

--- src/scheduled-events/listeners/remove-scheduled-call-on-discarded-contact.ts
+++ src/scheduled-events/listeners/remove-scheduled-call-on-discarded-contact.ts
@@ -1,11 +1,26 @@
 import { OwnerContactStatusChanged } from '../../owner/service/change-contact-status.service'
 import { ScheduledCallsRepository } from '../repository/scheduled-calls.repository'
+import type { EntityManager } from 'typeorm'
+import { ScheduledEvent } from '../scheduled-event.entity'
 
-export function removeScheduledCallOnDiscardedContact ({ scheduledCallsRepository }: { scheduledCallsRepository: ScheduledCallsRepository }) {
+interface Deps {
+  scheduledCallsRepository: ScheduledCallsRepository
+  usePostgres: boolean
+  entityManager: EntityManager
+}
+
+export function removeScheduledCallOnDiscardedContact ({ entityManager, scheduledCallsRepository, usePostgres }: Deps) {
   return async function ({ contactId, newContactStatus }: OwnerContactStatusChanged) {
     if (newContactStatus !== 'BAD') {
       return
     }
-    await scheduledCallsRepository.removeScheduledCallsForContact(contactId)
+    if (usePostgres) {
+      await entityManager.delete(ScheduledEvent, {
+        contact: { id: contactId },
+        type: 'CALLS'
+      })
+    } else {
+      await scheduledCallsRepository.removeScheduledCallsForContact(contactId)
+    }
   }
 }
