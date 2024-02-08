9165ad0e2ed719e8e29d522e2bc68ba6d24b500d
Jorge Velasco <jorge.velasco.silva@gmail.com>
2024-02-08 19:18:13 -0300

    Update proposal on Postgres only.

--- src/building/service/update-proposal.service.ts
+++ src/building/service/update-proposal.service.ts
@@ -1,17 +1,11 @@
 import { ProposalProps } from '../building'
-import { CouchbaseBuildingsRepository } from '../repository/couchbase-building.repository'
-import { CouchbaseProposalsRepository } from '../repository/couchbase-proposals.repository'
 import { PostgresProposalsRepository } from '../repository/postgres-proposals.repository'
-import t from 'tcomb'
 
 type UpdateProposalCommand = { patch: Partial<ProposalProps>; callerId: string; proposalId: string }
 
 export class UpdateProposalService {
   constructor (
-    private couchbaseBuildingsRepository: CouchbaseBuildingsRepository,
-    private couchbaseProposalsRepository: CouchbaseProposalsRepository,
-    private postgresProposalsRepository: PostgresProposalsRepository,
-    private usePostgres: boolean
+    private postgresProposalsRepository: PostgresProposalsRepository
   ) {
   }
 
@@ -22,31 +16,9 @@ export class UpdateProposalService {
       patch: proposalUpdate
     }
 
-    return this.usePostgres ? this.updateProposalInPostgres(cmd) : this.updateProposalInCouchbase(cmd)
-  }
-
-  private updateProposalInPostgres (cmd: UpdateProposalCommand) {
     return this.postgresProposalsRepository.save({
       ...cmd.patch,
       id: cmd.proposalId
     } as ProposalProps)
-  }
-
-  private async updateProposalInCouchbase (cmd: UpdateProposalCommand) {
-    const proposal = await this.couchbaseProposalsRepository.get(cmd.proposalId)
-    const updatedProposal = t.update(proposal, {
-      $merge: Object.assign({}, cmd.patch, {
-        updatedBy: cmd.callerId,
-        updatedAt: new Date()
-      })
-    })
-
-    await this.couchbaseProposalsRepository.save(updatedProposal)
-
-    const building = await this.couchbaseBuildingsRepository.get(proposal.buildingId)
-    const updatedBuilding = t.update(building, { recentProposal: { $set: proposal } })
-    await this.couchbaseBuildingsRepository.save(updatedBuilding)
-
-    return proposal
   }
 }
