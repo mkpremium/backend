a413581db09b9d53ddceba7036e3928bc4b41289
Jorge Velasco <jorge.velasco.silva@gmail.com>
2024-02-08 19:08:37 -0300

    Remove old e2e test.

--- test-e2e/.mocharc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  recursive: true,
-  exit: true,
-  require: 'test-e2e/setup.js',
-  timeout: 30000,
-}
--- test-e2e/setup.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-process.env.NODE_ENV = 'test'
-const chai = require('chai')
-
-const chaiAsPromised = require('chai-as-promised')
-
-chai.should()
-chai.use(chaiAsPromised)
