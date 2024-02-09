825ccb08b1fda7a2ef191a0c2c59696f62419b4a
Jorge Velasco <jorge.velasco.silva@gmail.com>
2024-02-08 21:10:33 -0300

    Remove `findOwnerByPhone` Couchbase implementation.

--- src/owner/repository/couchbase-owners.repository.ts
+++ src/owner/repository/couchbase-owners.repository.ts
@@ -12,54 +12,6 @@ import {
   OwnerRepository
 } from './owner.repository'
 
-const findOwnerByContactValueQuery = bucketName => `
-SELECT
-meta(owner).id,
-owner.person.name,
-owner.buildingId,
-owner.person.contacts,
-{
-    meta(building).id,
-    building.address,
-    building.metadata,
-    building.\`use\`,
-    building.location,
-    building.recentProposal,
-    building.cadastre,
-    building.floorArea,
-    building.negotiationStatus,
-    "featuredOwnerId": building.ownerId
-} as building,
-building.negotiationStatus,
-meta(worksheet).id worksheetId,
-{
-    lastEvent.eventDate,
-    "inPerson": lastEvent.event.inPerson,
-    "ownerId": lastEvent.event.ownerId,
-    "flipperName": lastEventFlipper.profile.firstName || ' ' || lastEventFlipper.profile.lastName
-} AS lastEvent,
-ARRAY {"at": sc.eventDate} FOR sc IN  scheduledCalls END as scheduledCalls
-
-FROM ${bucketName} owner
-JOIN ${bucketName} building ON building._documentType = 'building'
-  AND meta(building).id = owner.buildingId
-  AND (building.negotiationStatus IS MISSING OR building.negotiationStatus != 'DESCARTADO')
-JOIN ${bucketName} worksheet ON worksheet._documentType = 'worksheet' AND worksheet.relatedBuildingIds[0] = meta(building).id
-
-LEFT JOIN ${bucketName} lastEvent ON lastEvent._documentType = 'scheduled-event'
-    AND meta(lastEvent).id = worksheet.lastAddedMeeting.id
-LEFT JOIN ${bucketName} lastEventFlipper ON
-    (lastEvent IS NOT NULL AND lastEvent IS NOT MISSING)
-    AND lastEventFlipper._documentType = 'operator'
-    AND meta(lastEventFlipper).id = lastEvent.notifyTo
-LEFT NEST ${bucketName} scheduledCalls ON scheduledCalls._documentType = 'scheduled-event'
-    AND scheduledCalls.type = 'CALLS'
-    AND scheduledCalls.event.buildingId = meta(building).id
-
-WHERE owner._documentType = 'owner'
-AND ANY c IN owner.person.contacts SATISFIES c.\`value\` = $1 AND c.status != 'BAD' END
-`
-
 const buildingOwnersQuery = bucketName => `
 SELECT
 id,
@@ -73,13 +25,6 @@ WHERE _documentType = 'owner' and buildingId = $1
 `
 
 export class CouchbaseOwnersRepository extends CouchbaseRepository<OwnerProps> implements OwnerRepository {
-  async findByPhoneNumber (phoneNumber: string): Promise<FoundOwnerProps[]> {
-    return this.couchbaseAdapter.queryAsync(
-      findOwnerByContactValueQuery(this.bucketName),
-      [phoneNumber]
-    ).then(parseFoundPhones(phoneNumber))
-  }
-
   async buildingOwners (buildingId): Promise<BuildingOwnerProps[]> {
     return this.couchbaseAdapter.queryAsync(
       buildingOwnersQuery(this.bucketName),
