b8563a04a749e28d31e097a2f3c67d5c1d4207cd
Jorge Velasco <jorge.velasco.silva@gmail.com>
2024-02-10 17:09:47 -0300

    Remove Couchbase code for `PropertyManagerRepository`.

--- src/property-manager/PropertyManagerRepository.js
+++ src/property-manager/PropertyManagerRepository.js
@@ -1,27 +1,5 @@
-const activePropertyManagersQuery = bucketName => `
-    SELECT
-      id,
-      profile.city,
-      username as userName,
-      profitGoal.amount as profitGoal,
-      maxLine
-    FROM ${bucketName}
-    WHERE _documentType = 'operator'
-    AND enable = true
-    AND (ANY r IN roles SATISFIES r = 'BUSINESS' END)
-`
-
 export class PropertyManagerRepository {
-  /**
-   * @param {CouchbaseAdapter} couchbaseAdapter
-   */
-  constructor (couchbaseAdapter) {
-    this.couchbaseAdapter = couchbaseAdapter
-  }
-
   getActivePropertyManagers () {
-    return this.couchbaseAdapter.queryAsync(
-      activePropertyManagersQuery(this.couchbaseAdapter.bucketName)
-    )
+    throw new Error('Not implemented')
   }
 }
