2f9f9cbd984f9747c12f8b6c23f7f5e57a1659b9
Jorge Velasco <jorge.velasco.silva@gmail.com>
2024-02-10 12:06:52 -0300

    Don't save the featured owner in couchbase.

--- src/building/service/featured-owner.service.ts
+++ src/building/service/featured-owner.service.ts
@@ -1,20 +1,13 @@
-import { CouchbaseBuildingsRepository } from '../repository/couchbase-building.repository'
 import { EntityManager } from 'typeorm'
 import { Building } from '../building.entity'
 
 export class FeaturedOwnerService {
   constructor (
-    private couchbaseBuildingsRepository: CouchbaseBuildingsRepository,
-    private usePostgres: boolean,
     private entityManager: EntityManager
   ) {
   }
 
   async setBuildingFeaturedOwner (buildingId: string, ownerId: string) {
-    if (this.usePostgres) {
-      await this.entityManager.update(Building, { id: buildingId }, { featuredOwner: { id: ownerId } })
-    } else {
-      await this.couchbaseBuildingsRepository.setBuildingFeaturedOwner(buildingId, ownerId)
-    }
+    await this.entityManager.update(Building, { id: buildingId }, { featuredOwner: { id: ownerId } })
   }
 }
