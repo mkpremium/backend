8e163361df3a29e7c8646e0416cf391e1883a8af
Jorge Velasco <jorge.velasco.silva@gmail.com>
2024-02-10 17:17:25 -0300

    Remove Couchbase code from `StockRepository`.

--- src/stock/StockRepository.js
+++ src/stock/StockRepository.js
@@ -1,64 +1,9 @@
-const statsByPropertyManagerInPeriodQuery = bucketName => `
-    SELECT
-        stock.close.operatorId as propertyManagerId,
-        SUM(stock.sell.transactionAmount - stock.purchase.transactionAmount)
-        - SUM(IFNULL(building.totalExpensesAmount, 0)) as profitAmount
-    FROM ${bucketName} stock
-    LEFT JOIN ${bucketName} building ON building._documentType = 'building' AND building.id = stock.buildingId
-    WHERE stock._documentType = 'stock'
-      AND stock.close IS NOT NULL
-      AND stock.close.transactionDate BETWEEN $1 AND $2
-    GROUP BY stock.close.operatorId
-`
-
-const propertyManagerProfitInPeriodQuery = bucketName => `
-SELECT
-  agent.profitGoal.amount as profitGoal,
-  agent.profile.city as agentCity,
-  ARRAY_SUM(ARRAY (s.sell.transactionAmount - s.purchase.transactionAmount) FOR s IN stock END)
-  - ARRAY_SUM(ARRAY (b.totalExpensesAmount) FOR b IN building END) as profitAmount
-FROM ${bucketName} agent
-LEFT NEST ${bucketName} stock ON stock._documentType = 'stock'
-    AND stock.close IS NOT NULL
-    AND stock.close.operatorId = agent.id
-    AND stock.close.transactionDate BETWEEN $2 AND $3
-LEFT NEST ${bucketName} building ON building._documentType = 'building'
-    AND ANY s IN stock SATISFIES s.buildingId = building.id END
-WHERE agent._documentType = 'operator' AND agent.id = $1
-`
-
-const cityGoal = city => city === 'Lisboa' ? 700000 : 500000
-
 export class StockRepository {
-  /**
-   * @param {CouchbaseAdapter} couchbaseAdapter
-   */
-  constructor (couchbaseAdapter) {
-    this.couchbaseAdapter = couchbaseAdapter
-  }
-
   getTotalProfitInPeriodByPropertyManager (since, until) {
-    return this.couchbaseAdapter.queryAsync(
-      statsByPropertyManagerInPeriodQuery(this.couchbaseAdapter.bucketName),
-      [since.format('YYYY-MM-DD'), until.format('YYYY-MM-DD')]
-    ).catch(error => {
-      throw error
-    })
+    throw new Error(`Not implemented. since: ${since}, until: ${until}`)
   }
 
   async getPropertyManagerProfitInPeriod (propertyManagerId, since, until) {
-    const result = await this.couchbaseAdapter.queryAsync(
-      propertyManagerProfitInPeriodQuery(this.couchbaseAdapter.bucketName),
-      [propertyManagerId, since.format('YYYY-MM-DD'), until.format('YYYY-MM-DD')]
-    )
-
-    if (!result || result.length === 0) {
-      return 0
-    }
-
-    return {
-      profitAmount: result[0].profitAmount,
-      goal: result[0].profitGoal || cityGoal(result[0].agentCity)
-    }
+    throw new Error(`Not implemented. propertyManagerId: ${propertyManagerId}, since: ${since}, until: ${until}`)
   }
 }
--- src/stock/dependencies.ts
+++ src/stock/dependencies.ts
@@ -6,24 +6,23 @@ import {
   updateSellStockControllerFactory
 } from './controllers'
 import { StockSalesService } from './service/stock-sales.service'
+import { StockRepository } from './StockRepository'
 
 export async function setupStockDependencies (container: AwilixContainer, usePostgres: boolean) {
   if (usePostgres) {
     container.register({
-      stockRepository: asValue(null),
       propertyManagersRepository: asValue(null)
     })
   } else {
     const { PropertyManagerRepository } = await import('../property-manager/PropertyManagerRepository')
-    const { StockRepository } = await import('./StockRepository')
 
     container.register({
-      stockRepository: asClass(StockRepository).classic().singleton(),
       propertyManagersRepository: asClass(PropertyManagerRepository).classic().singleton()
     })
   }
 
   container.register({
+    stockRepository: asClass(StockRepository).classic().singleton(),
     stockSalesService: asClass(StockSalesService).classic().singleton(),
     propertyManagerRankingService: asClass(PropertyManagerRankingService).classic().singleton(),
     updatePurchaseStockController: asFunction(updatePurchaseStockFactory).classic(),
