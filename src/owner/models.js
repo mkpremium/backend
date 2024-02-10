41df2000fe1f871158a3b78b9f43f4a58990a917
Jorge Velasco <jorge.velasco.silva@gmail.com>
2024-02-10 16:17:41 -0300

    Remove unused Couchbase repository.

--- src/owner/models.js
+++
@@ -1,9 +0,0 @@
-import { CouchbaseModel } from '../db/model'
-import { Owner } from './owner'
-
-export class OwnerRepository extends CouchbaseModel {
-  constructor () {
-    super()
-    this.Struct = Owner
-  }
-}
