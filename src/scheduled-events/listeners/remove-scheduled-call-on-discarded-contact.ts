016cf135b759df1cced8b065145c6508b7c7e408
Jorge Velasco <jorge.velasco.silva@gmail.com>
2024-02-10 16:52:40 -0300

    Remove calls only in Postgres.

--- src/scheduled-events/listeners/remove-scheduled-call-on-discarded-contact.ts
+++ src/scheduled-events/listeners/remove-scheduled-call-on-discarded-contact.ts
@@ -1,26 +1,19 @@
 import { OwnerContactStatusChanged } from '../../owner/service/change-contact-status.service'
-import { ScheduledCallsRepository } from '../repository/scheduled-calls.repository'
 import type { EntityManager } from 'typeorm'
 import { ScheduledEvent } from '../scheduled-event.entity'
 
 interface Deps {
-  scheduledCallsRepository: ScheduledCallsRepository
-  usePostgres: boolean
   entityManager: EntityManager
 }
 
-export function removeScheduledCallOnDiscardedContact ({ entityManager, scheduledCallsRepository, usePostgres }: Deps) {
+export function removeScheduledCallOnDiscardedContact ({ entityManager }: Deps) {
   return async function ({ contactId, newContactStatus }: OwnerContactStatusChanged) {
     if (newContactStatus !== 'BAD') {
       return
     }
-    if (usePostgres) {
-      await entityManager.delete(ScheduledEvent, {
-        contact: { id: contactId },
-        type: 'CALLS'
-      })
-    } else {
-      await scheduledCallsRepository.removeScheduledCallsForContact(contactId)
-    }
+    await entityManager.delete(ScheduledEvent, {
+      contact: { id: contactId },
+      type: 'CALLS'
+    })
   }
 }
