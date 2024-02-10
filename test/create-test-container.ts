c6db9bcb3d2e793e0b62f3ef1911c3e5737321b5
Jorge Velasco <jorge.velasco.silva@gmail.com>
2024-02-10 15:59:11 -0300

    Drop Couchbase support in tests.

--- test/create-test-container.ts
+++ test/create-test-container.ts
@@ -1,17 +1,6 @@
 import { setupContainer } from '../src/infrastructure/dependencies'
-import type { Bucket } from 'couchbase'
 import { createContainer } from 'awilix'
 import { AppDataSource } from '../src/data-source'
-
-let cachedBucket: Bucket
-
-async function setupCouchbaseBucket () {
-  const { connectCouchbaseBucket } = await import('../src/db/connect-couchbase-bucket')
-  const bucket = await (cachedBucket ? Promise.resolve(cachedBucket) : connectCouchbaseBucket())
-
-  await flushBucket(bucket)
-  return bucket
-}
 
 export async function setupPostgres () {
   if (!AppDataSource.isInitialized) {
@@ -22,36 +11,13 @@ export async function setupPostgres () {
   return AppDataSource
 }
 
-export async function createTestContainer ({ couchbase, postgres }: {
+export async function createTestContainer ({ postgres }: {
   couchbase: boolean,
   postgres: boolean
 } = { couchbase: false, postgres: true }) {
-  const [bucket, dataSource] = await Promise.all([
-    couchbase ? setupCouchbaseBucket() : Promise.resolve(null),
-    postgres
-      ? setupPostgres()
-      : Promise.resolve({
-        getRepository: () => null
-      } as any)
-  ])
+  const dataSource = await setupPostgres()
   const container = createContainer()
-  await setupContainer(container, bucket, dataSource, postgres)
+  await setupContainer(container, null, dataSource, postgres)
 
   return container
-}
-
-export async function flushBucket (bucket: Bucket) {
-  const { N1qlQuery } = await import('couchbase')
-  return new Promise((resolve, reject) => {
-    console.time('deleteDocs')
-    bucket.query(N1qlQuery.fromString('DELETE FROM mkpremium_test'), (error) => {
-      console.timeEnd('deleteDocs')
-      if (error) {
-        console.error('Error deleting documents', error)
-        reject(error)
-        return
-      }
-      setTimeout(resolve, 1000)
-    })
-  })
 }
--- test/integration/create-test-app.ts
+++ test/integration/create-test-app.ts
@@ -1,12 +1,9 @@
 import { createApp } from '../../src/app'
 import { Express } from 'express'
-import { flushBucket, setupPostgres } from '../create-test-container'
+import { setupPostgres } from '../create-test-container'
 
-export const createTestApp = (database: 'couchbase' | 'postgres' = 'couchbase'): Promise<Express> => createApp(database)
+export const createTestApp = (database: 'postgres' = 'postgres'): Promise<Express> => createApp(database)
   .then(async app => {
-    if (database === 'couchbase') {
-      await flushBucket(app.locals.diContainer.resolve('couchbaseBucket'))
-    }
     await setupPostgres()
 
     return app
