dd8f247ef9700c76485cca8934820156977f2ccd
Jorge Velasco <jorge.velasco.silva@gmail.com>
2024-05-04 16:23:18 -0400

    Inline stock routes in routing.

--- src/stock/routing.ts
+++ src/stock/routing.ts
@@ -1,7 +1,27 @@
+import { Router } from 'express'
+import {
+  cancelSaleControllerFactory,
+  closeSellStockControllerFactory,
+  createSellPurchasedStockController,
+  purchaseStockControllerFactory,
+  updatePurchaseStockFactory,
+  updateSellStockControllerFactory
+} from './controllers'
+
 export const setupStockRouter = async (app, container, secured) => {
-  const { addStockRoutes } = await import('./routes')
-  app.use('/stock', secured, addStockRoutes(
-    container.resolve('stockSalesService'),
-    container
-  ))
+  const router = Router()
+  const stockSalesService = container.resolve('stockSalesService')
+
+  router.post('/purchase', purchaseStockControllerFactory(stockSalesService))
+  router.put('/purchase',
+    container.resolve('updatePurchaseStockController') as ReturnType<typeof updatePurchaseStockFactory>)
+
+  router.post('/sell', createSellPurchasedStockController(stockSalesService))
+  router.put('/sell',
+    container.resolve('updateSellStockController') as ReturnType<typeof updateSellStockControllerFactory>)
+  router.post('/sell/cancel', cancelSaleControllerFactory(stockSalesService))
+
+  router.post('/close', container.resolve('closeSellStockController') as ReturnType<typeof closeSellStockControllerFactory>)
+
+  app.use('/stock', secured, router)
 }
