75c31290a689535b38f2f69e9db9d93ac45ad586
Jorge Velasco <jorge.velasco.silva@gmail.com>
2024-05-04 17:34:44 -0400

    Remove unused struct.

--- src/stock/types.ts
+++ src/stock/types.ts
@@ -14,33 +14,3 @@ export const StockStatuses = {
   SELL: 'SELL',
   CLOSE: 'CLOSE'
 }
-const StockStatus = t.enums.of(Object.values(StockStatuses))
-
-export const Stock = t.struct({
-  id: t.maybe(t.String),
-  buildingId: t.String,
-  currentStatus: StockStatus,
-  purchase: Transaction,
-  sell: t.maybe(Transaction),
-  close: t.maybe(t.struct({
-    operatorId: t.String,
-    gain: t.Number,
-    transactionDate: t.Date
-  })),
-  _documentType: t.String
-}, {
-  name: 'Stock',
-  defaultProps: {
-    _documentType: 'stock'
-  }
-})
-
-export const TransactionParams = t.struct({
-  buildingId: t.String,
-  reservationAmount: t.Number,
-  reservationDate: t.Date,
-  transactionAmount: t.Number,
-  transactionDate: t.Date
-})
-
-export default t
