cde990bed84b76dab87a8050ef13d0670b36432c
Jorge Velasco <jorge.velasco.silva@gmail.com>
2024-05-04 18:37:21 -0400

    Remove unused file.

--- src/infrastructure/couchbase/record-to-domain.ts
+++
@@ -1,9 +0,0 @@
-import t from 'tcomb'
-
-export interface RecordToDomain {
-  couchbaseToDomain(): any
-}
-
-export const CouchbaseRecordToDomain = t.interface({
-  couchbaseToDomain: t.Function
-}) as unknown as (t.Interface<any> & RecordToDomain)
