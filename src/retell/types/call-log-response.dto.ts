322dfa434b330534801b3ddf0ca3d4fa08d808bc
pcharlesmorphy <sergeitorres@gmail.com>
2026-02-13 13:57:15 +0100

    Implemented Retell Webhook for Call Logs

---
+++ src/retell/types/call-log-response.dto.ts
@@ -0,0 +1,48 @@
+export interface CallLogResponse {
+    event: string;
+    call: Call;
+}
+
+export interface Call {
+    call_type: string;
+    from_number: string;
+    to_number: string;
+    call_analysis: CallAnalysis;
+    transcript: string;
+    disconnection_reason: string;
+    recording_url: string;
+    call_id: string;
+    call_status: string;
+    call_cost: CallCost;
+    start_timestamp: number;
+    end_timestamp: number;
+    duration_ms: number;
+    agent_id: string;
+    agent_name: string;
+    metadata: Metadata;
+}
+
+export interface CallAnalysis {
+    call_summary: string;
+    in_voicemail: boolean;
+    user_sentiment: string;
+    call_successful: boolean;
+    custom_analysis_data: Data;
+}
+
+export type Metadata = Record<string, unknown>;
+export type Data = Record<string, unknown>;
+
+export interface CallCost {
+    product_costs: ProductCost[];
+    total_duration_seconds: number;
+    total_duration_unit_price: number;
+    combined_cost: number;
+}
+
+export interface ProductCost {
+    product: string;
+    cost: number;
+    unit_price: number;
+    is_transfer_leg_cost: boolean;
+}
