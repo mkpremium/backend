09c215e042090e472fefa22c70da5f2d00d30698
pcharlesmorphy <sergeitorres@gmail.com>
2026-03-05 23:07:07 +0100

    add: luxon library to parse dates

--- src/retell/service/call-service.ts
+++ src/retell/service/call-service.ts
@@ -306,6 +306,7 @@ export class CallService {
 
       this.logger.info(`metadata: ${JSON.stringify(metadata, null, 2)}`)
       this.logger.info(body.args.scheduled_at)
+      this.logger.info(DateTime.fromMillis(scheduledAt).setZone('Europe/Madrid').toLocaleString(DateTime.DATETIME_MED))
 
       if (!phoneNumber) throw new Error('Missing phoneNumber in call payload')
       if (!metadata) throw new Error('Missing metadata in call payload')
